8d2dc74a0abaf96562bb540896f40c3d86c7c67c
Thomas Grimonet <tgrimonet@arista.com>
2019-10-30 09:40:05 +0100

    Issue #18 - Implement Configlet management
    Create configlet structure to load generated config to CVP
    Structure is saved in result['CVP_CONFIGLETS'] and can passed to cv_configlet

--- library/inventory_to_container.py
+++ library/inventory_to_container.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.connection import Connection, ConnectionError
-
+import glob
+import os
 import json
 import yaml
 import pprint
@@ -34,6 +35,20 @@ def isLeaf(tree, nid):
         return True
     else:
         return False
+
+
+def get_configlet(src_folder=str(), prefix='AVD' ,extension='cfg'):
+    src_configlets = glob.glob(src_folder + '/*.' + extension)
+    configlets = dict()
+    for file in src_configlets:
+        if prefix != 'none':
+            name = prefix + '_' + os.path.splitext(os.path.basename(file))[0]
+        else:
+            name = os.path.splitext(os.path.basename(file))[0]
+        with open(file, 'r') as file:
+            data = file.read()
+        configlets[name] = data
+    return configlets
 
 
 def get_devices(dict_inventory, search_container=None, devices=list()):
@@ -104,32 +119,29 @@ if __name__ == '__main__':
     argument_spec = dict(
         inventory=dict(type='str', required=True),
         container_root=dict(type='str', required=True),
-        destination=dict(type='str', required=True)
+        configlet_dir=dict(type='str', required=False),
+        configlet_prefix=dict(type='str', required=False, default='none'),
+        destination=dict(type='str', required=False)
     )
 
     module = AnsibleModule(argument_spec=argument_spec,
                            supports_check_mode=False)
-    result = dict(changed=False, inventory_to_cv_topology={})
+    result = dict(changed=False)
 
     # "/Users/tgrimonet/Projects/ansible-devel/github/aristanetworks/upstream-ansible-avd/examples/evpn-l3ls-cvp-deployment/inventory.yml"
     inventory_file = module.params['inventory']
     parent_container = module.params['container_root']
 
+    # Build containers & devices topology
     inventory_content = str()
     with open(inventory_file, 'r') as stream:
         try:
             inventory_content = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
-
-    # create inventory, use path to host config file as source or hosts in a comma separated string
-    loader = DataLoader()
-    inventory_ansible = InventoryManager(loader=loader, sources=[inventory_file])
-
     serialized_inventory = serialize(dict_inventory=inventory_content)
     tree_dc = serialized_inventory.subtree(parent_container)
     container_list = [tree_dc[node].tag for node in tree_dc.expand_tree()]
-
     container_json = {}
     for container in container_list:
         data = dict()
@@ -143,8 +155,15 @@ if __name__ == '__main__':
                     data['devices'] = devices
                 data['parent_container'] = parent.tag
             container_json[container] = data
+    result['CVP_TOPOLOGY'] = container_json
 
-    result['inventory_to_cv_topology']['CVP_TOPOLOGY'] = container_json
-    with open(module.params['destination'], 'w') as file:
-        documents = yaml.dump(result['inventory_to_cv_topology'], file)
+    # If set, build configlet topology
+    if module.params['configlet_dir'] is not None:
+        result['CVP_CONFIGLETS'] = get_configlet(src_folder=module.params['configlet_dir'],
+                                                 prefix=module.params['configlet_prefix'])
+
+    # Write vars to file if set by user
+    if module.params['destination'] is not None:
+        with open(module.params['destination'], 'w') as file:
+            documents = yaml.dump(result, file)
     module.exit_json(**result)
