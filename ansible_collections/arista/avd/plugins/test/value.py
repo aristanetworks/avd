4813d37a38398a14aa29fdf16e2383ec43dbf8d9
holbech <holbech@arista.com>
2020-12-30 16:57:25 +0100

    Add Test Plugin arista.avd.value

---
+++ ansible_collections/arista/avd/plugins/test/value.py
@@ -0,0 +1,41 @@
+#
+# arista.avd.value
+#
+# Example:
+# A is undefined
+# B is none
+# C is "c"
+# D is "d"
+#
+# Jinja test examples:
+# {% if A is arista.avd.value %}  =>  false
+# {% if B is arista.avd.value %}  =>  false
+# {% if C is arista.avd.value %}  =>  true
+# {% if D is arista.avd.value %}  =>  true
+#
+# {% if A is arista.avd.value("c") %}  =>  false
+# {% if B is arista.avd.value("c") %}  =>  false
+# {% if C is arista.avd.value("c") %}  =>  true
+# {% if D is arista.avd.value("c") %}  =>  false
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from jinja2.runtime import Undefined
+
+def value(value, test_value=None):
+    if isinstance(value, Undefined) or value is None:
+        #Invalid value - return false
+        return False
+    elif test_value is not None and value != test_value:
+        #Valid value but not matching the optional argument
+        return False
+    else:
+        #Valid value and is matching optional argument if provided - return true
+        return True
+
+class TestModule(object):
+    def tests(self):
+        return {
+            'value': value,
+        }
