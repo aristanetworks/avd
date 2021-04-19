43f518c174f7673042c06d556d6136c8a2c61226
holbech <holbech@arista.com>
2021-04-19 22:21:50 +0200

    Strip empty keys from facts

--- ansible_collections/arista/avd/plugins/action/yaml_template_to_fact.py
+++ ansible_collections/arista/avd/plugins/action/yaml_template_to_fact.py
@@ -29,16 +29,16 @@ class ActionModule(ActionBase):
                 n = self._task.args.get("root_key")
                 n = self._templar.template(n)
                 if not isidentifier(n):
-                    raise AnsibleActionFail("The root_key '%s' is not valid. Keys must start with a letter or underscore character, "
+                    raise AnsibleActionFail("The argument 'root_key' value of '%s' is not valid. Keys must start with a letter or underscore character, "
                                             "and contain only letters, numbers and underscores." % n)
                 root_key = n
 
-            if "yaml_templates" in self._task.args:
-                template_list = list(self._task.args.get("yaml_templates"))
+            if "templates" in self._task.args:
+                template_list = list(self._task.args.get("templates"))
             else:
-                raise AnsibleActionFail("The variable 'yaml_templates' is missing")
+                raise AnsibleActionFail("The argument 'templates' must be set")
         else:
-            raise AnsibleActionFail("The variable 'yaml_templates' is missing")
+            raise AnsibleActionFail("The argument 'templates' must be set")
 
         output = dict()
 
@@ -54,6 +54,7 @@ class ActionModule(ActionBase):
 
             template_output = template_lookup_module.run([template], template_vars)
             template_output_data = yaml.safe_load(template_output[0])
+            template_output_data = strip_null_from_output(template_output_data)
             if template_output_data:
                 output = combine(output, template_output_data, recursive=True, list_merge='append')
 
@@ -62,3 +63,36 @@ class ActionModule(ActionBase):
         else:
             result['ansible_facts'] = output
         return result
+
+
+def strip_null_from_output(data):
+    strip_values_tuple = (None,)
+    if isinstance(data, dict):
+        return strip_empties_from_dict(data, strip_values_tuple)
+    elif isinstance(data, list):
+        return strip_empties_from_list(data, strip_values_tuple)
+    return data
+
+
+def strip_empties_from_list(data, strip_values_tuple=(None, str(), list(), dict(),)):
+    new_data = []
+    for v in data:
+        if isinstance(v, dict):
+            v = strip_empties_from_dict(v, strip_values_tuple)
+        elif isinstance(v, list):
+            v = strip_empties_from_list(v, strip_values_tuple)
+        if v not in strip_values_tuple:
+            new_data.append(v)
+    return new_data
+
+
+def strip_empties_from_dict(data, strip_values_tuple=(None, str(), list(), dict(),)):
+    new_data = {}
+    for k, v in data.items():
+        if isinstance(v, dict):
+            v = strip_empties_from_dict(v, strip_values_tuple)
+        elif isinstance(v, list):
+            v = strip_empties_from_list(v, strip_values_tuple)
+        if v not in strip_values_tuple:
+            new_data[k] = v
+    return new_data
--- ansible_collections/arista/avd/plugins/modules/yaml_template_to_fact.py
+++ ansible_collections/arista/avd/plugins/modules/yaml_template_to_fact.py
@@ -21,12 +21,12 @@ ANSIBLE_METADATA = {'metadata_version': '1.0.0',
 
 DOCUMENTATION = r'''
 ---
-module: yaml_template_to_var
+module: yaml_template_to_fact
 version_added: "1.0.0"
 author: EMEA AS Team (@aristanetworks)
 short_description: Set facts from YAML via Jinja2 templates
 description:
-  - Set facts from YAML via Jinja2 templates
+  - Set facts from YAML produced by Jinja2 templates
 options:
   root_key:
     description: Root key under which the facts will be defined. If not set the facts well be set directly on root level.
@@ -42,7 +42,7 @@ options:
 EXAMPLES = r'''
 # tasks file for configlet_build_config
 - name: Generate device configuration in structured format
-  yaml_template_to_var:
+  yaml_template_to_fact:
     root_key: structured_config
     yaml_templates: "{{ templates.structured_config }}"
   check_mode: no
