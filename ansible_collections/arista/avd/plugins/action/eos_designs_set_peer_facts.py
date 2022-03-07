5ddd225d7b51fe53a2e0fbe8194531562f24e7ab
Claus Holbech <holbech@arista.com>
2022-03-07 16:13:04 +0100

    Add module arguments for each fact type

--- ansible_collections/arista/avd/plugins/action/eos_designs_set_peer_facts.py
+++ ansible_collections/arista/avd/plugins/action/eos_designs_set_peer_facts.py
@@ -12,33 +12,41 @@ class ActionModule(ActionBase):
         result = super().run(tmp, task_vars)
         del tmp  # tmp no longer has any effect
 
+        set_avd_topology_peers = self._task.args.get('avd_topology_peers', False)
+        set_avd_overlay_peers = self._task.args.get('avd_overlay_peers', False)
+
         hostvars = task_vars['hostvars']
 
         fabric_name = task_vars.get('fabric_name', 'all')
 
         hosts = task_vars.get('groups', {}).get(fabric_name, [])
 
-        avd_topology_peers = {}
-        avd_overlay_peers = {}
-        for host in hosts:
-            host_hostvars = hostvars.raw_get(host)
+        if set_avd_topology_peers or set_avd_overlay_peers:
+            avd_topology_peers = {}
+            avd_overlay_peers = {}
+            for host in hosts:
+                host_hostvars = hostvars.raw_get(host)
 
-            host_topology_peers = host_hostvars.get('topology', {}).get('peers', [])
+                if set_avd_topology_peers:
+                    host_topology_peers = host_hostvars.get('topology', {}).get('peers', [])
 
-            for peer in host_topology_peers:
-                if peer not in avd_topology_peers:
-                    avd_topology_peers[peer] = []
-                avd_topology_peers[peer].append(host)
+                    for peer in host_topology_peers:
+                        if peer not in avd_topology_peers:
+                            avd_topology_peers[peer] = []
+                        avd_topology_peers[peer].append(host)
 
-            host_evpn_route_servers = host_hostvars.get('switch', {}).get('evpn_route_servers', [])
+                if set_avd_overlay_peers:
+                    host_evpn_route_servers = host_hostvars.get('switch', {}).get('evpn_route_servers', [])
 
-            for peer in host_evpn_route_servers:
-                if peer not in avd_overlay_peers:
-                    avd_overlay_peers[peer] = []
-                avd_overlay_peers[peer].append(host)
+                    for peer in host_evpn_route_servers:
+                        if peer not in avd_overlay_peers:
+                            avd_overlay_peers[peer] = []
+                        avd_overlay_peers[peer].append(host)
 
-        result['ansible_facts'] = {
-            'avd_topology_peers': avd_topology_peers,
-            'avd_overlay_peers': avd_overlay_peers
-        }
+            result['ansible_facts'] = {}
+            if set_avd_topology_peers:
+                result['ansible_facts']['avd_topology_peers'] = avd_topology_peers
+            if set_avd_overlay_peers:
+                result['ansible_facts']['avd_overlay_peers'] = avd_overlay_peers
+
         return result
--- ansible_collections/arista/avd/plugins/modules/eos_designs_set_peer_facts.py
+++ ansible_collections/arista/avd/plugins/modules/eos_designs_set_peer_facts.py
@@ -1,6 +1,6 @@
 # GNU General Public License v3.0+
 #
-# Copyright 2022 Arista Networks AS-EMEA
+# Copyright 2022 Arista Networks
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,16 +19,31 @@ DOCUMENTATION = r'''
 ---
 module: eos_designs_set_peer_facts
 version_added: "3.4.0"
-author: EMEA AS Team (@aristanetworks)
+author: Arista Ansible Team <ansible@arista.com>
 short_description: Set eos_designs peer facts
 description:
   - Parse all hostvars and set a common avd_topology_peers and avd_overlay_peers dicts with list per host
+options:
+  avd_topology_peers:
+    description: |
+      Set avd_topology_peers fact with a list per device of downstream devices,
+      meaning devices pointing to this device as uplink switch
+    required: false
+    type: bool
+  avd_overlay_peers:
+    description: |
+      Set avd_overlay_peers fact with a list per device of EVPN Route Server Clients,
+      meaning devices pointing to this device as EVPN Router Server
+    required: false
+    type: bool
 '''
 
 EXAMPLES = r'''
 # tasks file for configlet_build_config
 - name: Set AVD peer facts
-  eos_designs_set_peer_facts: {}
+  eos_designs_set_peer_facts:
+    avd_topology_peers: True
+    avd_overlay_peers: True
   delegate_facts: True
   delegate_to: "{{ ansible_play_hosts[0] }}"
   run_once: True
