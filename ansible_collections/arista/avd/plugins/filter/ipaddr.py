ab2ef8adf6b83c118a4e7f6e1d603e55379a9c63
tgodaA <tgoda@arista.com>
2022-06-13 19:08:11 -0400

    Fix sanity errors and added testcases

--- ansible_collections/arista/avd/plugins/filter/ipaddr.py
+++ ansible_collections/arista/avd/plugins/filter/ipaddr.py
@@ -3,32 +3,39 @@ __metaclass__ = type
 
 import ipaddress
 
+
 class FilterModule(object):
 
     def net(self, val):
+        try:
+            return ipaddress.ip_network(val, strict=True)
+        except ValueError:
+            return
+
+    def subnet(self, val):
         if self.validate(val):
-            return ipaddress.ip_network(val, strict = False)
+            return ipaddress.ip_network(val, strict=False)
         return
 
     def netmask(self, val):
-        valid_subnet = self.net(val)
+        valid_subnet = self.subnet(val)
         if valid_subnet:
             return valid_subnet.netmask
 
     def prefix(self, val):
-        valid_subnet = self.net(val)
+        valid_subnet = self.subnet(val)
         if valid_subnet:
             return valid_subnet.prefixlen
         return
 
     def size(self, val):
-        valid_subnet = self.net(val)
+        valid_subnet = self.subnet(val)
         if valid_subnet:
-            return 2**(valid_subnet.max_prefixlen-valid_subnet.prefixlen)
+            return 2**(valid_subnet.max_prefixlen - valid_subnet.prefixlen)
         return
 
     def network(self, val):
-        valid_subnet = self.net(val)
+        valid_subnet = self.subnet(val)
         if valid_subnet:
             return valid_subnet.network_address
         return
@@ -37,7 +44,7 @@ class FilterModule(object):
         try:
             if isinstance(val, str) and '/' in val:
                 try:
-                    ipaddress.ip_network(val)
+                    ipaddress.ip_network(val, strict=True)
                     return
                 except ValueError:
                     ip = ipaddress.ip_interface(val).ip
@@ -57,23 +64,49 @@ class FilterModule(object):
         except ValueError:
             return
 
-    def ipaddr(self, ip_addresses, method = 'validate'):
+    def defaultattr(self, input_val, ip_address):
+        try:
+            input_val_int = int(input_val)
+            if self.size(ip_address) == 1:
+                return ip_address
+            else:
+                max_ips = self.size(ip_address)
+                if input_val_int < max_ips:
+                    network_address = ipaddress.ip_network(ip_address, strict=False)
+                    calc_ip = str(network_address.network_address + input_val_int)
+                    prefix = network_address.prefixlen
+                    return f'{calc_ip}/{prefix}'
+                else:
+                    return
+        except ValueError:
+            subnet_a = self.subnet(input_val)
+            subnet_b = ipaddress.ip_network(ip_address, strict=False)
+            if subnet_a and subnet_a.version == subnet_b.version and subnet_b.subnet_of(subnet_a):
+                return ip_address
+            else:
+                return
+
+    def ipaddr(self, ip_addresses, method='validate'):
         result = None
-        if type(ip_addresses) in (str,int):
-            result = getattr(self, method)(ip_addresses)
+        if type(ip_addresses) in (str, int):
+            try:
+                result = getattr(self, method)(ip_addresses)
+            except (TypeError, AttributeError):
+                if self.validate(ip_addresses):
+                    result = self.defaultattr(method, str(self.validate(ip_addresses)))
             if result is not None:
                 result = str(result)
         elif type(ip_addresses) is list:
             result = []
             for ip_address in ip_addresses:
-                result.append(self.ipaddr(ip_address, method = method))
+                result.append(self.ipaddr(ip_address, method=method))
 
         return result
 
     def ipmath(self, ip_addresses, add_val):
         result = None
         if type(ip_addresses) in (str, int) and self.validate(ip_addresses):
-            result = str(self.validate(ip_addresses) + int(add_val)).split('/')[0]
+            result = str(self.validate(ip_addresses) + int(add_val)).split('/', maxsplit=1)[0]
         elif type(ip_addresses) is list:
             result = []
             for ip_address in ip_addresses:
@@ -84,4 +117,5 @@ class FilterModule(object):
     def filters(self):
         return {
             'ipaddr': self.ipaddr,
+            'ipmath': self.ipmath,
         }
--- ansible_collections/arista/avd/tests/unit/filters/test_ipaddr.py
+++ ansible_collections/arista/avd/tests/unit/filters/test_ipaddr.py
@@ -7,17 +7,41 @@ import pytest
 f = FilterModule()
 
 VALIDATE_LIST_INPUT_EXPECTED = [('', None), (3232235521, '192.168.0.1'), ('192.168.4.256/24', None), ('192.168', None),
-                              ('10.10/24', None), ('192.168.0.1/', None), ('192.168.0.1', '192.168.0.1'), ('0.0.0.0','0.0.0.0'),
-                              ('255.255.255.255','255.255.255.255'), ('0.0.0.1/24', '0.0.0.1/24'), ('192.168.1.0/24', '192.168.1.0/24'),
-                              ('192.168.2.1/24', '192.168.2.1/24'), ('192.168.3.255/24', '192.168.3.255/24')]
+                                ('10.10/24', None), ('192.168.0.1/', None), ('192.168.0.1', '192.168.0.1'), ('0.0.0.0', '0.0.0.0'),
+                                ('255.255.255.255', '255.255.255.255'), ('0.0.0.1/24', '0.0.0.1/24'), ('192.168.1.0/24', '192.168.1.0/24'),
+                                ('192.168.2.1/24', '192.168.2.1/24'), ('192.168.3.255/24', '192.168.3.255/24')]
 
-IP_ADDRESSES = ['192.24.2.1', 'host.fqdn', '::1', '192.168.32.0/24', 'fe80::100/10', True, '', '192.168.32.1/24', '42540766412265424405338506004571095040/64']
-IP_ADDRESSES_VALIDATE_EXPECTED = ['192.24.2.1', None, '::1', '192.168.32.0/24', 'fe80::100/10', None, None, '192.168.32.1/24', None]
+IP_ADDRESSES_INPUT = ['192.24.2.1', 'host.fqdn', '::1', '192.168.32.0/24', 'fe80::100/10', True, '', '192.168.32.1/24',
+                      '42540766412265424405338506004571095040/64', '192.168.1.255/24', '192.168.1.34/28', '192.168.1.34/25']
 
-IP_ADDRESSES_NETWORK = ['192.24.2.1', 'host.fqdn', '::1', '192.168.32.0/24', 'fe80::100/10', True, '', '192.168.32.1/24',
-                        '42540766412265424405338506004571095040/64', '192.168.1.255/24', '192.168.1.34/28', '192.168.1.34/25']
-IP_ADDRESSES_NETWORK_EXPECTED = ['192.24.2.1/32', None, '::1/128', '192.168.32.0/24', 'fe80::/10', None, None, '192.168.32.0/24', None,
-                                 '192.168.1.0/24', '192.168.1.32/28', '192.168.1.0/25']
+IP_ADDRESSES_VALIDATE_EXPECTED = ['192.24.2.1', None, '::1', '192.168.32.0/24', 'fe80::100/10', None, None, '192.168.32.1/24', None,
+                                  '192.168.1.255/24', '192.168.1.34/28', '192.168.1.34/25']
+IP_ADDRESSES_NET_EXPECTED = ['192.24.2.1/32', None, '::1/128', '192.168.32.0/24', None, None, None, None, None, None, None, None]
+IP_ADDRESSES_SUBNETWORK_EXPECTED = ['192.24.2.1/32', None, '::1/128', '192.168.32.0/24', 'fe80::/10', None, None, '192.168.32.0/24', None,
+                                    '192.168.1.0/24', '192.168.1.32/28', '192.168.1.0/25']
+IP_ADDRESSES_NETMASK_EXPECTED = ['255.255.255.255', None, 'ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff', '255.255.255.0', 'ffc0::', None, None,
+                                 '255.255.255.0', None, '255.255.255.0', '255.255.255.240', '255.255.255.128']
+IP_ADDRESSES_PREFIX_EXPECTED = ['32', None, '128', '24', '10', None, None, '24', None, '24', '28', '25']
+IP_ADDRESSES_SIZE_EXPECTED = ['1', None, '1', '256', '332306998946228968225951765070086144', None, None, '256', None, '256', '16', '128']
+IP_NETWORK_ADDRESSES_EXPECTED = ['192.24.2.1', None, '::1', '192.168.32.0', 'fe80::', None, None, '192.168.32.0', None, '192.168.1.0',
+                                 '192.168.1.32', '192.168.1.0']
+IP_ADDRESSES_EXPECTED = ['192.24.2.1', None, '::1', None, 'fe80::100', None, None, '192.168.32.1', None, '192.168.1.255',
+                         '192.168.1.34', '192.168.1.34']
+DEFAULTATTR_INPUT = [5, '5', '192.168.0.0/16']
+IP_ADDRESSES_DEFAULTATTR_EXPECTED = [['192.24.2.1', None, '::1', '192.168.32.5/24', 'fe80::5/10', None, None, '192.168.32.5/24', None,
+                                     '192.168.1.5/24', '192.168.1.37/28', '192.168.1.5/25'],
+                                     [None, None, None, '192.168.32.0/24', None, None, None, '192.168.32.1/24', None, '192.168.1.255/24',
+                                      '192.168.1.34/28', '192.168.1.34/25']
+                                     ]
+INPUT_IPMATH_OFFSET = [1, '10', -1]
+IPMATH_ADDRESSES_EXPECTED = [['192.24.2.2', None, '::2', '192.168.32.1', 'fe80::101', None, None, '192.168.32.2', None, '192.168.2.0',
+                              '192.168.1.35', '192.168.1.35'],
+                             ['192.24.2.11', None, '::b', '192.168.32.10', 'fe80::10a', None, None, '192.168.32.11', None, '192.168.2.9',
+                              '192.168.1.44', '192.168.1.44'],
+                             ['192.24.2.0', None, '::', '192.168.31.255', 'fe80::ff', None, None, '192.168.32.0', None, '192.168.1.254',
+                              '192.168.1.33', '192.168.1.33']
+                             ]
+
 
 class TestIpAddrFilter():
     @pytest.mark.parametrize("VALIDATE_INPUT,VALIDATE_EXPECTED", VALIDATE_LIST_INPUT_EXPECTED)
@@ -27,16 +51,16 @@ class TestIpAddrFilter():
             resp = str(resp)
         assert resp == VALIDATE_EXPECTED
 
-    @pytest.mark.parametrize("IP_ADDRESS", IP_ADDRESSES)
+    @pytest.mark.parametrize("IP_ADDRESS", IP_ADDRESSES_INPUT)
     def test_ipaddr_validate_item(self, IP_ADDRESS):
         resp = f.ipaddr(IP_ADDRESS)
         expected_ip = f.validate(IP_ADDRESS)
-        if type(IP_ADDRESS) in (str,int) and expected_ip:
+        if type(IP_ADDRESS) in (str, int) and expected_ip:
             assert resp == str(expected_ip)
         else:
             assert resp is None
 
-    @pytest.mark.parametrize("IP_ADDRESS_LIST_INPUT,IP_ADDRESS_LIST_EXPECTED", [(IP_ADDRESSES, IP_ADDRESSES_VALIDATE_EXPECTED)])
+    @pytest.mark.parametrize("IP_ADDRESS_LIST_INPUT,IP_ADDRESS_LIST_EXPECTED", [(IP_ADDRESSES_INPUT, IP_ADDRESSES_VALIDATE_EXPECTED)])
     def test_ipaddr_validate_list(self, IP_ADDRESS_LIST_INPUT, IP_ADDRESS_LIST_EXPECTED):
         resp_list = f.ipaddr(IP_ADDRESS_LIST_INPUT)
         for index, resp_ip in enumerate(resp_list):
@@ -45,7 +69,59 @@ class TestIpAddrFilter():
             else:
                 assert resp_ip is None
 
-    @pytest.mark.parametrize("IP_ADDRESS_LIST_INPUT,IP_ADDRESS_LIST_EXPECTED", [(IP_ADDRESSES_NETWORK, IP_ADDRESSES_NETWORK_EXPECTED)])
+    @pytest.mark.parametrize("IP_ADDRESS_LIST_INPUT,IP_ADDRESS_LIST_EXPECTED", [(IP_ADDRESSES_INPUT, IP_ADDRESSES_NET_EXPECTED)])
     def test_ipaddr_network_list(self, IP_ADDRESS_LIST_INPUT, IP_ADDRESS_LIST_EXPECTED):
-        resp_list = f.ipaddr(IP_ADDRESS_LIST_INPUT, method = 'net')
+        resp_list = f.ipaddr(IP_ADDRESS_LIST_INPUT, method='net')
         assert resp_list == IP_ADDRESS_LIST_EXPECTED
+
+    @pytest.mark.parametrize("IP_ADDRESS_LIST_INPUT,IP_ADDRESS_LIST_EXPECTED", [(IP_ADDRESSES_INPUT, IP_ADDRESSES_SUBNETWORK_EXPECTED)])
+    def test_ipaddr_subnetwork_list(self, IP_ADDRESS_LIST_INPUT, IP_ADDRESS_LIST_EXPECTED):
+        resp_list = f.ipaddr(IP_ADDRESS_LIST_INPUT, method='subnet')
+        assert resp_list == IP_ADDRESS_LIST_EXPECTED
+
+    @pytest.mark.parametrize("IP_ADDRESS_LIST_INPUT,IP_ADDRESS_LIST_EXPECTED", [(IP_ADDRESSES_INPUT, IP_ADDRESSES_NETMASK_EXPECTED)])
+    def test_ipaddr_netmask_list(self, IP_ADDRESS_LIST_INPUT, IP_ADDRESS_LIST_EXPECTED):
+        resp_list = f.ipaddr(IP_ADDRESS_LIST_INPUT, method='netmask')
+        assert resp_list == IP_ADDRESS_LIST_EXPECTED
+
+    @pytest.mark.parametrize("IP_ADDRESS_LIST_INPUT,IP_ADDRESS_LIST_EXPECTED", [(IP_ADDRESSES_INPUT, IP_ADDRESSES_PREFIX_EXPECTED)])
+    def test_ipaddr_prefixes_list(self, IP_ADDRESS_LIST_INPUT, IP_ADDRESS_LIST_EXPECTED):
+        resp_list = f.ipaddr(IP_ADDRESS_LIST_INPUT, method='prefix')
+        assert resp_list == IP_ADDRESS_LIST_EXPECTED
+
+    @pytest.mark.parametrize("IP_ADDRESS_LIST_INPUT,IP_ADDRESS_LIST_EXPECTED", [(IP_ADDRESSES_INPUT, IP_ADDRESSES_SIZE_EXPECTED)])
+    def test_ipaddr_size_list(self, IP_ADDRESS_LIST_INPUT, IP_ADDRESS_LIST_EXPECTED):
+        resp_list = f.ipaddr(IP_ADDRESS_LIST_INPUT, method='size')
+        assert resp_list == IP_ADDRESS_LIST_EXPECTED
+
+    @pytest.mark.parametrize("IP_ADDRESS_LIST_INPUT,IP_ADDRESS_LIST_EXPECTED", [(IP_ADDRESSES_INPUT, IP_NETWORK_ADDRESSES_EXPECTED)])
+    def test_ipaddr_network_address_list(self, IP_ADDRESS_LIST_INPUT, IP_ADDRESS_LIST_EXPECTED):
+        resp_list = f.ipaddr(IP_ADDRESS_LIST_INPUT, method='network')
+        assert resp_list == IP_ADDRESS_LIST_EXPECTED
+
+    @pytest.mark.parametrize("IP_ADDRESS_LIST_INPUT,IP_ADDRESS_LIST_EXPECTED", [(IP_ADDRESSES_INPUT, IP_ADDRESSES_EXPECTED)])
+    def test_ipaddr_network_address_list(self, IP_ADDRESS_LIST_INPUT, IP_ADDRESS_LIST_EXPECTED):
+        resp_list = f.ipaddr(IP_ADDRESS_LIST_INPUT, method='address')
+        assert resp_list == IP_ADDRESS_LIST_EXPECTED
+
+    @pytest.mark.parametrize("INPUT_LISTS,IP_ADDRESS_LISTS_EXPECTED", [((IP_ADDRESSES_INPUT, DEFAULTATTR_INPUT), IP_ADDRESSES_DEFAULTATTR_EXPECTED)])
+    def test_ipaddr_defaultattr_offset_list(self, INPUT_LISTS, IP_ADDRESS_LISTS_EXPECTED):
+        IP_ADDRESS_LIST_INPUT = INPUT_LISTS[0]
+        for DEFAULTATTR_INPUT_ITEM in INPUT_LISTS[1]:
+            try:
+                INPUT_VAL = int(DEFAULTATTR_INPUT_ITEM)
+                EXPECTED_OUTPUT = IP_ADDRESS_LISTS_EXPECTED[0]
+            except ValueError:
+                INPUT_VAL = DEFAULTATTR_INPUT_ITEM
+                EXPECTED_OUTPUT = IP_ADDRESS_LISTS_EXPECTED[1]
+            resp_list = f.ipaddr(IP_ADDRESS_LIST_INPUT, method=INPUT_VAL)
+            print(resp_list, EXPECTED_OUTPUT)
+            assert resp_list == EXPECTED_OUTPUT
+
+    @pytest.mark.parametrize("INPUT_LISTS,IP_ADDRESS_LISTS_EXPECTED", [((IP_ADDRESSES_INPUT, INPUT_IPMATH_OFFSET), IPMATH_ADDRESSES_EXPECTED)])
+    def test_ipaddr_network_address_list(self, INPUT_LISTS, IP_ADDRESS_LISTS_EXPECTED):
+        IP_ADDRESS_LIST_INPUT = INPUT_LISTS[0]
+        for index, OFFSET in enumerate(INPUT_LISTS[1]):
+            resp_list = f.ipmath(IP_ADDRESS_LIST_INPUT, OFFSET)
+            IP_ADDRESS_LIST_EXPECTED = IP_ADDRESS_LISTS_EXPECTED[index]
+            assert resp_list == IP_ADDRESS_LIST_EXPECTED
