77516be1c0041a1917adbc7a2e118d89e9f01725
Claus Holbech <holbech@arista.com>
2022-07-04 09:30:38 +0200

    Fix various ansible-test issues

--- ansible_collections/arista/avd/plugins/filter/range_expand.py
+++ ansible_collections/arista/avd/plugins/filter/range_expand.py
@@ -4,7 +4,6 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-from itertools import count, groupby
 import re
 from ansible.errors import AnsibleFilterError
 
