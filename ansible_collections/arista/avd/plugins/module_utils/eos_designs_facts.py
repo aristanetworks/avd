e568d3eab1d2b137a30c077a9b935b0849000ce9
Carl Buchmann <carl.buchmann@arista.com>
2022-07-06 16:19:39 -0400

    fix pep8 issues

--- ansible_collections/arista/avd/plugins/module_utils/eos_designs_facts.py
+++ ansible_collections/arista/avd/plugins/module_utils/eos_designs_facts.py
@@ -771,13 +771,13 @@ class EosDesignsFacts:
                 )
             elif self.mlag is True:
                 peer_overlay_rd_type_admin_subfield = get(self._hostvars,
-                                f"avd_switch_facts..{self.mlag_peer}..switch..overlay_rd_type_admin_subfield",
-                                org_key=f"avd_switch_facts.({self.mlag_peer}).switch.overlay_rd_type_admin_subfield",
-                                separator="..")
+                                                          f"avd_switch_facts..{self.mlag_peer}..switch..overlay_rd_type_admin_subfield",
+                                                          org_key=f"avd_switch_facts.({self.mlag_peer}).switch.overlay_rd_type_admin_subfield",
+                                                          separator="..")
                 if str(self.overlay_rd_type_admin_subfield) == str(peer_overlay_rd_type_admin_subfield):
                     raise AristaAvdError(
-                    "For MLAG devices Route Distinguisher must be unique when 'evpn_multicast: True' "
-                    "since it will create a multi-vtep configuration."
+                        "For MLAG devices Route Distinguisher must be unique when 'evpn_multicast: True' "
+                        "since it will create a multi-vtep configuration."
                     )
             return get(self._hostvars, "evpn_multicast", default=False)
         return None
