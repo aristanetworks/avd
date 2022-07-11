08936887d1c1495947429ae76c5527b8e045840f
gmuloc <gmulocher@arista.com>
2022-07-11 10:19:29 +0200

    CI: refactor release.yml as per comments

--- .github/generate_release.py
+++ .github/generate_release.py
@@ -34,9 +34,9 @@ CATEGORIES = {
     "Fix": "Bug Fixes 🐛",
     "Cut": "Cut ✂️",
     "Doc": "Documentation 📚",
-    "CI": "CI 👷",
+    #    "CI": "CI 👷",
     "Bump": "Bump ☝️",
-    "Test": "Test 🚨",
+    #    "Test": "Test 🚨",
     "Revert": "Revert ⏪ ",
     "Refactor": "Refactoring 🔨",
 }
@@ -48,68 +48,118 @@ class Dumper(yaml.Dumper):
     https://github.com/yaml/pyyaml/issues/234#issuecomment-765894586
     """
 
+    # pylint: disable=R0901,W0613,W1113
+
     def increase_indent(self, flow=False, *args, **kwargs):
         return super().increase_indent(flow=flow, indentless=False)
 
 
 if __name__ == "__main__":
-    result_list = []
+    exclude_list = []
+    categories_list = []
+    # First add exclude labels
     for scope in SCOPES:
         if scope != "":
-            # First add Breaking Changes
-            result_list.append(
-                {
-                    "title": f"`{scope}` Breaking Changes 🛠",
-                    "labels": [f"rn: Feat({scope})!", f"rn: Fix({scope})!"],
-                }
-            )
-            if scope in ["eos_designs", "eos_cli_config_gen"]:
-                result_list[-1]["labels"].extend(
-                    [
-                        "rn: Feat(eos_designs-eos_cli_config_gen)!",
-                        "rn: Feat(eos_cli_config_gen-eos_designs)!",
-                    ]
-                )
-            # Then add the rest of the changes for each scope
-            for cc_type, rn_title in CATEGORIES.items():
-                result_list.append(
-                    {
-                        "title": f"`{scope}` {rn_title}",
-                        "labels": [f"rn: {cc_type}({scope})"],
-                    }
-                )
-                if scope in ["eos_designs", "eos_cli_config_gen"]:
-                    result_list[-1]["labels"].extend(
-                        [
-                            "rn: Feat(eos_designs-eos_cli_config_gen)",
-                            "rn: Feat(eos_cli_config_gen-eos_designs)",
-                        ]
-                    )
-        else:  # scope == ""
-            result_list.append(
-                {
-                    "title": "Other Breaking Changes 🛠",
-                    "labels": ["rn: Feat!", "rn: Fix!"],
-                }
-            )
-            for cc_type, rn_title in CATEGORIES.items():
-                result_list.append(
-                    {
-                        "title": f"Other {rn_title}",
-                        "labels": [f"rn: {cc_type}"],
-                    }
-                )
+            exclude_list.append(f"rn: Test({scope})")
+            exclude_list.append(f"rn: CI({scope})")
+        else:
+            exclude_list.append("rn: Test")
+            exclude_list.append("rn: CI")
+    # Then add the categories
+    # First add Breaking Changes
+    breaking_labels = []
+    for scope in SCOPES:
+        for cc_type, rn_title in CATEGORIES.items():
+            breaking_labels.append(f"rn: {cc_type}({scope})!")
+    categories_list.append(
+        {
+            "title": "Breaking Changes 🛠",
+            "labels": breaking_labels,
+        }
+    )
+    # Add fixes in eos_cli_config_gen
+    categories_list.append(
+        {
+            "title": "Fixed issues in eos_cli_config_gen 🐛",
+            "labels": "rn: Fix(eos_cli_config_gen)",
+        }
+    )
+    # Add fixes in eos_designs
+    categories_list.append(
+        {
+            "title": "Fixed issues in eos_designs 🐛",
+            "labels": "rn: Fix(eos_designs)",
+        }
+    )
+    # Add other fixes
+    other_fixes_labels = []
+    for scope in SCOPES:
+        if scope not in ["eos_cli_config_gen", "eos_designs", ""]:
+            other_fixes_labels.append(f"rn: Fix({scope})")
+        elif scope == "":
+            other_fixes_labels.append("rn: Fix")
+    categories_list.append(
+        {
+            "title": "Other Fixed issues 🐛",
+            "labels": other_fixes_labels,
+        }
+    )
+    # Add Documentation
+    doc_labels = []
+    for scope in SCOPES:
+        if scope != "":
+            doc_labels.append(f"rn: Doc({scope})")
+        elif scope == "":
+            doc_labels.append("rn: Doc")
+    categories_list.append(
+        {
+            "title": "Documentation 📚",
+            "labels": doc_labels,
+        }
+    )
+    # Add new features in eos_cli_config_gen
+    categories_list.append(
+        {
+            "title": "New features and enhancements in eos_cli_config_gen ✨",
+            "labels": "rn: Feat(eos_cli_config_gen)",
+        }
+    )
+    # Add new features in eos_designs
+    categories_list.append(
+        {
+            "title": "New features and enhancements in eos_designs ✨",
+            "labels": "rn: Feat(eos_designs)",
+        }
+    )
+    # Add other new features
+    other_feat_labels = []
+    for scope in SCOPES:
+        if scope not in ["eos_cli_config_gen", "eos_designs", ""]:
+            other_feat_labels.append(f"rn: Feat({scope})")
+        elif scope == "":
+            other_feat_labels.append("rn: Feat")
+    categories_list.append(
+        {
+            "title": "Other new features and enhancements ✨",
+            "labels": other_feat_labels,
+        }
+    )
 
     # Add the catch all
-    result_list.append(
+    categories_list.append(
         {
             "title": "Other Changes 🔀",
             "labels": ["*"],
         }
     )
-    with open(r"release.yml", "w") as release_file:
+    with open(r"release.yml", "w", encoding="utf-8") as release_file:
         yaml.dump(
-            {"changelog": {"categories": result_list}},
+            {
+                "changelog": {
+                    "exclude": {"labels": exclude_list},
+                    "categories": categories_list,
+                }
+            },
             release_file,
             Dumper=Dumper,
             sort_keys=False,
