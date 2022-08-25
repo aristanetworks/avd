f4dd01a0a76cc38f02c355f70aa2841f3edca0c2
gmuloc <gmulocher@arista.com>
2022-08-25 16:27:55 +0200

    Feat(plugins): initial code for requirement check

---
+++ ansible_collections/arista/avd/plugins/action/verify_requirements.py
@@ -0,0 +1,81 @@
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+import importlib.metadata
+import re
+import sys
+
+from pip._vendor.packaging.specifiers import SpecifierSet
+
+from ansible.plugins.action import ActionBase
+from ansible.errors import AnsibleActionFail
+from ansible.utils.display import Display
+
+python_version_info = dict(
+    major=sys.version_info[0],
+    minor=sys.version_info[1],
+    micro=sys.version_info[2],
+    releaselevel=sys.version_info[3],
+    serial=sys.version_info[4],
+)
+
+
+class ActionModule(ActionBase):
+    def run(self, tmp=None, task_vars=None):
+        if task_vars is None:
+            task_vars = {}
+
+        result = super().run(tmp, task_vars)
+        del tmp  # tmp no longer has any effect
+        dependencies_dict = {"not_found": {}, "valid": {}, "mismatched": {}}
+
+        if not (self._task.args and "dependencies" in self._task.args):
+            raise AnsibleActionFail("The argument 'dependencies' must be set")
+
+        dependencies = self._task.args.get("dependencies")
+
+        if not isinstance(dependencies, list):
+            raise AnsibleActionFail("The argument 'dependencies' is not a list")
+
+        # Can match this format
+        # requests [security] >=2.8.1, == 2.8.* ; python_version < "2.7"
+        req_re = re.compile(
+            r"(^[a-zA-Z][a-zA-Z0-9_-]+) ?((?:\[[a-zA-Z0-9]+\])?) ?((?:(?:==|[!><~]=?) ?(?:[0-9.*]+),? ?)*)(?: ?; ?(.*))?$"
+        )
+
+        for dep in dependencies:
+            match = req_re.match(dep)
+            if not match:
+                raise AnsibleActionFail(f"Failed to parse {dep}")
+            pkg, label, specifiers_str, extra = match.groups()
+            specifiers_set = SpecifierSet(specifiers_str)
+
+            try:
+                installed_version = importlib.metadata.version(pkg)
+                Display().vvvv(f"Found {pkg} {installed_version} installed!", False)
+            except importlib.metadata.PackageNotFoundError:
+                dependencies_dict["not_found"][pkg] = {
+                    "installed": None,
+                    "desired": str(specifiers_set) if len(specifiers_set) > 0 else None,
+                }
+                result["failed"] = True
+                continue
+
+            if specifiers_set.contains(installed_version):
+                dependencies_dict["valid"][pkg] = {
+                    "installed": installed_version,
+                    "desired": str(specifiers_set) if len(specifiers_set) > 0 else None,
+                }
+            else:
+                dependencies_dict["mismatched"][pkg] = {
+                    "installed": installed_version,
+                    "desired": str(specifiers_set) if len(specifiers_set) > 0 else None,
+                }
+                result["failed"] = True
+
+        result["python_version_info"] = python_version_info
+        result["python_path"] = sys.path
+        result["dependencies"] = dependencies_dict
+
+        return result
---
+++ ansible_collections/arista/avd/plugins/modules/verify_requirements.py
@@ -0,0 +1,41 @@
+# Copyright 2022 Arista Networks
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DOCUMENTATION = r"""
+---
+module: verify_requirements
+version_added: "3.8.0"
+author: Arista Ansible Team (@aristanetworks)
+short_description: Verify Python requirements when running AVD
+description:
+  - The `arista.avd.verify_requirements` module is an Ansible Action Plugin providing the following capabilities
+  - Given a list of python requirements, verify if the installed libraries match these requirements
+options:
+  dependencies:
+    description: |
+      - List of strings of dependencies with pip file syntax
+    required: True
+    type: list
+"""
+
+EXAMPLES = r"""
+- name: Verify python requirements
+  arista.avd.verify_requirements:
+    dependencies:
+      - jinja2 >= 2.9
+      - ansible == 2.13
+  check_mode: False
+  run_once: True
+"""
