aaf266ea443a29d1980114998ce978d5b853bc52
gmuloc <gmulocher@arista.com>
2022-10-13 09:18:28 +0200

    Test: add some tests for bgp_encrypt filter

--- ansible_collections/arista/avd/tests/unit/filters/test_bgp_encrypt.py
+++ ansible_collections/arista/avd/tests/unit/filters/test_bgp_encrypt.py
@@ -23,6 +23,29 @@ INVALID_INPUT_DICT_DECRYPT = [
     {"key": "10.42.42.43", "password": "3QGcqpU2YTwKh2jVQ4Vj/A=="},
     {"key": "AVD-TEST-DUMMY", "passwowrd": "bM7t58t04qSqLHAfZR/Szg=="},
 ]
+# The following list uses all the molecule BGP passwords available
+# and the expected encryption
+# The password is always arista123
+MOLECULE_PASSWORDS_TEST = [
+    ({"key": "UNDERLAY-PEERS", "password": "arista123"}, "0nsCUm70mvSTxVO0ldytrg=="),
+    ({"key": "UNDERLAY_PEERS", "password": "arista123"}, "af6F4WLl4wUrWRZcwbEwkQ=="),
+    ({"key": "123.1.1.10", "password": "arista123"}, "oBztv71m2uhR7hh58/OCNA=="),
+    ({"key": "123.1.1.11", "password": "arista123"}, "oBztv71m2uhR7hh58/OCNA=="),
+    ({"key": "MPLS-IBGP-PEERS", "password": "arista123"}, "mWV4B6WpLCfOTyKATLWuBg=="),
+    ({"key": "EVPN-OVERLAY-RS-PEERS", "password": "arista123"}, "dRx9sULvl+hzkCMYJLEQCw=="),
+    ({"key": "EVPN-OVERLAY", "password": "arista123"}, "MY+KbyJy4kSu+X/blnVwsg=="),
+    ({"key": "IPV4-UNDERLAY", "password": "arista123"}, "dt5J2fw8tymeDFPyoYLB3w=="),
+    ({"key": "IPV6-UNDERLAY", "password": "arista123"}, "WkH9/oj4atEwv2MgOprY8A=="),
+    ({"key": "IPV6-UNDERLAY-MLAG", "password": "arista123"}, "CXS0NveSYzQRmm6SRGp42w=="),
+    ({"key": "IPV4-UNDERLAY-MLAG", "password": "arista123"}, "46jF9S9T7v5RRceVzhrlBg=="),
+    ({"key": "MPLS-OVERLAY-PEERS", "password": "arista123"}, "SHsTgDgjVUU5a9blyxSt3Q=="),
+    ({"key": "192.168.48.1", "password": "arista123"}, "toZKiUFLVUTU4hdS5V8F4Q=="),
+    ({"key": "192.168.48.3", "password": "arista123"}, "OajzUG59/YF0NkgvOQyRnQ=="),
+    ({"key": "MLAG-PEERS", "password": "arista123"}, "15AwQNBEJ1nyF/kBEtoAGw=="),
+    ({"key": "OVERLAY-PEERS", "password": "arista123"}, "64fqSH5CFUNLRHErezMrRg=="),
+    ({"key": "RR-OVERLAY-PEERS", "password": "arista123"}, "04FdfTXWrEfpDTUc3mlSjg=="),
+    ({"key": "MLAG_PEER", "password": "arista123"}, "arwUnrq9ydqIhjfTwRhAlg=="),
+]
 
 
 f = FilterModule()
@@ -54,8 +77,6 @@ def test_bgp_encrypt(input_dict, expected):
     """
     Test bgp_encrypt
     """
-    print(bgp_encrypt(input_dict))
-    print(expected)
     assert bgp_encrypt(input_dict) == expected
 
 
@@ -87,3 +108,11 @@ def test_bgp_encrypt_module():
     assert isinstance(resp, dict)
     assert "bgp_encrypt" in resp.keys()
     assert "bgp_decrypt" in resp.keys()
+
+
+@pytest.mark.parametrize("input_dict, expected", MOLECULE_PASSWORDS_TEST)
+def test_molucule_bgp_encrypt(input_dict, expected):
+    """
+    Test bgp_encrypt
+    """
+    assert bgp_encrypt(input_dict) == expected
