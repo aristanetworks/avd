64569b0e61f3c88db79aaaca2d04f5ee41ea1f6e
Claus Holbech <holbech@arista.com>
2023-02-27 10:42:42 +0100

    Remove urlopen

--- ansible_collections/arista/avd/plugins/plugin_utils/schema/refresolver.py
+++ ansible_collections/arista/avd/plugins/plugin_utils/schema/refresolver.py
@@ -1,7 +1,3 @@
-from urllib.request import urlopen
-
-from yaml import safe_load
-
 from ansible_collections.arista.avd.plugins.plugin_utils.errors import AristaAvdError
 
 try:
@@ -13,15 +9,6 @@ else:
     JSONSCHEMA_IMPORT_ERROR = None
 
 
-def yaml_file_loader(uri):
-    """
-    Load YAML file from URI
-    Used to resolve $ref with file:// style
-    """
-    with urlopen(uri) as data:
-        return safe_load(data.read().encode("UTF-8"))
-
-
 def create_refresolver(schema: dict, store: dict):
     if JSONSCHEMA_IMPORT_ERROR:
         raise AristaAvdError('Python library "jsonschema" must be installed to use this plugin') from JSONSCHEMA_IMPORT_ERROR
@@ -29,8 +16,5 @@ def create_refresolver(schema: dict, store: dict):
     return jsonschema.validators.RefResolver(
         base_uri=schema.get("$id", ""),
         referrer=schema,
-        handlers={
-            "file": yaml_file_loader,
-        },
         store=store,
     )
