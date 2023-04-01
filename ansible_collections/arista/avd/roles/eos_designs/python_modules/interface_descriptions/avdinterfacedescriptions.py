cd5fca8de6122fb5c9264dd100a104d8cae9140d
Claus Holbech <holbech@arista.com>
2023-04-01 22:52:15 +0200

    Refactor(eos_designs): Remove template data from avd_switch_facts

--- ansible_collections/arista/avd/roles/eos_designs/python_modules/interface_descriptions/avdinterfacedescriptions.py
+++ ansible_collections/arista/avd/roles/eos_designs/python_modules/interface_descriptions/avdinterfacedescriptions.py
@@ -1,11 +1,12 @@
 from collections import ChainMap
-from functools import cached_property
 
 from ansible_collections.arista.avd.plugins.plugin_utils.avdfacts import AvdFacts
 from ansible_collections.arista.avd.plugins.plugin_utils.utils import get
 
+from .utils import UtilsMixin
 
-class AvdInterfaceDescriptions(AvdFacts):
+
+class AvdInterfaceDescriptions(AvdFacts, UtilsMixin):
 
     """
     Class used to render Interface Descriptions either from custom Jinja2 templates or using default Python Logic
@@ -21,24 +22,8 @@ class AvdInterfaceDescriptions(AvdFacts):
         template_vars = ChainMap(kwargs, self._hostvars)
         return self.template_var(template_path, template_vars)
 
-    @cached_property
-    def _default_mpls_overlay_role(self) -> str:
-        return get(self._hostvars, "switch.default_mpls_overlay_role")
-
-    @cached_property
-    def _mpls_lsr(self) -> str:
-        return get(self._hostvars, "switch.mpls_lsr")
-
-    @cached_property
-    def _mlag_peer(self) -> str:
-        return get(self._hostvars, "switch.mlag_peer", required=True)
-
-    @cached_property
-    def _mlag_port_channel_id(self) -> str:
-        return get(self._hostvars, "switch.mlag_port_channel_id", required=True)
-
     def underlay_ethernet_interfaces(self, link_type: str, link_peer: str, link_peer_interface: str) -> str:
-        if template_path := get(self._hostvars, "switch.interface_descriptions.underlay_ethernet_interfaces"):
+        if template_path := self._interface_descriptions_templates.get("underlay_ethernet_interfaces"):
             return self._template(
                 template_path,
                 link={
@@ -63,10 +48,7 @@ class AvdInterfaceDescriptions(AvdFacts):
         link_peer_channel_group_id: int,
         link_channel_description: str,
     ) -> str:
-        if template_path := get(
-            self._hostvars,
-            "switch.interface_descriptions.underlay_port_channel_interfaces",
-        ):
+        if template_path := self._interface_descriptions_templates.get("underlay_port_channel_interfaces"):
             return self._template(
                 template_path,
                 link={
@@ -84,32 +66,26 @@ class AvdInterfaceDescriptions(AvdFacts):
         return f"{link_peer}_Po{link_peer_channel_group_id}"
 
     def mlag_ethernet_interfaces(self, mlag_interface: str) -> str:
-        if template_path := get(self._hostvars, "switch.interface_descriptions.mlag_ethernet_interfaces"):
+        if template_path := self._interface_descriptions_templates.get("mlag_ethernet_interfaces"):
             return self._template(template_path, mlag_interface=mlag_interface)
 
         return f"MLAG_PEER_{self._mlag_peer}_{mlag_interface}"
 
     def mlag_port_channel_interfaces(self) -> str:
-        if template_path := get(self._hostvars, "switch.interface_descriptions.mlag_port_channel_interfaces"):
+        if template_path := self._interface_descriptions_templates.get("mlag_port_channel_interfaces"):
             return self._template(template_path)
 
         return f"MLAG_PEER_{self._mlag_peer}_Po{self._mlag_port_channel_id}"
 
     def connected_endpoints_ethernet_interfaces(self, peer: str = None, peer_interface: str = None) -> str:
-        if template_path := get(
-            self._hostvars,
-            "switch.interface_descriptions.connected_endpoints_ethernet_interfaces",
-        ):
+        if template_path := self._interface_descriptions_templates.get("connected_endpoints_ethernet_interfaces"):
             return self._template(template_path, peer=peer, peer_interface=peer_interface)
 
         elements = [peer, peer_interface]
         return "_".join([str(element) for element in elements if element is not None])
 
     def connected_endpoints_port_channel_interfaces(self, peer: str = None, adapter_port_channel_description: str = None) -> str:
-        if template_path := get(
-            self._hostvars,
-            "switch.interface_descriptions.connected_endpoints_port_channel_interfaces",
-        ):
+        if template_path := self._interface_descriptions_templates.get("connected_endpoints_port_channel_interfaces"):
             return self._template(
                 template_path,
                 peer=peer,
@@ -120,7 +96,7 @@ class AvdInterfaceDescriptions(AvdFacts):
         return "_".join([str(element) for element in elements if element is not None])
 
     def overlay_loopback_interface(self, overlay_loopback_description: str = None) -> str:
-        if template_path := get(self._hostvars, "switch.interface_descriptions.overlay_loopback_interface"):
+        if template_path := self._interface_descriptions_templates.get("overlay_loopback_interface"):
             return self._template(template_path, overlay_loopback_description=overlay_loopback_description)
 
         if overlay_loopback_description is not None:
@@ -140,7 +116,7 @@ class AvdInterfaceDescriptions(AvdFacts):
         return "EVPN_Overlay_Peering"
 
     def vtep_loopback_interface(self) -> str:
-        if template_path := get(self._hostvars, "switch.interface_descriptions.vtep_loopback_interface"):
+        if template_path := self._interface_descriptions_templates.get("vtep_loopback_interface"):
             return self._template(template_path)
 
         return "VTEP_VXLAN_Tunnel_Source"
--- ansible_collections/arista/avd/roles/eos_designs/python_modules/interface_descriptions/load_interface_descriptions.py
+++ ansible_collections/arista/avd/roles/eos_designs/python_modules/interface_descriptions/load_interface_descriptions.py
@@ -1,6 +1,7 @@
-from ansible_collections.arista.avd.plugins.plugin_utils.utils import get, load_python_class
+from ansible_collections.arista.avd.plugins.plugin_utils.utils import load_python_class
 
 from .avdinterfacedescriptions import AvdInterfaceDescriptions
+from .utils import get_interface_descriptions_templates
 
 DEFAULT_AVD_INTERFACE_DESCRIPTIONS_PYTHON_MODULE = "ansible_collections.arista.avd.roles.eos_designs.python_modules.interface_descriptions"
 DEFAULT_AVD_INTERFACE_DESCRIPTIONS_PYTHON_CLASS_NAME = "AvdInterfaceDescriptions"
@@ -11,16 +12,10 @@ def load_interfacedescriptions(hostvars, templar) -> AvdInterfaceDescriptions:
     Load the python_module defined in `templates.interface_descriptions.python_module`
     Return the class defined by `templates.interface_descriptions.python_class_name`
     """
-    module_path = get(
-        hostvars,
-        "switch.interface_descriptions.python_module",
-        default=DEFAULT_AVD_INTERFACE_DESCRIPTIONS_PYTHON_MODULE,
-    )
-    class_name = get(
-        hostvars,
-        "switch.interface_descriptions.python_class_name",
-        default=DEFAULT_AVD_INTERFACE_DESCRIPTIONS_PYTHON_CLASS_NAME,
-    )
+    interface_descriptions_templates = get_interface_descriptions_templates(hostvars)
+
+    module_path = interface_descriptions_templates.get("python_module", DEFAULT_AVD_INTERFACE_DESCRIPTIONS_PYTHON_MODULE)
+    class_name = interface_descriptions_templates.get("python_class_name", DEFAULT_AVD_INTERFACE_DESCRIPTIONS_PYTHON_CLASS_NAME)
 
     cls = load_python_class(
         module_path,
