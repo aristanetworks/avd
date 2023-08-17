4fbb00da6d031874bd5b0ceb2b01d4484469f59f
Claus Holbech <holbech@arista.com>
2023-08-17 07:39:43 +0200

    insert licenses and rename custom build-backend

--- python-avd/tests/eos_designs/test_validate_structured_config.py
+++ python-avd/tests/eos_designs/test_validate_structured_config.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2023 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the LICENSE file.
 from pyavd import validate_structured_config
 from pyavd.avd_schema_tools import AvdSchemaTools
 from pyavd.vendor.errors import AvdValidationError
