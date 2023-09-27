d7ae13c13a6452c4b655a122f2c84df1aa8c1ec8
Claus Holbech <holbech@arista.com>
2023-09-27 11:30:56 +0200

    Read license from license-short.txt

--- python-avd/pyavd/schema/constants.py
+++ python-avd/pyavd/schema/constants.py
@@ -5,10 +5,9 @@ from pathlib import Path
 
 from yaml import safe_load
 
-EOS_DESIGNS_SCHEMA_PATH = Path(__file__).parents[3].joinpath("ansible_collections/arista/avd/roles/eos_designs/schemas/eos_designs.schema.yml")
-EOS_CLI_CONFIG_GEN_SCHEMA_PATH = (
-    Path(__file__).parents[3].joinpath("ansible_collections/arista/avd/roles/eos_cli_config_gen/schemas/eos_cli_config_gen.schema.yml")
-)
+REPO_ROOT = Path(__file__).parents[3]
+EOS_DESIGNS_SCHEMA_PATH = REPO_ROOT.joinpath("ansible_collections/arista/avd/roles/eos_designs/schemas/eos_designs.schema.yml")
+EOS_CLI_CONFIG_GEN_SCHEMA_PATH = REPO_ROOT.joinpath("ansible_collections/arista/avd/roles/eos_cli_config_gen/schemas/eos_cli_config_gen.schema.yml")
 
 with open(EOS_CLI_CONFIG_GEN_SCHEMA_PATH, encoding="UTF-8") as file:
     EOS_CLI_CONFIG_GEN_SCHEMA = safe_load(file)
@@ -25,3 +24,5 @@ SCHEMA_PATHS = {
     "eos_cli_config_gen": EOS_CLI_CONFIG_GEN_SCHEMA_PATH,
     "eos_designs": EOS_DESIGNS_SCHEMA_PATH,
 }
+
+LICENSE_HEADER = REPO_ROOT.joinpath("development/license-short.txt").read_text(encoding="UTF-8").strip()
--- python-avd/pyavd/schema/generate_docs/mdtabsgen.py
+++ python-avd/pyavd/schema/generate_docs/mdtabsgen.py
@@ -3,6 +3,7 @@
 # that can be found in the LICENSE file.
 from textwrap import indent
 
+from ..constants import LICENSE_HEADER
 from ..metaschema.meta_schema_model import AristaAvdSchema
 from .tablegen import get_table
 from .yamlgen import get_yaml
@@ -18,9 +19,7 @@ def get_md_tabs(schema: AristaAvdSchema, target_table: str | None = None) -> str
     """
     return f"""\
 <!--
-  ~ Copyright (c) 2023 Arista Networks, Inc.
-  ~ Use of this source code is governed by the Apache License 2.0
-  ~ that can be found in the LICENSE file.
+{indent(LICENSE_HEADER, "  ~ ")}
   -->
 === "Table"
 
