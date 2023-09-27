874fa506aadf8f771f98a1eba894b108e2c20a38
gmuloc <gmulocher@arista.com>
2023-09-27 20:16:43 +0200

    Refactor: Add support for 2.12 ansible

--- ansible_collections/arista/avd/plugins/action/eos_validate_state_runner.py
+++ ansible_collections/arista/avd/plugins/action/eos_validate_state_runner.py
@@ -8,6 +8,8 @@ __metaclass__ = type
 import logging
 
 from ansible.errors import AnsibleActionFail
+from ansible.module_utils.common.arg_spec import ArgumentSpecValidator, ValidationResult
+from ansible.module_utils.errors import UnsupportedError
 from ansible.plugins.action import ActionBase, display
 
 from ansible_collections.arista.avd.plugins.plugin_utils.eos_validate_state_utils import get_anta_results
@@ -25,15 +27,7 @@ class ActionModule(ActionBase):
         if task_vars is None:
             task_vars = {}
 
-        validation_result, new_module_args = self.validate_argument_spec(
-            argument_spec={
-                "logging_level": {"type": "str", "default": "WARNING"},
-                "save_catalog": {"type": "bool", "defaut": False},
-                "device_catalog_output_dir": {"type": "str"},
-                "skipped_tests": {"type": "dict"},
-            },
-            required_if=[("save_catalog", True, ["device_catalog_output_dir"])],
-        )
+        validation_result, new_module_args = self._validate_argument_spec()
 
         result = super().run(tmp, task_vars)
         del tmp  # tmp no longer has any effect
@@ -81,6 +75,48 @@ class ActionModule(ActionBase):
         result["results"] = anta_results
 
         return result
+
+    def _validate_argument_spec(self) -> [ValidationResult, dict]:
+        """
+        Helper to validate arguments as we still support Ansible from 2.12.6
+        and validate_argument_spec was introduced in 2.13
+        """
+        argument_spec = {
+            "logging_level": {"type": "str", "default": "WARNING"},
+            "save_catalog": {"type": "bool", "defaut": False},
+            "device_catalog_output_dir": {"type": "str"},
+            "skipped_tests": {"type": "dict"},
+        }
+        required_if = [("save_catalog", True, ["device_catalog_output_dir"])]
+
+        # TODO - refactor this when we bump ansible-core requirement and keep only the if branch
+
+        if hasattr(self, "validate_argument_spec"):
+            # Yay you are running ansible > 2.13 congrats!
+            validation_result, new_module_args = self.validate_argument_spec(argument_spec=argument_spec, required_if=required_if)
+        else:
+            # You should probably upgrade ansible if you are here ;)
+            new_module_args = self._task.args.copy()
+            validator = ArgumentSpecValidator(
+                argument_spec,
+                required_if=required_if,
+            )
+            validation_result = validator.validate(new_module_args)
+            new_module_args.update(validation_result.validated_parameters)
+            try:
+                error = validation_result.errors[0]
+            except IndexError:
+                error = None
+
+            # Fail for validation errors, even in check mode
+            if error:
+                msg = validation_result.errors.msg
+                if isinstance(error, UnsupportedError):
+                    msg = f"Unsupported parameters for ({self._load_name}) module: {msg}"
+
+                raise AnsibleActionFail(msg)
+
+        return validation_result, new_module_args
 
 
 def setup_module_logging(hostname: str, result: dict) -> None:
