ae4516eeb365c8ddc031a9418bf46da70a8ddab9
Claus Holbech <holbech@arista.com>
2023-11-21 18:53:17 +0100

    Fix(eos_cli_config_gen, eos_designs): BGP VRF Prefix-lists not allowed outside of AF (#3358)

--- ansible_collections/arista/avd/roles/eos_designs/python_modules/network_services/router_bgp.py
+++ ansible_collections/arista/avd/roles/eos_designs/python_modules/network_services/router_bgp.py
@@ -244,9 +244,20 @@ class RouterBgpMixin(UtilsMixin):
                             )
 
                 for bgp_peer in vrf["bgp_peers"]:
+                    # Below we pop various keys that are not supported by the eos_cli_config_gen schema.
+                    # The rest of the keys are relayed directly to eos_cli_config_gen.
+                    # 'ip_address' is popped even though it is supported. It will be added again later
+                    # to ensure it comes first in the generated dict.
                     peer_ip = bgp_peer.pop("ip_address")
                     address_family = f"address_family_ipv{ipaddress.ip_address(peer_ip).version}"
-                    neighbor = {"ip_address": peer_ip, "activate": True}
+                    neighbor = strip_empties_from_dict(
+                        {
+                            "ip_address": peer_ip,
+                            "activate": True,
+                            "prefix_list_in": bgp_peer.pop("prefix_list_in", None),
+                            "prefix_list_out": bgp_peer.pop("prefix_list_out", None),
+                        }
+                    )
                     bgp_vrf.setdefault(address_family, {}).setdefault("neighbors", []).append(neighbor)
 
                     if bgp_peer.get("set_ipv4_next_hop") is not None or bgp_peer.get("set_ipv6_next_hop") is not None:
