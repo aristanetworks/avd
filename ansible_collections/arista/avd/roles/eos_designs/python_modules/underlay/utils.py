661de9924c8d164b81bb90488a9df9853d613760
Joe Recchia <joerecchia@arista.com>
2024-01-15 10:23:08 -0500

    Cleaned up files to pass pre-commit

--- ansible_collections/arista/avd/roles/eos_designs/python_modules/underlay/utils.py
+++ ansible_collections/arista/avd/roles/eos_designs/python_modules/underlay/utils.py
@@ -4,16 +4,12 @@
 from __future__ import annotations
 
 from functools import cached_property
-from typing import TYPE_CHECKING
 
 from ansible_collections.arista.avd.plugins.filter.natural_sort import natural_sort
 from ansible_collections.arista.avd.plugins.plugin_utils.eos_designs_shared_utils.shared_utils import SharedUtils
 from ansible_collections.arista.avd.plugins.plugin_utils.strip_empties import strip_empties_from_dict
 from ansible_collections.arista.avd.plugins.plugin_utils.utils import get
-from ansible_collections.arista.avd.plugins.plugin_utils.errors import AristaAvdError
 
-if TYPE_CHECKING:
-    from ansible_collections.arista.avd.plugins.plugin_utils.eos_designs_facts import EosDesignsFacts
 
 class UtilsMixin:
     """
