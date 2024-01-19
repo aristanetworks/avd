5747735fb4566bb8401ebe63bbafbf509b7b46f4
bmeuer <bmeuer@arista.com>
2024-01-19 12:14:12 +0100

    remove spaces

--- ansible_collections/arista/avd/roles/eos_designs/python_modules/connected_endpoints/port_channel_interfaces.py
+++ ansible_collections/arista/avd/roles/eos_designs/python_modules/connected_endpoints/port_channel_interfaces.py
@@ -175,7 +175,7 @@ class PortChannelInterfacesMixin(UtilsMixin):
             port_channel_interface["mlag"] = channel_group_id
 
         # LACP Fallback
-        if port_channel_mode in ["active", "passive"] and             (lacp_fallback_mode := get(adapter, "port_channel.lacp_fallback.mode")) in ["static", "individual"]:
+        if port_channel_mode in ["active", "passive"] and (lacp_fallback_mode := get(adapter, "port_channel.lacp_fallback.mode")) in ["static", "individual"]:
             port_channel_interface.update(
                 {
                     "lacp_fallback_mode": lacp_fallback_mode,
