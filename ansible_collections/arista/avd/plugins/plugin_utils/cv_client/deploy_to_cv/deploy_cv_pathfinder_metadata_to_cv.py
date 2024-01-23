fa6da5ee982ac4cc77763f52c4a1ed3e8b918b2c
Claus Holbech <holbech@arista.com>
2024-01-23 15:34:47 +0100

    Temp for test

--- ansible_collections/arista/avd/plugins/plugin_utils/cv_client/deploy_to_cv/deploy_cv_pathfinder_metadata_to_cv.py
+++ ansible_collections/arista/avd/plugins/plugin_utils/cv_client/deploy_to_cv/deploy_cv_pathfinder_metadata_to_cv.py
@@ -12,7 +12,7 @@ from .models import CVDevice, CVPathfinderMetadata, DeployToCvResult
 
 LOGGER = getLogger(__name__)
 
-CV_PATHFINDER_METADATA_STUDIO_ID = "studio-caravan"
+CV_PATHFINDER_METADATA_STUDIO_ID = "a219112d-de90-42d3-abb0-796e0c4bde82"  # "studio-caravan"
 CV_PATHFINDER_DEFAULT_STUDIO_INPUTS = {"pathfinders": [], "pathgroups": [], "regions": [], "routers": [], "vrfs": [], "version": 3}
 
 
