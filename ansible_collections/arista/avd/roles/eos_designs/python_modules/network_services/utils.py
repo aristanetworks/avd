f0c398681e3a48cd0156d525fb3d8da935fecf2f
Guillaume Mulocher <gmulocher@arista.com>
2024-01-24 12:52:00 +0100

    Feat(eos_designs): Add support for virtual topologies constraints (#3535)
    Co-authored-by: Claus Holbech <holbech@arista.com>

--- ansible_collections/arista/avd/roles/eos_designs/python_modules/network_services/utils.py
+++ ansible_collections/arista/avd/roles/eos_designs/python_modules/network_services/utils.py
@@ -244,7 +244,7 @@ class UtilsMixin(UtilsFilteredTenantsMixin):
         * implement also the jitter / ...
         """
         wan_local_path_group_names = [path_group["name"] for path_group in self.shared_utils.wan_local_path_groups]
-        wan_load_balance_policy = {"name": name, "path_groups": []}
+        wan_load_balance_policy = {"name": name, "path_groups": [], **get(input_dict, "constraints", default={})}
 
         # An entry is composed of a list of path-groups in `names` and a `priority`
         policy_entries = get(input_dict, "path_groups", [])
