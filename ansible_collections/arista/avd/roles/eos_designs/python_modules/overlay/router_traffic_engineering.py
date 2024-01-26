0c9f0914a4288de9b9cf0769a875f75fff847aa3
gmuloc <gmulocher@arista.com>
2024-01-26 10:38:58 +0100

    Feat(eos_designs): Add router traffic-engineering for CV Pathfinder

---
+++ ansible_collections/arista/avd/roles/eos_designs/python_modules/overlay/router_traffic_engineering.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2023-2024 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the LICENSE file.
+from __future__ import annotations
+
+from functools import cached_property
+
+from ansible_collections.arista.avd.plugins.plugin_utils.strip_empties import strip_empties_from_dict
+from ansible_collections.arista.avd.plugins.plugin_utils.utils import get, get_item
+
+from .utils import UtilsMixin
+
+
+class RouterTrafficEngineering(UtilsMixin):
+    """
+    Mixin Class used to generate structured config for one key.
+    Class should only be used as Mixin to a AvdStructuredConfig class
+    """
+
+    @cached_property
+    def router_traffic_engineering(self) -> dict | None:
+        """
+        Return structured config for router traffic-engineering
+        """
+
+        if not self.shared_utils.cv_pathfinder_role:
+            return None
+
+        return {"enabled": True}
