5c81dad162d195b4d7227ed27cca20f52018db01
ayushmittal-arista <94678581+ayushmittal-arista@users.noreply.github.com>
2024-03-12 20:27:25 +0530

    Feat(eos_designs): Allow custom name for pathfinder Flow tracker tracker and exporter settings (#3721)
    Co-authored-by: gmuloc <gmulocher@arista.com>

--- ansible_collections/arista/avd/roles/eos_designs/python_modules/overlay/flow_tracking.py
+++ ansible_collections/arista/avd/roles/eos_designs/python_modules/overlay/flow_tracking.py
@@ -5,6 +5,8 @@ from __future__ import annotations
 
 from functools import cached_property
 
+from ansible_collections.arista.avd.plugins.plugin_utils.utils import get
+
 from .utils import UtilsMixin
 
 
@@ -18,19 +20,25 @@ class FlowTrackingMixin(UtilsMixin):
     def flow_tracking(self) -> dict | None:
         """
         Return structured config for flow_tracking
-
-        TODO: Make this configurable.
         """
         if not self.shared_utils.is_cv_pathfinder_router:
             return None
+
+        flow_tracking = get(self._hostvars, "flow_tracking_settings", default={})
+        exporter_name = get(flow_tracking, "exporter.name", default="CV-TELEMETRY")
+        template_interval = get(flow_tracking, "exporter.template_interval", default=3600000)
+        on_inactive_timeout = get(flow_tracking, "record_export.on_inactive_timeout", default=70000)
+        on_interval = get(flow_tracking, "record_export.on_interval", default=300000)
 
         return {
             "hardware": {
                 "trackers": [
                     {
                         "name": self.shared_utils.wan_flow_tracker_name,
-                        "record_export": {"on_inactive_timeout": 70000, "on_interval": 5000},
-                        "exporters": [{"name": "DPI-EXPORTER", "collector": {"host": "127.0.0.1"}, "local_interface": "Loopback0", "template_interval": 5000}],
+                        "record_export": {"on_inactive_timeout": on_inactive_timeout, "on_interval": on_interval},
+                        "exporters": [
+                            {"name": exporter_name, "collector": {"host": "127.0.0.1"}, "local_interface": "Loopback0", "template_interval": template_interval}
+                        ],
                     }
                 ],
                 "shutdown": False,
