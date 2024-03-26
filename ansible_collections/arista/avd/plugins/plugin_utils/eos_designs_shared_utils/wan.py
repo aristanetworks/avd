dcf6cd1e7b417d01c5fb1be19cc0bc20a14d8990
Claus Holbech <holbech@arista.com>
2024-03-26 10:37:31 +0100

    Refactor(eos_designs): Optional CV Pathfinder region for pathfinders (#3774)

--- ansible_collections/arista/avd/plugins/plugin_utils/eos_designs_shared_utils/wan.py
+++ ansible_collections/arista/avd/plugins/plugin_utils/eos_designs_shared_utils/wan.py
@@ -249,18 +249,23 @@ class WanMixin:
         )
 
     @cached_property
-    def wan_region(self: SharedUtils) -> dict:
+    def wan_region(self: SharedUtils) -> dict | None:
         """
         WAN region for CV Pathfinder
 
         Also checking if site names are unique across all regions.
+
+        The region is required for edges, but optional for pathfinders
         """
         node_defined_region = get(
             self.switch_data_combined,
             "cv_pathfinder_region",
-            required=True,
+            required=self.is_cv_pathfinder_client,
             org_key="A node variable 'cv_pathfinder_region' must be defined when 'wan_role' is 'client' and 'wan_mode' is 'cv-pathfinder'",
         )
+        if node_defined_region is None:
+            return None
+
         regions = get(
             self.hostvars, "cv_pathfinder_regions", required=True, org_key="'cv_pathfinder_regions' key must be set when 'wan_mode' is 'cv-pathfinder'."
         )
