7a84fc0f6ce3efa713142507174000cd1400f5ac
Frederik Holbech <frederik@holbech.dk>
2024-04-25 14:56:49 +0200

    Updated script. Added ignore_files argument

--- python-avd/scripts/find_missing_tables.py
+++ python-avd/scripts/find_missing_tables.py
@@ -1,7 +1,7 @@
 import argparse
 import glob
 import sys
-import pathlib
+from pathlib import Path
 """
 Help on find_missing_tables.py:
 
@@ -12,19 +12,21 @@ DESCRIPTION
     Checks if table files are added as snips in relevant role docs. Uses argparse to call arguments in CLI with 4 arguments:
         - Glob for table files -> glob
         - Glob for md files, ie. the role docs -> glob
-        - Start of relative path to role doc -> Default datatype of argparse arguments: str. For the use in the avd system, the argument: "roles", should be used as standard
-        - Ignored files. The list of table files that is to be ignored in the search -> list
+        - Start of relative path to role doc -> Path. For the use in the avd system, the argument: "ansible_collections/arista/avd", should be used as standard
+        - Ignored files. The table files that is to be ignored in the search -> Path
 
         Using these 4 arguments the script calls the functions check_files_in_markdown(table_files, md_files)
-        and returns the set with the table_files not covered in the role docs
+        and returns the table files not covered in the role docs
 
-        Can take any number of table_files and any number of role docs, and checks all md_files for all of the table files.
+        Can take any number of table_files, any number of role docs, one root path, any number of ignored files, and checks all role docs files for all of the table files.
 
-        Adds all table_files to a set, uses the function "make_path_relative" with the argument p__path_root to match the format of the paths from the table files
-        to the format of the table files in the role docs. The .find() returns the first instance of the argument in the path string,
-        so this solution even works for strings containing the directory name multiple times
+        checks role docs for the snip string, and adds the table files from the role docs to the md_file_set. 
 
-        checks role docs for the snip string, and adds the table_files to the md_file_set, containing all the table files from the role docs already, and then returns the missing table files
+        Adds all table files from the table_files_glob to a set, then reformats to match root_path, and adds to a new set. Discard then the ignored_files,
+        and compare to the set of table files found in the role docs.
+        Returns the difference set of the two, which is then looped through, printing all the missing files, excepting the ignored ones.
+
+        If set is empty, sys.exit()
 """
 
 parser = argparse.ArgumentParser()
@@ -39,32 +41,33 @@ parser.add_argument(
 parser.add_argument(
     "--markdown_files_glob",
     type=glob.glob,
+    dest="markdown_files",
     nargs="*",
     help="The glob for the markdown file",
 )
 parser.add_argument(
-    "--root-path",
+    "--root_path",
     dest="root_path",
     nargs="*",
-    type=pathlib.Path,
+    type=Path,
     help="The start of the relative path of the md files, ie. the place in the table path from where to cut down to match",
 )
-
+parser.add_argument(
+    "--ignore_files",
+    dest="ignore_files",
+    nargs="*",
+    type=Path,
+    help="The table files to be ignored, when checking for missing table files"
+)
 args = parser.parse_args()
 
-root_path = args.p__path_root[0]
+ignored_files = list((args.ignore_files))
 
-md_files = list(*args.m__md_file_glob)
+root_path = Path(*args.root_path)
+
+md_files = list(*args.markdown_files)
+
 table_files = set(*args.table_files)
-
-def make_path_relative(path: str):
-    index = path.find(f"{root_path}")
-    if index != -1:
-        relative_path = path[index:].replace("\\", "/")
-        return relative_path
-    else:
-        return path
-
 
 def check_files_in_markdown(table_files: set, md_files: list):
     md_file_set = set()
@@ -76,17 +79,19 @@ def check_files_in_markdown(table_files: set, md_files: list):
                 if line.strip() == "--8<--":
                     flag = not flag
                 elif flag:
-                    md_file_set.add(line.strip())
+                    md_file_set.add(Path(line.strip()))
 
-    for table_file in table_files:
-        table_files.discard(table_file)
-        table_files.add(make_path_relative(table_file))
-    return table_files.difference(md_file_set)
+    table_paths_set = {Path(table_file).relative_to(root_path) for table_file in table_files}
 
+    for ignored_file in ignored_files:
+        table_paths_set.discard(ignored_file)
 
-print("\n".join(check_files_in_markdown(table_files, md_files)))
+    return table_paths_set.difference(md_file_set)
 
-if len(check_files_in_markdown(table_files, md_files)) == 0:
+missing_files = check_files_in_markdown(table_files, md_files)
+if len(missing_files) == 0:
     sys.exit(0)
 else:
+    for missing_file in missing_files:
+        print(missing_file)
     sys.exit(1)
