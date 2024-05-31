766f2ce8fd9b7e31d7359aae90cc7a9ca1e35283
Claus Holbech <holbech@arista.com>
2024-05-31 11:42:09 +0200

    Disable sonarlint on lines containing accepted issues

--- python-avd/pyavd/templater.py
+++ python-avd/pyavd/templater.py
@@ -40,7 +40,8 @@ class Templar:
             ]
         )
 
-        self.environment = Environment(
+        # Accepting SonarLint issue: No autoescaping is ok, since we are not using this for a website, so XSS is not applicable.
+        self.environment = Environment(  # NOSONAR
             extensions=JINJA2_EXTENSIONS,
             loader=self.loader,
             undefined=Undefined,
