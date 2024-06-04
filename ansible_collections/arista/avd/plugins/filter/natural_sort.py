29718e86ca74740b9777fb813cb2b3125847a1d5
Shivani-gslab <schourasiya-ext@arista.com>
2024-06-04 15:59:05 +0530

    Refactor(plugins): Move jinja filter code for arista.avd.natural_sort to PyAVD

--- ansible_collections/arista/avd/plugins/filter/natural_sort.py
+++ ansible_collections/arista/avd/plugins/filter/natural_sort.py
@@ -8,10 +8,15 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-import re
+from ansible_collections.arista.avd.plugins.plugin_utils.pyavd_wrappers import wrap_filter
 
-from jinja2.runtime import Undefined
-from jinja2.utils import Namespace
+try:
+    from pyavd.j2filters.natural_sort import natural_sort
+
+    PYAVD_IMPORT_EXCEPTION = None
+except ImportError as e:
+    natural_sort = None
+    PYAVD_IMPORT_EXCEPTION = e
 
 DOCUMENTATION = r"""
 ---
@@ -55,27 +60,8 @@ _value:
 """
 
 
-def convert(text):
-    return int(text) if text.isdigit() else text.lower()
-
-
-def natural_sort(iterable, sort_key=None):
-    if isinstance(iterable, Undefined) or iterable is None:
-        return []
-
-    def alphanum_key(key):
-        if sort_key is not None and isinstance(key, dict):
-            return [convert(c) for c in re.split("([0-9]+)", str(key.get(sort_key, key)))]
-        elif sort_key is not None and isinstance(key, Namespace):
-            return [convert(c) for c in re.split("([0-9]+)", getattr(key, sort_key))]
-        else:
-            return [convert(c) for c in re.split("([0-9]+)", str(key))]
-
-    return sorted(iterable, key=alphanum_key)
-
-
 class FilterModule(object):
     def filters(self):
         return {
-            "natural_sort": natural_sort,
+            "natural_sort": wrap_filter("arista.avd.natural_sort", PYAVD_IMPORT_EXCEPTION)(natural_sort),
         }
--- python-avd/tests/pyavd/j2filters/test_natural_sort.py
+++ python-avd/tests/pyavd/j2filters/test_natural_sort.py
@@ -7,12 +7,9 @@ __metaclass__ = type
 
 import pytest
 from jinja2.runtime import Undefined
-
-from ansible_collections.arista.avd.plugins.filter.natural_sort import FilterModule, convert
-from ansible_collections.arista.avd.plugins.filter.natural_sort import natural_sort as _natural_sort
-from ansible_collections.arista.avd.tests.unit.filters.filter_utils import natural_sort
-
-f = FilterModule()
+from natsort import os_sorted
+from pyavd.j2filters.natural_sort import convert
+from pyavd.j2filters.natural_sort import natural_sort as _natural_sort
 
 STRINGS_VALID = ["100", "200", "ABC"]
 ITEMS_TO_NATURAL_SORT = [None, [], {}, "", ["1,2,3,4", "11,2,3,4", "5.6.7.8"], {"a1": 123, "a2": 2, "a10": 333, "a11": 4456}]
@@ -28,15 +25,10 @@ class TestNaturalSortFilter:
             assert resp == STRING_VALID.lower()
 
     @pytest.mark.parametrize("ITEM_TO_NATURAL_SORT", ITEMS_TO_NATURAL_SORT)
-    def test_natural_sort_invalid(self, ITEM_TO_NATURAL_SORT):
+    def test_natural_sort(self, ITEM_TO_NATURAL_SORT):
         resp = _natural_sort(ITEM_TO_NATURAL_SORT)
         if ITEM_TO_NATURAL_SORT is None or isinstance(ITEM_TO_NATURAL_SORT, Undefined):
             resp == []
         else:
-            resp_natsort = natural_sort(ITEM_TO_NATURAL_SORT)
+            resp_natsort = os_sorted(ITEM_TO_NATURAL_SORT)
             assert resp == resp_natsort
-
-    def test_natural_sort_filter(self):
-        resp = f.filters()
-        assert isinstance(resp, dict)
-        assert "natural_sort" in resp.keys()
