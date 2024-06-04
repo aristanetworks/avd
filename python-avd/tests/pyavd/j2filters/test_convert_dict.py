734ef8bb2ab11f17651c157ebb129a1cb6a2a61c
Vibhu-gslab <vibhu.tripathi-ext@arista.com>
2024-06-04 17:53:14 +0530

    adding unit test

--- python-avd/tests/pyavd/j2filters/test_convert_dict.py
+++ python-avd/tests/pyavd/j2filters/test_convert_dict.py
@@ -5,7 +5,8 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-from ansible_collections.arista.avd.plugins.filter.convert_dicts import FilterModule, convert_dicts
+from ansible_collections.arista.avd.plugins.filter.convert_dicts import FilterModule
+from pyavd.j2filters.default import convert_dicts
 
 nested_list_of_dict = {
     "TEST1": [{"type": "permit", "extcommunities": "65000:65000"}, {"type": "deny", "extcommunities": "65002:65002"}],
