78e867143c1ee08e948f7ea927a3d2b85e9a7701
Shivani-gslab <schourasiya-ext@arista.com>
2024-06-04 19:00:39 +0530

    Updating tests

--- python-avd/tests/pyavd/j2filters/test_natural_sort.py
+++ python-avd/tests/pyavd/j2filters/test_natural_sort.py
@@ -6,17 +6,11 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 import pytest
-from jinja2.runtime import Undefined
-from natsort import os_sorted
-from pyavd.j2filters.natural_sort import convert
-from pyavd.j2filters.natural_sort import natural_sort as _natural_sort
-
-STRINGS_VALID = ["100", "200", "ABC"]
-ITEMS_TO_NATURAL_SORT = [None, [], {}, "", ["1,2,3,4", "11,2,3,4", "5.6.7.8"], {"a1": 123, "a2": 2, "a10": 333, "a11": 4456}]
+from pyavd.j2filters.natural_sort import convert, natural_sort
 
 
 class TestNaturalSortFilter:
-    @pytest.mark.parametrize("STRING_VALID", STRINGS_VALID)
+    @pytest.mark.parametrize("STRING_VALID", ["100", "200", "ABC"])
     def test_convert_function(self, STRING_VALID):
         resp = convert(STRING_VALID)
         if STRING_VALID.isdigit():
@@ -24,11 +18,17 @@ class TestNaturalSortFilter:
         else:
             assert resp == STRING_VALID.lower()
 
-    @pytest.mark.parametrize("ITEM_TO_NATURAL_SORT", ITEMS_TO_NATURAL_SORT)
-    def test_natural_sort(self, ITEM_TO_NATURAL_SORT):
-        resp = _natural_sort(ITEM_TO_NATURAL_SORT)
-        if ITEM_TO_NATURAL_SORT is None or isinstance(ITEM_TO_NATURAL_SORT, Undefined):
-            resp == []
-        else:
-            resp_natsort = os_sorted(ITEM_TO_NATURAL_SORT)
-            assert resp == resp_natsort
+    @pytest.mark.parametrize(
+        "ITEM_TO_NATURAL_SORT, EXPECTED_OUTPUT",
+        [
+            (None, []),
+            ([], []),
+            ({}, []),
+            ("", []),
+            (["1,2,3,4", "11,2,3,4", "5.6.7.8"], ["1,2,3,4", "5.6.7.8", "11,2,3,4"]),
+            ({"a1": 123, "a10": 333, "a2": 2, "a11": 4456}, ["a1", "a2", "a10", "a11"]),
+        ],
+    )
+    def test_natural_sort(self, ITEM_TO_NATURAL_SORT, EXPECTED_OUTPUT):
+        resp = natural_sort(ITEM_TO_NATURAL_SORT)
+        assert resp == EXPECTED_OUTPUT
