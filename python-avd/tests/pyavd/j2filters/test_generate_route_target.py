4e6db6de5d429b935b44016d99e3330ab1d27020
Vibhu-gslab <vibhu.tripathi-ext@arista.com>
2024-06-12 13:21:08 +0530

    fixing comments

--- python-avd/tests/pyavd/j2filters/test_generate_route_target.py
+++ python-avd/tests/pyavd/j2filters/test_generate_route_target.py
@@ -8,9 +8,14 @@ __metaclass__ = type
 import pytest
 from pyavd.j2filters.generate_route_target import generate_route_target
 
+ESI_SHORT = "0303:0202:0101"
+ESI_SHORT_STR = "ESI_SHORT"
+ESI_SHORT_NONE = None
+ESI_SHORT_EMPTY_STR = ""
+ESI_SHORT_SINGLE_DIGIT = "3"
 
 class TestEsiManagementFilter:
-    @pytest.mark.parametrize("esi_short, route_target", [("0303:0202:0101", "03:03:02:02:01:01"), (None, None), ("ESI_SHORT", "ES:I_:SH:OR"), ("", "")])
+    @pytest.mark.parametrize("esi_short, route_target", [(ESI_SHORT, "03:03:02:02:01:01"), (ESI_SHORT_NONE, None), (ESI_SHORT_STR, "ES:I_:SH:OR"), (ESI_SHORT_EMPTY_STR, ""), (ESI_SHORT_SINGLE_DIGIT, "")])
     def test_generate_route_target(self, esi_short, route_target):
         resp = generate_route_target(esi_short)
         assert resp == route_target
