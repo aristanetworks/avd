5d3fe00b142c670c886a6263c1ad7fccc280aaea
Claus Holbech <holbech@arista.com>
2024-06-14 16:10:02 +0200

    fix generate_Esi

--- python-avd/pyavd/j2filters/generate_esi.py
+++ python-avd/pyavd/j2filters/generate_esi.py
@@ -1,11 +1,6 @@
 # Copyright (c) 2024 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
-from __future__ import absolute_import, division, print_function
-
-__metaclass__ = type
-
-
 def generate_esi(short_esi: str, esi_prefix: str = "0000:0000:") -> str:
     """
     Transforms short_esi to EVPN ESI format.
