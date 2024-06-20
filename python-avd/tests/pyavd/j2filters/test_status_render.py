97a66379ec8a0ed65641dd1ec197e4f48a97e359
Claus Holbech <holbech@arista.com>
2024-06-20 14:46:02 +0200

    Update python-avd/tests/pyavd/j2filters/test_status_render.py
    Co-authored-by: Mahesh Kumar <122076792+MaheshGSLAB@users.noreply.github.com>

--- python-avd/tests/pyavd/j2filters/test_status_render.py
+++ python-avd/tests/pyavd/j2filters/test_status_render.py
@@ -1,9 +1,7 @@
 # Copyright (c) 2023-2024 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
-from __future__ import absolute_import, division, print_function
-
-__metaclass__ = type
+from __future__ import annotations
 
 import pytest
 from pyavd.j2filters.status_render import status_render
