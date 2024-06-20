61a0cfc2d549350788e75e36fe8f0203db1f749b
Mahesh Kumar <mahesh.kumar-ext@arista.com>
2024-06-20 00:53:46 -0700

    fix the import error

--- ansible_collections/arista/avd/plugins/plugin_utils/cv_client/workflows/deploy_cv_pathfinder_metadata_to_cv.py
+++ ansible_collections/arista/avd/plugins/plugin_utils/cv_client/workflows/deploy_cv_pathfinder_metadata_to_cv.py
@@ -6,7 +6,8 @@ from __future__ import annotations
 from copy import deepcopy
 from logging import getLogger
 
-from ....filter.decrypt import simple_7_decrypt
+from ansible_collections.arista.avd.plugins.filter.decrypt import simple_7_decrypt
+
 from ...utils.get import get, get_v2
 from ..api.arista.studio.v1 import InputSchema
 from ..client import CVClient
