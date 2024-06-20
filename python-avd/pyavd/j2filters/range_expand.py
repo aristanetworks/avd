5ea0fbbe966e3b8bfc5dfe9b5bfd2cc816108477
Laxmikant Chintakindi <laxmikant.c-ext@Laxmikants-MacBook-Pro.local>
2024-06-20 15:58:21 +0530

    Ignoring pylint.

--- python-avd/pyavd/j2filters/range_expand.py
+++ python-avd/pyavd/j2filters/range_expand.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2024 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
+# pylint: disable=cell-var-from-loop
 from __future__ import annotations
 
 import re
