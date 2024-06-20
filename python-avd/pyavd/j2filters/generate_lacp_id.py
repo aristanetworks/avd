19d66f93edb5dad53b10830481c6edcdb5819834
Shivani-gslab <schourasiya-ext@arista.com>
2024-06-20 08:59:32 +0530

    Refactor(plugins): Move jinja filter code for arista.avd.generate_lacp_id to PyAVD

---
+++ python-avd/pyavd/j2filters/generate_lacp_id.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2023-2024 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the LICENSE file.
+from __future__ import annotations
+
+
+def generate_lacp_id(esi_short: str):
+    """
+    Transforms short_esi `0303:0202:0101` to LACP ID format `0303.0202.0101`
+
+    Args:
+        esi_short (str): Short ESI value as per AVD definition in eos_designs
+    Returns:
+        str: LACP ID
+
+    """
+    if isinstance(esi_short, str):
+        return esi_short.replace(":", ".")
---
+++ python-avd/tests/pyavd/j2filters/test_generate_lacp_id.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2023-2024 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the LICENSE file.
+import pytest
+from pyavd.j2filters.generate_lacp_id import generate_lacp_id
+
+ESI_SHORT_INPUT = [
+    ("0404:0202:0101", "0404.0202.0101"),
+    ("000:000:0303:0202:0101", "000.000.0303.0202.0101")]
+
+
+class TestGenerateLacpIdFilter:
+
+    @pytest.mark.parametrize("esi_short, lacp_id", ESI_SHORT_INPUT)
+    def test_generate_lacp_id(self, esi_short, lacp_id):
+        assert esi_short is not None and esi_short != ""
+        resp = generate_lacp_id(esi_short)
+        assert resp == lacp_id
