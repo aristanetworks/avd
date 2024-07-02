d92d3d9c264c532d00e804a82a64dee83d681466
gmuloc <gmulocher@arista.com>
2024-07-02 09:35:21 +0200

    Doc: Maybe fix ansible-lint

--- ansible_collections/arista/avd/plugins/action/emit_warning.py
+++ ansible_collections/arista/avd/plugins/action/emit_warning.py
@@ -10,6 +10,32 @@ display = Display()
 
 __metaclass__ = type
 
+DOCUMENTATION = r"""
+---
+module: emit_warning
+version_added: "4.9.0"
+author: EMEA AS Team (@aristanetworks)
+short_description: Emit a warning in a task.
+description:
+  - Emit a warning in a task.
+options:
+  message:
+    description: The warning message to emit.
+    type: str
+"""
+
+EXAMPLES = r"""
+---
+- name: Enit a warning
+  arista.avd.emit_warning:
+    message |-
+      The warning message.
+  delegate_to: localhost
+  check_mode: false
+  run_once: true
+  changed_when: false
+"""
+
 
 class ActionModule(ActionBase):
     def run(self, tmp=None, task_vars=None):
