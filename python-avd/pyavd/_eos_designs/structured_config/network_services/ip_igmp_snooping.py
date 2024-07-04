d93fb7899b61273e17e1dde60d770b1cb426586d
Shivani-gslab <schourasiya-ext@arista.com>
2024-07-04 13:04:29 +0530

    Feat(eos_designs): add option for ip igmp snooping fast-leave

--- python-avd/pyavd/_eos_designs/structured_config/network_services/ip_igmp_snooping.py
+++ python-avd/pyavd/_eos_designs/structured_config/network_services/ip_igmp_snooping.py
@@ -114,6 +114,14 @@ class IpIgmpSnoopingMixin(UtilsMixin):
             if version is not None:
                 ip_igmp_snooping_vlan["querier"]["version"] = version
 
+            fast_leave = default(
+                igmp_snooping_querier.get("fast_leave"),
+                tenant_igmp_snooping_querier.get("fast_leave"),
+                get(tenant, "evpn_l2_multicast.fast_leave"),
+            )
+            if fast_leave is not None:
+                ip_igmp_snooping_vlan["fast_leave"] = fast_leave
+
         if ip_igmp_snooping_vlan:
             return {"id": int(vlan["id"]), **ip_igmp_snooping_vlan}
 
