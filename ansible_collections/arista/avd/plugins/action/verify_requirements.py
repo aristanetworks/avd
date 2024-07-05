70ae53bb2998d41f1a0670e63db15801ccc9c69c
Guillaume Mulocher <gmulocher@arista.com>
2024-07-05 15:38:11 +0200

    Refactor: Deprecate Python3.9 support (#4177)

--- ansible_collections/arista/avd/plugins/action/verify_requirements.py
+++ ansible_collections/arista/avd/plugins/action/verify_requirements.py
@@ -54,16 +54,20 @@ def _validate_python_version(info: dict, result: dict) -> bool:
         display.error(f"Python Version running {running_version} - Minimum Version required is {min_version}", False)
         return False
     # Keeping this for next deprecation adjust the message as required
-    # elif sys.version_info[:2] == MIN_PYTHON_SUPPORTED_VERSION:
-    #     result.setdefault("deprecations", []).append(
-    #         {
-    #             "msg": (
-    #                 f"You are currently running Python {running_version}. The next minor release of AVD after November 6th 2023 will drop support for Python"
-    #                 f" {min_version} as it will be dropping support for ansible-core<2.14 and ansible-core>=2.14 does not support Python {min_version} as"
-    #                 " documented here: https://docs.ansible.com/ansible/latest/reference_appendices/release_and_maintenance.html#ansible-core-support-matrix"
-    #             )
-    #         }
-    #     )
+    elif sys.version_info[:2] == MIN_PYTHON_SUPPORTED_VERSION:
+        result.setdefault("deprecations", []).append(
+            {
+                "msg": (
+                    f"You are currently running Python version {running_version}. "
+                    "AVD version 5.0.0 will drop support for Python version {min_version}. "
+                    "The decision has been taken to remove Python version 3.9 support in AVD "
+                    "collection to anticipate its removal in `ansible-core`. `ansible-core` "
+                    "version 2.15 End-Of-Life is scheduled for November 2024 and it will be the "
+                    "last `ansible-core` version supporting Python version 3.9 as documented here: "
+                    "https://docs.ansible.com/ansible/latest/reference_appendices/release_and_maintenance.html#ansible-core-support-matrix."
+                )
+            }
+        )
 
     return True
 
