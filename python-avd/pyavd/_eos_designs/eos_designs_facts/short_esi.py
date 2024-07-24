f589f2cb435230c642ff4fd088187b6f2724d526
Vibhu-gslab <vibhu.tripathi-ext@arista.com>
2024-07-24 15:10:01 +0530

    moving logic

--- python-avd/pyavd/_eos_designs/eos_designs_facts/short_esi.py
+++ python-avd/pyavd/_eos_designs/eos_designs_facts/short_esi.py
@@ -27,12 +27,12 @@ class ShortEsiMixin:
         If short_esi is set to "auto" we will use sha256 to create a
         unique short_esi value based on various uplink information.
         """
+        if self.shared_utils.mlag_role == "secondary":
+            # On the MLAG Secondary use short-esi from MLAG primary
+            if (peer_short_esi := self.shared_utils.mlag_peer_facts._short_esi) is not None:
+                return peer_short_esi
         short_esi = get(self.shared_utils.switch_data_combined, "short_esi")
         if short_esi == "auto":
-            if self.shared_utils.mlag_role == "secondary":
-                # On the MLAG Secondary use short-esi from MLAG primary
-                if (peer_short_esi := self.shared_utils.mlag_peer_facts._short_esi) is not None:
-                    return peer_short_esi
             esi_seed_1 = "".join(self.shared_utils.uplink_switches[:2])
             esi_seed_2 = "".join(self.shared_utils.uplink_switch_interfaces[:2])
             esi_seed_3 = "".join(default(self.shared_utils.uplink_interfaces, [])[:2])
