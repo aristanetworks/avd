98df162984d8ce3ed461bcda206ae58814417db3
Claus Holbech <holbech@arista.com>
2024-08-08 12:49:35 +0200

    Move interface description classes to pyavd.api

--- python-avd/pyavd/_eos_designs/structured_config/underlay/loopback_interfaces.py
+++ python-avd/pyavd/_eos_designs/structured_config/underlay/loopback_interfaces.py
@@ -6,9 +6,9 @@ from __future__ import annotations
 from functools import cached_property
 from typing import TYPE_CHECKING
 
-from pyavd._eos_designs.interface_descriptions.models import InterfaceDescriptionData
 from pyavd._errors import AristaAvdMissingVariableError
 from pyavd._utils import get
+from pyavd.api.interface_descriptions import InterfaceDescriptionData
 
 from .utils import UtilsMixin
 
