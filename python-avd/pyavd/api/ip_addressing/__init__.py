b08a5754836fcd767c7155d73b6bf8a7bc0c2eaa
Vibhu-gslab <vibhu.tripathi-ext@arista.com>
2024-09-10 17:31:20 +0530

    Refactor(eos_designs): Use VRF ID instead of VRF VNI as offset for evpn underlay l3 multicast group exit

--- python-avd/pyavd/api/ip_addressing/__init__.py
+++ python-avd/pyavd/api/ip_addressing/__init__.py
@@ -314,8 +314,8 @@ class AvdIpAddressing(AvdFacts, UtilsMixin):
     def evpn_underlay_l3_multicast_group(
         self,
         underlay_l3_multicast_group_ipv4_pool: str,
-        vrf_vni: int,
-        vrf_id: int,  # pylint: disable=unused-argument # noqa: ARG002
+        vrf_vni: int,  # pylint: disable=unused-argument # noqa: ARG002
+        vrf_id: int,
         evpn_underlay_l3_multicast_group_ipv4_pool_offset: int,
     ) -> str:
         """
@@ -323,7 +323,7 @@ class AvdIpAddressing(AvdFacts, UtilsMixin):
 
         TODO: Change algorithm to use VRF ID instead of VRF VNI as offset.
         """
-        offset = vrf_vni - 1 + evpn_underlay_l3_multicast_group_ipv4_pool_offset
+        offset = vrf_id - 1 + evpn_underlay_l3_multicast_group_ipv4_pool_offset
         return get_ip_from_pool(underlay_l3_multicast_group_ipv4_pool, 32, offset, 0)
 
     def evpn_underlay_l2_multicast_group(
