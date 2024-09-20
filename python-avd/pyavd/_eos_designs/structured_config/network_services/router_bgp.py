b6d09cf069bf0d54eb8c80df9f203e054df57de3
Laxmikant Chintakindi <laxmikant.c-ext@Laxmikants-MacBook-Pro.local>
2024-09-20 22:41:36 +0530

    Fix CI.

--- python-avd/pyavd/_eos_designs/structured_config/network_services/router_bgp.py
+++ python-avd/pyavd/_eos_designs/structured_config/network_services/router_bgp.py
@@ -168,7 +168,7 @@ class RouterBgpMixin(UtilsMixin):
                 bgp_vrf_redistribute_connected = get(vrf, "redistribute_connected", default=True)
                 if vrf_name != "default":
                     # Non-default VRF
-                    bgp_vrf["router_id] = self.shared_utils.router_id
+                    bgp_vrf["router_id"] = self.shared_utils.router_id
                     if bgp_vrf_redistribute_connected is True:
                         bgp_vrf["redistribute_routes"] = [{"source_protocol": "connected"}]
                     # Redistribution of static routes for VRF default are handled elsewhere
