48b14bd8a4211a008a5ff7493da74e8880dd76ad
laxmikantchintakindi <159624484+laxmikantchintakindi@users.noreply.github.com>
2024-09-20 22:26:12 +0530

    Update python-avd/pyavd/_eos_designs/structured_config/network_services/router_bgp.py
    Co-authored-by: Guillaume Mulocher <gmulocher@arista.com>

--- python-avd/pyavd/_eos_designs/structured_config/network_services/router_bgp.py
+++ python-avd/pyavd/_eos_designs/structured_config/network_services/router_bgp.py
@@ -170,7 +170,7 @@ class RouterBgpMixin(UtilsMixin):
                     # Non-default VRF
                     bgp_vrf["router_id] = self.shared_utils.router_id
                     if bgp_vrf_redistribute_connected is True:
-                        bgp_vrf |= {"redistribute_routes": [{"source_protocol": "connected"}]}
+                        bgp_vrf["redistribute_routes"] = [{"source_protocol": "connected"}]
                     # Redistribution of static routes for VRF default are handled elsewhere
                     # since there is a choice between redistributing to underlay or overlay.
                     if (bgp_vrf_redistribute_static := vrf.get("redistribute_static")) is True or (
