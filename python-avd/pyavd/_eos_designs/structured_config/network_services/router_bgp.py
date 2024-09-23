b92ade35ed3592433c4cdf9531113d6fb070ff9b
Vibhu-gslab <vibhu.tripathi-ext@arista.com>
2024-09-23 16:54:57 +0530

    fixing bug

--- python-avd/pyavd/_eos_designs/structured_config/network_services/router_bgp.py
+++ python-avd/pyavd/_eos_designs/structured_config/network_services/router_bgp.py
@@ -318,7 +318,10 @@ class RouterBgpMixin(UtilsMixin):
     def _update_router_bgp_vrf_mlag_neighbor_cfg(self: AvdStructuredConfigNetworkServices, bgp_vrf: dict, vrf: dict, tenant: dict, vlan_id: int) -> None:
         """In-place update MLAG neighbor part of structured config for *one* VRF under router_bgp.vrfs."""
         if not self._mlag_ibgp_peering_redistribute(vrf, tenant):
-            bgp_vrf["redistribute_routes"][0]["route_map"] = "RM-CONN-2-BGP-VRFS"
+            try:
+                bgp_vrf["redistribute_routes"][0]["route_map"] = "RM-CONN-2-BGP-VRFS"
+            except KeyError:
+                bgp_vrf.setdefault("redistribute_routes", []).append({"source_protocol": "connected", "route_map": "RM-CONN-2-BGP-VRFS"})
 
         if self.shared_utils.underlay_rfc5549 and self.shared_utils.overlay_mlag_rfc5549:
             interface_name = f"Vlan{vlan_id}"
