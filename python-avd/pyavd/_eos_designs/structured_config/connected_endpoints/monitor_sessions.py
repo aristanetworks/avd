4920a468cc06a394e1f79b377a57ceb9f38dc768
Laxmikant Chintakindi <laxmikant.c-ext@Laxmikants-MacBook-Pro.local>
2024-10-10 14:03:39 +0530

    Fix CI.

--- python-avd/pyavd/_eos_designs/structured_config/connected_endpoints/monitor_sessions.py
+++ python-avd/pyavd/_eos_designs/structured_config/connected_endpoints/monitor_sessions.py
@@ -50,7 +50,10 @@ class MonitorSessionsMixin(UtilsMixin):
 
                 if get(merged_settings, "session_settings.access_group") is not None:
                     if get(session, "source_settings.access_group") is not None:
-                        msg = f"For {self.shared_utils.hostname}, an ACL is set on {session['interface']} even though ACL is set for monitor session {session_name}."
+                        msg = (
+                            f"For {self.shared_utils.hostname}, an ACL is set on {session['interface']}"
+                            f"even though ACL is set for monitor session {session_name}."
+                        )
                         raise AristaAvdError(msg)
                 elif (access_group := get(session, "source_settings.access_group")) is not None:
                     source["access_group"] = {
