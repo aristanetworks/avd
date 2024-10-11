5640c82df37206584bbd0cc10908cc7d9ff58ae1
Laxmikant Chintakindi <laxmikant.c-ext@Laxmikants-MacBook-Pro.local>
2024-10-11 17:01:13 +0530

    Fix

--- python-avd/pyavd/_eos_designs/structured_config/connected_endpoints/monitor_sessions.py
+++ python-avd/pyavd/_eos_designs/structured_config/connected_endpoints/monitor_sessions.py
@@ -51,8 +51,8 @@ class MonitorSessionsMixin(UtilsMixin):
                 if get(merged_settings, "session_settings.access_group") is not None:
                     if get(session, "source_settings.access_group") is not None:
                         msg = (
-                            f"For {self.shared_utils.hostname}, an ACL is set on {session['interface']}"
-                            f" even though ACL is set for monitor session {session_name} in connected endpoints settings."
+                            f"Can not set ACL for {self.shared_utils.hostname} on interface {session['interface']} since it is already"
+                            f" present on monitor session {session_name} in connected endpoints settings."
                         )
                         raise AristaAvdError(msg)
                 elif (access_group := get(session, "source_settings.access_group")) is not None:
