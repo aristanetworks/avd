47370c538eebd1d791ca8db96ce45c136cc6ccc6
Claus Holbech <holbech@arista.com>
2025-03-05 07:16:51 +0100

    remove changes to pyavd

--- python-avd/tests/avdutils/validation/conftest.py
+++ python-avd/tests/avdutils/validation/conftest.py
@@ -1,6 +1,13 @@
 # Copyright (c) 2025 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
-from pyavd._schema.init_store import init_store
+from pathlib import Path
+from typing import cast
 
-init_store()
+from avdutils._validation import init_store_from_fragments
+from schema_tools.constants import SCHEMAS
+
+init_store_from_fragments(
+    eos_cli_config_gen=cast(Path, SCHEMAS["eos_cli_config_gen"].fragments_dir),
+    eos_designs=cast(Path, SCHEMAS["eos_designs"].fragments_dir),
+)
