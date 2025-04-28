619de78843b435d90928beb4f594efde6b816c81
Alexey Gorbunov <ag.justice@gmail.com>
2025-04-28 10:33:45 -0700

    Address PR review comments

--- python-avd/pyavd/_cv/client/workspace.py
+++ python-avd/pyavd/_cv/client/workspace.py
@@ -225,10 +225,11 @@ class WorkspaceMixin(Protocol):
         """
         Monitor a Workspace using arista.workspace.v1.WorkspaceService.Subscribe API for a response to the given request_id.
 
-        Blocks until a response is returned or timed out.
+        Blocks until a response in a terminal state (ResponseStatus.SUCCESS or ResponseStatus.FAIL) is returned or timed out.
+        Responses in an intermediate state (ResponseStatus.UNSPECIFIED) are logged only.
 
         Parameters:
-            workspace_id: Unique identifier the Workspace.
+            workspace_id: Unique identifier for the Workspace.
             request_id: Unique identifier for the Request.
             timeout: Timeout in seconds for the Workspace to build.
 
@@ -247,12 +248,7 @@ class WorkspaceMixin(Protocol):
             responses = client.subscribe(request, metadata=self._metadata, timeout=timeout)
             async for response in responses:
                 if request_id in response.value.responses.values:
-                    LOGGER.info(
-                        "wait_for_workspace_response: Got response for request '%s' at '%s' UTC: %s",
-                        request_id,
-                        response.time,
-                        response.value.responses.values[request_id],
-                    )
+                    LOGGER.info("wait_for_workspace_response: Got response for request '%s': %s", request_id, response.value.responses.values[request_id])
                     if response.value.responses.values[request_id].status != ResponseStatus.UNSPECIFIED:
                         return response.value.responses.values[request_id], response.value
                 else:
