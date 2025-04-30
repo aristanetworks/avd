9ed9a55cd6c1695e99c237eae8e9b1321b4602a4
Vibhu-gslab <vibhu.tripathi-ext@arista.com>
2025-04-30 14:36:53 +0530

    adding underlay_multicast check

--- python-avd/pyavd/_eos_designs/structured_config/network_services/vxlan_interface.py
+++ python-avd/pyavd/_eos_designs/structured_config/network_services/vxlan_interface.py
@@ -196,22 +196,23 @@ class VxlanInterfaceMixin(Protocol):
                 vlan.id,
                 tenant.evpn_l2_multicast.underlay_l2_multicast_group_ipv4_pool_offset,
             )
-        if vlan.vxlan_flood_multicast.enabled is not None:
-            vxlan_vlan.flood_group = (
-                vlan.vxlan_flood_multicast.underlay_multicast_group
-                if vlan.vxlan_flood_multicast.underlay_multicast_group and vlan.vxlan_flood_multicast.enabled is True
-                else None
-            )
-        elif tenant.vxlan_flood_multicast.enabled is True:
-            if not tenant.vxlan_flood_multicast.underlay_l2_multicast_group_ipv4_pool:
-                msg = f"'vxlan_flood_multicast.underlay_l2_multicast_group_ipv4_pool' for Tenant: {tenant.name} is required."
-                raise AristaAvdInvalidInputsError(msg)
+        if self.shared_utils.underlay_multicast:
+            if vlan.vxlan_flood_multicast.enabled is not None:
+                vxlan_vlan.flood_group = (
+                    vlan.vxlan_flood_multicast.underlay_multicast_group
+                    if vlan.vxlan_flood_multicast.underlay_multicast_group and vlan.vxlan_flood_multicast.enabled is True
+                    else None
+                )
+            elif tenant.vxlan_flood_multicast.enabled is True:
+                if not tenant.vxlan_flood_multicast.underlay_l2_multicast_group_ipv4_pool:
+                    msg = f"'vxlan_flood_multicast.underlay_l2_multicast_group_ipv4_pool' for Tenant: {tenant.name} is required."
+                    raise AristaAvdInvalidInputsError(msg)
 
-            vxlan_vlan.flood_group = self.shared_utils.ip_addressing.evpn_underlay_l2_flood_group(
-                tenant.vxlan_flood_multicast.underlay_l2_multicast_group_ipv4_pool,
-                vlan.id,
-                tenant.vxlan_flood_multicast.underlay_l2_multicast_group_ipv4_pool_offset,
-            )
+                vxlan_vlan.flood_group = self.shared_utils.ip_addressing.evpn_underlay_l2_flood_group(
+                    tenant.vxlan_flood_multicast.underlay_l2_multicast_group_ipv4_pool,
+                    vlan.id,
+                    tenant.vxlan_flood_multicast.underlay_l2_multicast_group_ipv4_pool_offset,
+                )
 
         if self.shared_utils.overlay_her and self.inputs.overlay_her_flood_list_per_vni and (vlan_id_entry := self._overlay_her_flood_lists.get(vlan.id)):
             vxlan_vlan.flood_vteps.extend(natural_sort(unique(vlan_id_entry)))
