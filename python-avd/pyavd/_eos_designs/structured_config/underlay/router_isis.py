03d31d97d9ad0d1f678c2e4bcc93b908df5541a0
MaheshGSLAB <mahesh.kumar-ext@arista.com>
2025-04-30 13:04:13 +0530

    Refactor(eos_designs): Improved test coverage for underlay/router_isis.py

--- python-avd/pyavd/_eos_designs/structured_config/underlay/router_isis.py
+++ python-avd/pyavd/_eos_designs/structured_config/underlay/router_isis.py
@@ -71,9 +71,6 @@ class RouterIsisMixin(Protocol):
                 )
                 raise AristaAvdInvalidInputsError(msg)
 
-            if self.shared_utils.id is None:
-                msg = f"'id' is not set on '{self.shared_utils.hostname}' and is required to set ISIS NET address using the node ID"
-                raise AristaAvdInvalidInputsError(msg)
             system_id = f"{isis_system_id_prefix}.{self.shared_utils.id:04d}"
         else:
             system_id = self.ipv4_to_isis_system_id(self.shared_utils.router_id)
