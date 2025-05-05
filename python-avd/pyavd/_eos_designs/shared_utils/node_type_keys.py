67f9fd1870d848ac042355372117f8124d550955
Shivani-gslab <schourasiya-ext@arista.com>
2025-05-05 12:54:31 +0530

    Adding negative test to cover node_type error

--- python-avd/pyavd/_eos_designs/shared_utils/node_type_keys.py
+++ python-avd/pyavd/_eos_designs/shared_utils/node_type_keys.py
@@ -206,6 +206,6 @@ class NodeTypeKeysMixin(Protocol):
             if node_type_key.type == self.type:
                 return node_type_key
 
-        # This should never happen, as it should be caught during validation
+        # This can happen when the "avd_data_validation_mode" is set to "warning" or when running with pyavd.
         msg = f"Could not find the given type '{self.type}' in node_type_keys or custom_node_type_keys."
         raise AristaAvdInvalidInputsError(msg)
