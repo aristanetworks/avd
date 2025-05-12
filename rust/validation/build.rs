c7256de324ade406c37675651007a842b3990608
Guillaume Mulocher <gmulocher@arista.com>
2025-05-12 10:30:35 +0200

    Update rust/validation/build.rs

--- rust/validation/build.rs
+++ rust/validation/build.rs
@@ -15,7 +15,7 @@ fn main() {
         let enabled_features: Vec<&str> = features_string_ref.split(',').collect();
 
         if enabled_features.contains(&"python_bindings") {
-            if env::var("CARGO_CFG_TARGET_OS") == Ok("macos") {
+            if env::var("CARGO_CFG_TARGET_OS") == Ok("macos".to_string()) {
                 println!("cargo:warning=Compiling using feature 'python_bindings' on 'macos'");
                 // Needed for MacOS when using pyo3 extension-module
                 pyo3_build_config::add_extension_module_link_args();
