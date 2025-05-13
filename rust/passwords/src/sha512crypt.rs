80ff324dcf676906c583630a8b1d070e0a44feb9
gmuloc <gmulocher@arista.com>
2025-05-13 16:45:42 +0200

    Refactor: Address PR comments

--- rust/passwords/src/sha512crypt.rs
+++ rust/passwords/src/sha512crypt.rs
@@ -6,14 +6,10 @@ use sha_crypt::{CryptError, Sha512Params, sha512_crypt_b64};
 
 pub fn sha512_crypt(password: String, salt: String) -> Result<String, CryptError> {
     // Setting rounds to 5000 which is the default for sha512crypt
-    let params = Sha512Params::new(5_000).expect("Could not instantiate Sha512Params.");
-
+    let params = Sha512Params::new(5_000)?;
     // Hash the password
-    let hashed_password = sha512_crypt_b64(password.as_bytes(), salt.as_bytes(), &params)
-        .expect("Failed to compute sha512 password.");
-
-    let mut result = format!("$6${salt}${hashed_password}");
-    Ok(result)
+    let hashed_password = sha512_crypt_b64(password.as_bytes(), salt.as_bytes(), &params)?;
+    Ok(format!("$6${salt}${hashed_password}"))
 }
 
 #[cfg(test)]
