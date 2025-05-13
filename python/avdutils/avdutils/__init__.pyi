6e7c03c180ffe5ed3a11b3a17f807eb1b866f672
gmuloc <gmulocher@arista.com>
2025-05-13 14:41:36 +0200

    CI: Clean-up

--- python/avdutils/avdutils/__init__.pyi
+++ python/avdutils/avdutils/__init__.pyi
@@ -1,4 +1,4 @@
 # Copyright (c) 2025 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
-from . import validation, passwords  # noqa: F401
+from . import passwords, validation  # noqa: F401
