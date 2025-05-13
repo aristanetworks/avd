de09a09544aabeaabf2e76ef5f2e447904552d1e
gmuloc <gmulocher@arista.com>
2025-05-13 14:37:30 +0200

    Refactor(rust): Address PR comments

--- rust/passwords/src/lib.rs
+++ rust/passwords/src/lib.rs
@@ -11,22 +11,27 @@ use pyo3::pymodule;
 mod passwords {
 
     use crate::sha512crypt;
-    use pyo3::{Bound, PyResult, pyfunction, types::PyModule};
-
-    #[pymodule_init]
-    fn init(_m: &Bound<'_, PyModule>) -> PyResult<()> {
-        Ok(())
-    }
+    use pyo3::{
+        PyResult,
+        exceptions::{PyRuntimeError, PyValueError},
+        pyfunction,
+    };
+    use sha_crypt::CryptError;
 
     #[pyfunction]
     /// Computes the SHA512 crypt value for the password given the salt
-    pub fn sha512_crypt(password: String, salt: String) -> String {
-        let result = sha512crypt::main(password, salt);
-
-        match result {
-            Ok(res) => res,
-            Err(e) => format!("{e:?}").to_string(),
-            // Err(e) => serde_json::to_string(&e).unwrap(),
-        }
+    pub fn sha512_crypt(password: String, salt: String) -> PyResult<String> {
+        sha512crypt::sha512_crypt(password, salt).map_err(|err: CryptError| match err {
+            CryptError::RoundsError => {
+                PyValueError::new_err("Invalid number of rounds specified for SHA512 crypt.")
+            }
+            CryptError::StringError(utf8_err) => PyValueError::new_err(format!(
+                "String encoding error during SHA512 crypt (UTF-8 conversion failed): {}",
+                utf8_err
+            )),
+            CryptError::RandomError => PyRuntimeError::new_err(
+                "A random number generation error occurred during SHA512 crypt.",
+            ),
+        })
     }
 }
--- rust/passwords/src/sha512crypt.rs
+++ rust/passwords/src/sha512crypt.rs
@@ -2,9 +2,9 @@
 // Use of this source code is governed by the Apache License 2.0
 // that can be found in the LICENSE file.
 
-use sha_crypt::{sha512_crypt_b64, CryptError, Sha512Params};
+use sha_crypt::{CryptError, Sha512Params, sha512_crypt_b64};
 
-pub fn main(password: String, salt: String) -> Result<String, CryptError> {
+pub fn sha512_crypt(password: String, salt: String) -> Result<String, CryptError> {
     // Setting rounds to 5000 which is the default for sha512crypt
     let params = Sha512Params::new(5_000).expect("Could not instantiate Sha512Params.");
 
@@ -30,7 +30,10 @@ mod tests {
         let salt = "1234567890ABCDEF";
         let password = "LittleDropBobbyTable";
 
-        let hash = main(password.to_string(), salt.to_string()).unwrap();
-        assert_eq!(hash, "$6$1234567890ABCDEF$Em9R7hgj77mOWT2JjGxPzUQEXpe0HmEpcxlhR5W.cMjg48.AJ1L3qFxTKuvXdmsiisbVh04tvKKH1ab.15PaD1");
+        let hash = sha512_crypt(password.to_string(), salt.to_string()).unwrap();
+        assert_eq!(
+            hash,
+            "$6$1234567890ABCDEF$Em9R7hgj77mOWT2JjGxPzUQEXpe0HmEpcxlhR5W.cMjg48.AJ1L3qFxTKuvXdmsiisbVh04tvKKH1ab.15PaD1"
+        );
     }
 }
