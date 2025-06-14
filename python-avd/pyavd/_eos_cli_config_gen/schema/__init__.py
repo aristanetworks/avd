1b208d2dca89eba7579289350d92c9424302a1f6
Alex Kudlov <alex@stillworld.com>
2025-06-14 18:40:26 +0000

    molecule

--- python-avd/pyavd/_eos_cli_config_gen/schema/__init__.py
+++ python-avd/pyavd/_eos_cli_config_gen/schema/__init__.py
@@ -63813,6 +63813,7 @@ class EosCliConfigGen(EosCliConfigGenRootModel):
             "ip_verify_unicast_source_reachable_via": {"type": str},
             "ip_igmp": {"type": bool},
             "ip_igmp_version": {"type": int},
+            "ip_igmp_querier_address_virtual": {"type": bool},
             "ip_igmp_host_proxy": {"type": IpIgmpHostProxy},
             "ip_helpers": {"type": IpHelpers},
             "ip_dhcp_relay_all_subnets": {"type": bool},
@@ -63893,6 +63894,7 @@ class EosCliConfigGen(EosCliConfigGenRootModel):
         ip_verify_unicast_source_reachable_via: Literal["any", "rx"] | None
         ip_igmp: bool | None
         ip_igmp_version: int | None
+        ip_igmp_querier_address_virtual: bool | None
         ip_igmp_host_proxy: IpIgmpHostProxy
         """Subclass of AvdModel."""
         ip_helpers: IpHelpers
@@ -64042,6 +64044,7 @@ class EosCliConfigGen(EosCliConfigGenRootModel):
                 ip_verify_unicast_source_reachable_via: Literal["any", "rx"] | None | UndefinedType = Undefined,
                 ip_igmp: bool | None | UndefinedType = Undefined,
                 ip_igmp_version: int | None | UndefinedType = Undefined,
+                ip_igmp_querier_address_virtual: bool | None | UndefinedType = Undefined,
                 ip_igmp_host_proxy: IpIgmpHostProxy | UndefinedType = Undefined,
                 ip_helpers: IpHelpers | UndefinedType = Undefined,
                 ip_dhcp_relay_all_subnets: bool | None | UndefinedType = Undefined,
@@ -64120,6 +64123,7 @@ class EosCliConfigGen(EosCliConfigGenRootModel):
                     ip_verify_unicast_source_reachable_via: ip_verify_unicast_source_reachable_via
                     ip_igmp: ip_igmp
                     ip_igmp_version: ip_igmp_version
+                    ip_igmp_querier_address_virtual: ip_igmp_querier_address_virtual
                     ip_igmp_host_proxy: Subclass of AvdModel.
                     ip_helpers:
                        List of DHCP servers.
