1ba74b65f2cc53b92818f0d982c512843a35d043
Vibhu-gslab <109593615+Vibhu-gslab@users.noreply.github.com>
2025-06-17 11:48:02 +0530

    Update python-avd/pyavd/_eos_designs/structured_config/network_services/vxlan_interface.py
    Co-authored-by: Claus Holbech <holbech@arista.com>

--- python-avd/pyavd/_eos_designs/structured_config/network_services/vxlan_interface.py
+++ python-avd/pyavd/_eos_designs/structured_config/network_services/vxlan_interface.py
@@ -212,9 +212,9 @@ class VxlanInterfaceMixin(Protocol):
             if vxlan_vlan.flood_group is None:
                 if not tenant.vxlan_flood_multicast.underlay_l2_multicast_group_ipv4_pool:
                     msg = (
-                        "'vxlan_flood_multicast.underlay_l2_multicast_group_ipv4_pool' for Tenant: "
-                        f"{tenant.name} or 'vxlan_flood_multicast.underlay_l2_multicast_group' for VLAN: "
-                        f"{vlan.id} is required."
+                        "'vxlan_flood_multicast.underlay_l2_multicast_group_ipv4_pool' for Tenant "
+                        f"'{tenant.name}' or 'vxlan_flood_multicast.underlay_l2_multicast_group' for VLAN "
+                        f"'{vlan.id}' is required."
                     )
                     raise AristaAvdInvalidInputsError(msg)
                 vxlan_vlan.flood_group = self.shared_utils.ip_addressing.evpn_underlay_l2_flood_group(
