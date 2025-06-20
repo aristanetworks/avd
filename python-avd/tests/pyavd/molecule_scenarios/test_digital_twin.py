e61b68fd29546f5a0d9fc465a4a0aca0ef4a9b67
Alexey Gorbunov <ag.justice@gmail.com>
2025-06-19 17:50:02 -0700

    pytest wip

--- python-avd/tests/pyavd/molecule_scenarios/test_digital_twin.py
+++ python-avd/tests/pyavd/molecule_scenarios/test_digital_twin.py
@@ -1,15 +1,46 @@
 # Copyright (c) 2023-2025 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
+import json
+import sys
 from copy import deepcopy
 from typing import Any
+from unittest.mock import patch
 
 import pytest
 from yaml import CSafeLoader, load
 
-from pyavd import get_device_config, get_device_doc, validate_structured_config
+from pyavd import get_device_config, get_device_doc, get_device_structured_config, validate_inputs, validate_structured_config
 from pyavd._utils import get
 from tests.models import MoleculeHost
+
+
+def mocked_hostvars(self: MoleculeHost, digital_twin_environment: str = "act") -> dict:
+    match digital_twin_environment:
+        case "act":
+            staged_hostvars = json.loads(
+                json.dumps(
+                    self.scenario._vars.get_vars(host=self.ansible_host)
+                    | {
+                        "digital_twin_mode": True,
+                        "digital_twin": {
+                            "environment": "act",
+                            "fabric": {
+                                "username": "act_user",
+                                "password": "act_pass",
+                                "os_version": "4.33.0F",
+                            },
+                        },
+                    }
+                )
+            )
+            # Drop Inline Jinja keys
+            for node_type_key in get(staged_hostvars, "node_type_keys", []):
+                if "ip_addressing" in node_type_key:
+                    node_type_key.pop("ip_addressing", None)
+            return staged_hostvars
+        case _:
+            return {}
 
 
 def mocked_structured_config(self: MoleculeHost, digital_twin_environment: str = "act") -> dict:
@@ -87,3 +118,42 @@ def test_digital_twin_act_get_device_doc(molecule_host: MoleculeHost, monkeypatc
 
     assert isinstance(device_doc, str)
     assert device_doc == expected_doc
+
+
+@pytest.mark.molecule_scenarios(
+    "eos_designs-twodc-5stage-clos",
+)
+def test_digital_twin_act_get_device_structured_config(molecule_host: MoleculeHost, monkeypatch: Any) -> None:
+    """Test get_device_structured_config for Digital Twin ACT mode."""
+    for host in molecule_host.scenario.hosts:
+        host.__dict__.pop("hostvars", None)
+        monkeypatch.setattr(type(host), "hostvars", property(lambda host: mocked_hostvars(host, "act")))
+
+    inputs = deepcopy(molecule_host.hostvars)
+
+    # run validation on inputs to ensure it is converted
+    validate_inputs(inputs)
+
+    molecule_host.__dict__.pop("structured_config", None)
+    monkeypatch.setattr(type(molecule_host), "structured_config", property(lambda molecule_host: mocked_structured_config(molecule_host, "act")))
+    expected_structured_config = molecule_host.structured_config
+
+    with patch("sys.path", [*sys.path, *molecule_host.scenario.extra_python_paths]):
+        avd_facts = molecule_host.scenario.avd_facts
+        structured_config = get_device_structured_config(molecule_host.name, inputs, avd_facts)
+
+    # Ignore/drop calculated IPs on Spines' P2P links due to inability to support inline Jinja
+    for ethernet_interface in get(expected_structured_config, "ethernet_interfaces", []):
+        ethernet_interface.pop("ip_address", None)
+    for bgp_peer in get(expected_structured_config, "router_bgp.neighbors", []):
+        bgp_peer.pop("ip_address", None)
+    for ethernet_interface in get(structured_config, "ethernet_interfaces", []):
+        ethernet_interface.pop("ip_address", None)
+    for bgp_peer in get(structured_config, "router_bgp.neighbors", []):
+        bgp_peer.pop("ip_address", None)
+
+    assert isinstance(structured_config, dict)
+    assert molecule_host.name == structured_config["hostname"]
+    assert expected_structured_config == structured_config
+    # Test that we can dump the returned data as json.
+    assert json.dumps(structured_config)
