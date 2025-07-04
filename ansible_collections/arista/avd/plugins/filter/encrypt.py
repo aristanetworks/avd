d9ef6ae9f736b7c79062611ed3ec1f896973a48c
Guillaume Mulocher <gmulocher@arista.com>
2025-07-04 16:11:03 +0200

    Update ansible_collections/arista/avd/plugins/filter/encrypt.py
    Co-authored-by: Mahesh Kumar <122076792+MaheshGSLAB@users.noreply.github.com>

--- ansible_collections/arista/avd/plugins/filter/encrypt.py
+++ ansible_collections/arista/avd/plugins/filter/encrypt.py
@@ -70,7 +70,7 @@ options:
   salt:
     type: integer
     description: |-
-      Salt used for simple type-7 obfuscation. Required for `passwd_type: tacacs` and `radius`.
+      Salt used for simple type-7 obfuscation. Required when `passwd_type` is `radius` or `tacacs`.
     min: 0
     max: 15
 """
