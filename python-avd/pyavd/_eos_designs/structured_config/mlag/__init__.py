a5e38384cb39e84af50ccfd78989464671305d76
Laxmikant Chintakindi <laxmikant.c-ext@Laxmikants-MacBook-Pro.local>
2025-07-07 10:50:30 +0530

    Fix CI

--- python-avd/pyavd/_eos_designs/structured_config/mlag/__init__.py
+++ python-avd/pyavd/_eos_designs/structured_config/mlag/__init__.py
@@ -9,7 +9,7 @@ from pyavd._eos_cli_config_gen.schema import EosCliConfigGen
 from pyavd._eos_designs.structured_config.structured_config_generator import StructuredConfigGenerator, structured_config_contributor
 from pyavd._errors import AristaAvdMissingVariableError
 from pyavd._utils import AvdStringFormatter, default
-from pyavd._utils.password_utils.password import isis_encrypt, ospf_message_digest_encrypt
+from pyavd._utils.password_utils.password import ospf_message_digest_encrypt
 from pyavd.api.interface_descriptions import InterfaceDescriptionData
 from pyavd.j2filters import list_compress
 
