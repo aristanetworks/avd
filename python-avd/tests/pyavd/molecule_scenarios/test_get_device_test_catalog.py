68ae1b44725d0f979c76b9bd0fff8955f2edc369
MaheshGSLAB <mahesh.kumar-ext@arista.com>
2025-08-01 11:30:01 +0530

    updated the code logic

--- python-avd/tests/pyavd/molecule_scenarios/test_get_device_test_catalog.py
+++ python-avd/tests/pyavd/molecule_scenarios/test_get_device_test_catalog.py
@@ -17,10 +17,12 @@ SETTINGS_WITH_BGP_VRFS = AvdCatalogGenerationSettings(input_factory_settings=Inp
 SETTINGS_FILTERED_DEFAULT = AvdCatalogGenerationSettings(skip_tests=["VerifyNTP"])
 SETTINGS_FILTERED_SVC_LEAF = AvdCatalogGenerationSettings(skip_tests=["VerifyNTP"], run_tests=["VerifyReachability"])
 SETTINGS_FILTERED_SPINE = AvdCatalogGenerationSettings(run_tests=["VerifyLLDPNeighbors"], skip_tests=["VerifyLLDPNeighbors"])
-TEST_CASES: dict[str, Any] = {
-    "default_run": {},
-    "allow_bgp_vrfs_run": {},
+TEST_SETTINGS_MAP: dict[str, Any] = {
+    "default_run": {"default": None},
+    "allow_bgp_vrfs_run": {"default": SETTINGS_WITH_BGP_VRFS},
     "filtered_run": {
+        "default": SETTINGS_FILTERED_DEFAULT,
+        # Host-specific overrides
         "dc1-svc-leaf1a": SETTINGS_FILTERED_SVC_LEAF,
         "dc1-svc-leaf1b": SETTINGS_FILTERED_SVC_LEAF,
         "dc2-spine1": SETTINGS_FILTERED_SPINE,
@@ -34,8 +36,8 @@ RunName = Literal["default_run", "allow_bgp_vrfs_run", "filtered_run"]
 @pytest.mark.molecule_scenarios("anta_runner")
 @pytest.mark.parametrize(
     "run_name",
-    ["default_run", "allow_bgp_vrfs_run", "filtered_run"],
-    ids=["default_run", "allow_bgp_vrfs_run", "filtered_run"],
+    TEST_SETTINGS_MAP.keys(),
+    ids=TEST_SETTINGS_MAP.keys(),
 )
 def test_get_device_test_catalog(molecule_host: MoleculeHost, molecule_scenario: MoleculeScenario, run_name: RunName) -> None:
     """Verify get_device_test_catalog generates the correct ANTA catalog."""
@@ -43,11 +45,8 @@ def test_get_device_test_catalog(molecule_host: MoleculeHost, molecule_scenario:
     minimal_configs = get_minimal_structured_configs(all_configs)
     host_config = deepcopy(molecule_host.structured_config)
 
-    settings = None
-    if run_name == "allow_bgp_vrfs_run":
-        settings = SETTINGS_WITH_BGP_VRFS
-    elif run_name == "filtered_run":
-        settings = get(TEST_CASES[run_name], f"{molecule_host.name}", SETTINGS_FILTERED_DEFAULT)
+    run_settings = TEST_SETTINGS_MAP[run_name]
+    settings = get(run_settings, f"{molecule_host.name}", run_settings["default"])
 
     expected_data = deepcopy(molecule_host.get_test_catalog(run_name=run_name))
     result_catalog = get_device_test_catalog(molecule_host.name, host_config, minimal_configs, settings=settings)
