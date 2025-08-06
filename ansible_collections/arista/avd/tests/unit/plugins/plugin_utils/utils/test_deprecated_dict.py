7dad5826482818ead0f9c1736c51b48b255c8abd
Claus Holbech <holbech@arista.com>
2025-08-06 08:49:26 +0200

    linting

--- ansible_collections/arista/avd/tests/unit/plugins/plugin_utils/utils/test_deprecated_dict.py
+++ ansible_collections/arista/avd/tests/unit/plugins/plugin_utils/utils/test_deprecated_dict.py
@@ -11,22 +11,22 @@ def test__deprecated_dict(mock_display: Mock) -> None:
     test_dict = {"test_key1": "test_value_1", "test_key2": "test_value_2"}
 
     # Testing with __getitem__
-    deprecated_dict = DeprecatedDict(test_dict,_display=mock_display, _message="This dict is deprecated")
+    deprecated_dict = DeprecatedDict(test_dict, _display=mock_display, _message="This dict is deprecated")
     mock_display.assert_not_called()
     _unused = deprecated_dict["test_key1"]
     deprecated_method: Mock = mock_display.deprecated
-    deprecated_method.assert_called_once_with(msg='This dict is deprecated', version='6.0.0', collection_name='arista.avd', removed=False)
+    deprecated_method.assert_called_once_with(msg="This dict is deprecated", version="6.0.0", collection_name="arista.avd", removed=False)
     _unused = deprecated_dict["test_key1"]
     _unused = deprecated_dict["test_key2"]
-    deprecated_method.assert_called_once_with(msg='This dict is deprecated', version='6.0.0', collection_name='arista.avd', removed=False)
+    deprecated_method.assert_called_once_with(msg="This dict is deprecated", version="6.0.0", collection_name="arista.avd", removed=False)
 
     mock_display.reset_mock()
     # Testing with .get()
-    deprecated_dict = DeprecatedDict(test_dict,_display=mock_display, _message="This dict is deprecated")
+    deprecated_dict = DeprecatedDict(test_dict, _display=mock_display, _message="This dict is deprecated")
     mock_display.assert_not_called()
     _unused = deprecated_dict.get("test_key1")
     deprecated_method: Mock = mock_display.deprecated
-    deprecated_method.assert_called_once_with(msg='This dict is deprecated', version='6.0.0', collection_name='arista.avd', removed=False)
+    deprecated_method.assert_called_once_with(msg="This dict is deprecated", version="6.0.0", collection_name="arista.avd", removed=False)
     _unused = deprecated_dict.get("test_key1")
     _unused = deprecated_dict.get("test_key2")
-    deprecated_method.assert_called_once_with(msg='This dict is deprecated', version='6.0.0', collection_name='arista.avd', removed=False)
+    deprecated_method.assert_called_once_with(msg="This dict is deprecated", version="6.0.0", collection_name="arista.avd", removed=False)
