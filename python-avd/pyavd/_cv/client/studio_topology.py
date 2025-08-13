be4a4b4c58ef7f19ccd56f4d5fc93bba3cdcdbda
Tamas Plugor <tamas@arista.com>
2025-08-13 18:29:23 +0100

    fixing studio_topology.py

--- python-avd/pyavd/_cv/client/studio_topology.py
+++ python-avd/pyavd/_cv/client/studio_topology.py
@@ -3,76 +3,357 @@
 # that can be found in the LICENSE file.
 from __future__ import annotations
 
-import logging
-from typing import TYPE_CHECKING
+import json
+from logging import getLogger
+from typing import TYPE_CHECKING, Literal, Protocol
 
-from pyavd._cv.client.exceptions import CVClientException, CVDecommissioningFailed
-from pyavd._cv.workflows.create_workspace_on_cv import create_workspace_on_cv
-from pyavd._cv.workflows.finalize_workspace_on_cv import finalize_workspace_on_cv
-from pyavd._cv.workflows.models import CVWorkspace, DeployToCvResult
+from aristaproto import Casing
+
+from pyavd._cv.api.arista.studio.v1 import (
+    InputsConfig,
+    InputsConfigServiceStub,
+    InputsConfigSetSomeRequest,
+    InputsKey,
+)
+from pyavd._cv.api.arista.studio_topology.v1 import (
+    Decommission,
+    DecommissionConfig,
+    DecommissionConfigServiceStub,
+    DecommissionConfigSetSomeRequest,
+    DecommissionServiceStub,
+    DecommissionStatus,
+    DecommissionStreamRequest,
+    DeviceInfo,
+    DeviceInputConfig,
+    DeviceInputConfigServiceStub,
+    DeviceInputConfigSetSomeRequest,
+    DeviceKey,
+    DeviceState,
+    DeviceStateServiceStub,
+    DeviceStateStreamRequest,
+)
+from pyavd._cv.api.arista.time import TimeBounds
+from pyavd._cv.api.fmp import MacAddress, RepeatedString
+
+from .async_decorators import GRPCRequestHandler
+from .constants import DEFAULT_API_TIMEOUT
+from .exceptions import CVDecommissioningFailed
 
 if TYPE_CHECKING:
-    from pyavd._cv.client import CVClient
+    from datetime import datetime
+
+    from . import CVClientProtocol
+
+LOGGER = getLogger(__name__)
+
+GRPCRequestHandler.max_retries = 25
+
+DECOMMISSION_STATUS_MAP = {"unspecified": DecommissionStatus.UNSPECIFIED, "success": DecommissionStatus.SUCCESS, "failure": DecommissionStatus.FAILURE}
+
+TOPOLOGY_STUDIO_ID = "TOPOLOGY"
 
 
-async def decommission_devices(
-    cv_client: CVClient,
-    device_ids: list[str],
-    workspace: CVWorkspace | None = None,
-    logger: logging.Logger | None = None,
-) -> None:
-    """
-    Decommission devices on CloudVision using the new Studio aware decommissioning workflow.
+class StudioTopologyMixin(Protocol):
+    """Only to be used as mixin on CVClient class."""
 
-    Args:
-        cv_client: An active and authenticated CVClient instance.
-        device_ids: List of device serial numbers to decommission.
-        workspace: CloudVision Workspace to create/build/submit.
-        logger: Logger instance.
+    async def get_studio_topology(
+        self: CVClientProtocol,
+        workspace_id: str,
+        device_ids: list[str] | None = None,
+        time: datetime | None = None,
+        timeout: float = DEFAULT_API_TIMEOUT,
+    ) -> list[dict]:
+        """
+        Get Studio Topology using either studio inputs API or studio_topology API depending on the supported APIs.
 
-    Returns:
-        None.
-    """
-    if logger is None:
-        logger = logging.getLogger(__name__)
+        Parameters:
+            workspace_id: Unique identifier of the Workspace for which the information is fetched. Use "" for mainline.
+            device_ids: List of Device IDs / Serial numbers to get inputs for. If not given, all devices are returned.
+            time: Timestamp from which the information is fetched. `now()` if not set.
+            timeout: Timeout in seconds.
 
-    result = DeployToCvResult(workspace=workspace or CVWorkspace(), change_control=None)
-
-    try:
-        # Create a workspace
-        await create_workspace_on_cv(workspace=result.workspace, cv_client=cv_client)
-
+        Returns:
+            TopologyInput objects for the requested devices.
+        """
+        LOGGER.info("get_studio_topology: Trying the legacy Topology Studio Inputs API")
         try:
-            # Decommission devices
-            logger.info("Decommissioning devices %s in workspace %s", device_ids, result.workspace.id)
-            await cv_client.set_topology_decommissions(device_ids=device_ids, workspace_id=result.workspace.id)
-            logger.info("Getting the decommissioning status of all devices in workspace %s", result.workspace.id)
-            # Check for device decommissioning status to be successful
-            decommission_status = await cv_client.get_all_decommissions(device_ids=device_ids, workspace_id=result.workspace.id, state="success")
-            if decommission_status.error != "":
-                decommission_status.state = "failed"
-                logger.info("decommission_status: %s", decommission_status)
-                msg = f"Decommission failed during execution: {decommission_status.error}"
-                raise CVDecommissioningFailed(msg)
+            return await self.get_topology_studio_inputs(workspace_id, device_ids, time, timeout)
+        except Exception as e:
+            # The old API is not working. Try the new.
+            LOGGER.info("get_studio_topology: Failed the legacy Topology Studio Inputs API, %s", e)
 
-            decommission_status.state = "success"
+        LOGGER.info("get_studio_topology: Trying the New Studio Topology API")
+        return await self._get_studio_topology(workspace_id, device_ids, time, timeout)
 
-        except CVClientException as e:
-            result.errors.append(e)
-            result.failed = True
+    @GRPCRequestHandler()
+    async def _get_studio_topology(
+        self: CVClientProtocol,
+        workspace_id: str,
+        device_ids: list[str] | None = None,
+        time: datetime | None = None,
+        timeout: float = DEFAULT_API_TIMEOUT,
+    ) -> list[dict]:
+        """
+        Get Studio Topology using arista.studio_topology.v1.GetAll API.
 
-        # Build, submit or abandon Workspace. If failed, we always abandon.
-        if result.failed:
-            logger.warning("Abandoning workspace %s due to errors.", result.workspace.id)
-            await cv_client.abandon_workspace(workspace_id=result.workspace.id)
-            result.workspace.state = "abandoned"
-            return result
+        Parameters:
+            workspace_id: Unique identifier of the Workspace for which the information is fetched. Use "" for mainline.
+            device_ids: List of Device IDs / Serial numbers to get inputs for. If not given, all devices are returned.
+            time: Timestamp from which the information is fetched. `now()` if not set.
+            timeout: Timeout in seconds.
 
-        # Build and submit the workspace
-        await finalize_workspace_on_cv(workspace=result.workspace, cv_client=cv_client)
+        Returns:
+            TopologyInput objects for the requested devices.
+        """
+        topology_inputs: list[dict] = []
+        request = DeviceStateStreamRequest(partial_eq_filter=[DeviceState(key=DeviceKey(workspace_id=workspace_id))], time=TimeBounds(start=None, end=time))
 
-    except CVClientException as e:
-        result.errors.append(e)
-        result.failed = True
+        client = DeviceStateServiceStub(self._channel)
 
-    return result
+        responses = client.get_all(request, metadata=self._metadata, timeout=timeout)
+        async for response in responses:
+            device_state = response.value
+            if device_ids and device_state.key.device_id not in device_ids:
+                continue
+            device_info = device_state.device_info.to_dict(casing=Casing.SNAKE)
+            # MAC address is wrapped in a special fmp object. Unwrapping.
+            device_info["mac_address"] = device_state.device_info.mac_address.value
+            device_info["interfaces"] = [interface_info.to_dict(casing=Casing.SNAKE) for interface_info in device_state.interface_infos.values]
+            topology_inputs.append(device_info)
+
+        return topology_inputs
+
+    async def get_topology_studio_inputs(
+        self: CVClientProtocol,
+        workspace_id: str,
+        device_ids: list[str] | None = None,
+        time: datetime | None = None,
+        timeout: float = DEFAULT_API_TIMEOUT,
+    ) -> list[dict]:
+        """
+        Get Topology Studio Inputs using arista.studio.v1.InputsService.GetAll and arista.studio.v1.InputsConfigService.GetAll APIs.
+
+        Parameters:
+            workspace_id: Unique identifier of the Workspace for which the information is fetched. Use "" for mainline.
+            device_ids: List of Device IDs / Serial numbers to get inputs for. If not given, all devices are returned.
+            time: Timestamp from which the information is fetched. `now()` if not set.
+            timeout: Timeout in seconds.
+
+        Returns:
+            TopologyInput objects for the requested devices.
+        """
+        topology_inputs: list[dict] = []
+        studio_inputs: dict = await self.get_studio_inputs(
+            studio_id=TOPOLOGY_STUDIO_ID,
+            workspace_id=workspace_id,
+            default_value={},
+            time=time,
+            timeout=timeout,
+        )
+        for device_entry in studio_inputs.get("devices", []):
+            if not isinstance(device_entry, dict):
+                continue
+            device_id = str(device_entry.get("tags", {}).get("query", "")).removeprefix("device:")
+
+            # Ignore the device if it is not one of the requested devices.
+            if device_ids and device_id not in device_ids:
+                continue
+
+            device_info: dict = device_entry.get("inputs", {}).get("device", {})
+            interfaces: list[dict] = device_info.get("interfaces", [])
+            topology_inputs.append(
+                {
+                    "device_id": device_id,
+                    "hostname": device_info.get("hostname"),
+                    "mac_address": device_info.get("macAddress"),
+                    "model_name": device_info.get("modelName"),
+                    "interfaces": [
+                        {
+                            "name": str(interface.get("tags", {}).get("query", "")).removeprefix("interface:").split("@", maxsplit=1)[0],
+                            "neighbor_device_id": interface.get("inputs", {}).get("interface", {}).get("neighborDeviceId"),
+                            "neighbor_interface_name": interface.get("inputs", {}).get("interface", {}).get("neighborInterfaceName"),
+                        }
+                        for interface in interfaces
+                    ],
+                },
+            )
+        return topology_inputs
+
+    @GRPCRequestHandler()
+    async def set_studio_topology(
+        self: CVClientProtocol,
+        workspace_id: str,
+        device_inputs: list[tuple[str, str, str]],
+        timeout: float = DEFAULT_API_TIMEOUT,
+    ) -> None:
+        """
+        Set Studio Topology using either studio inputs API or studio_topology API depending on the supported APIs.
+
+        Parameters:
+            workspace_id: Unique identifier of the Workspace for which the information is set.
+            device_inputs: List of Tuples with the format (<device_id>, <hostname>, <system_mac>).
+            timeout: Base timeout in seconds. 0.1 second will be added per device.
+        """
+        LOGGER.info("set_studio_topology: Trying the legacy Topology Studio Inputs API")
+        await self.set_topology_studio_inputs(workspace_id, device_inputs, timeout)
+
+        LOGGER.info("set_studio_topology: Trying the New Studio Topology API")
+        await self._set_studio_topology(workspace_id, device_inputs, timeout)
+
+    @GRPCRequestHandler()
+    async def _set_studio_topology(
+        self: CVClientProtocol,
+        workspace_id: str,
+        device_inputs: list[tuple[str, str, str]],
+        timeout: float = DEFAULT_API_TIMEOUT,
+    ) -> list[DeviceKey]:
+        """
+        Set Topology Studio Inputs using arista.studio.v1.InputsConfigService.Set API.
+
+        Parameters:
+            workspace_id: Unique identifier of the Workspace for which the information is set.
+            device_inputs: List of Tuples with the format (<device_id>, <hostname>, <system_mac>).
+            timeout: Base timeout in seconds. 0.1 second will be added per device.
+        """
+        request = DeviceInputConfigSetSomeRequest(
+            values=[
+                DeviceInputConfig(
+                    key=DeviceKey(device_id=device_id, workspace_id=workspace_id),
+                    device_info=DeviceInfo(device_id=device_id, hostname=hostname, mac_address=MacAddress(value=system_mac)),
+                )
+                for device_id, hostname, system_mac in device_inputs
+            ]
+        )
+
+        client = DeviceInputConfigServiceStub(self._channel)
+        responses = client.set_some(request, metadata=self._metadata, timeout=timeout)
+        return [response.key async for response in responses]
+
+    @GRPCRequestHandler()
+    async def set_topology_studio_inputs(
+        self: CVClientProtocol,
+        workspace_id: str,
+        device_inputs: list[tuple[str, str, str]],
+        timeout: float = DEFAULT_API_TIMEOUT,
+    ) -> list[InputsKey]:
+        """
+        Set Topology Studio Inputs using arista.studio.v1.InputsConfigService.Set API.
+
+        Parameters:
+            workspace_id: Unique identifier of the Workspace for which the information is set.
+            device_inputs: List of Tuples with the format (<device_id>, <hostname>, <system_mac>).
+            timeout: Base timeout in seconds. 0.1 second will be added per device.
+        """
+        device_inputs_by_id = {device_id: {"hostname": hostname, "macAddress": system_mac} for device_id, hostname, system_mac in device_inputs}
+
+        # We need to get all the devices to make sure we get the correct index of devices.
+        studio_inputs: dict = await self.get_studio_inputs(studio_id=TOPOLOGY_STUDIO_ID, workspace_id=workspace_id, default_value={}, timeout=timeout)
+
+        request = InputsConfigSetSomeRequest(values=[])
+
+        for device_index, device_entry in enumerate(studio_inputs.get("devices", [])):
+            if not isinstance(device_entry, dict):
+                continue
+
+            device_id = str(device_entry.get("tags", {}).get("query", "")).removeprefix("device:")
+
+            # Ignore the device if it is not one of the requested devices.
+            if device_id not in device_inputs_by_id:
+                continue
+
+            # Update the given fields for the device and add a separate SetSome entry for this device.
+            device_info: dict = device_entry.get("inputs", {}).get("device", {})
+            device_info.update(device_inputs_by_id.pop(device_id))
+
+            request.values.append(
+                InputsConfig(
+                    key=InputsKey(
+                        studio_id=TOPOLOGY_STUDIO_ID,
+                        workspace_id=workspace_id,
+                        path=RepeatedString(values=["devices", str(device_index), "inputs", "device"]),
+                    ),
+                    inputs=json.dumps(device_info),
+                ),
+            )
+
+        index_offset = len(studio_inputs.get("devices", []))
+        # Add any devices not part of the topology studio already.
+        for index, device in enumerate(device_inputs_by_id.items()):
+            device_id, device_inputs = device
+            device_index = index + index_offset
+            device_entry = {
+                "inputs": {"device": {**device_inputs, "modelName": "", "interfaces": []}},
+                "tags": {"query": f"device:{device_id}"},
+            }
+            request.values.append(
+                InputsConfig(
+                    key=InputsKey(
+                        studio_id=TOPOLOGY_STUDIO_ID,
+                        workspace_id=workspace_id,
+                        path=RepeatedString(values=["devices", str(device_index)]),
+                    ),
+                    inputs=json.dumps(device_entry),
+                ),
+            )
+
+        client = InputsConfigServiceStub(self._channel)
+        responses = client.set_some(request, metadata=self._metadata, timeout=timeout + len(request.values) * 0.1)
+        return [response.key async for response in responses]
+
+    @GRPCRequestHandler()
+    async def set_topology_decommissions(
+        self: CVClientProtocol,
+        device_ids: list[str],
+        workspace_id: str,
+        timeout: float = DEFAULT_API_TIMEOUT,
+    ) -> list[DecommissionConfig]:
+        """
+        Decommission devices using arista.studio_topology.v1.DecommissionConfigService.SetSome API.
+
+        Parameters:
+            device_ids: Device IDs to decommission.
+            workspace_id: Workspace ID to decommission the devices from.
+            timeout: Timeout in seconds.
+
+        Returns:
+            List of DecommissionConfig objects.
+        """
+        decommission_configs = [DecommissionConfig(key=DeviceKey(device_id=device_id, workspace_id=workspace_id)) for device_id in device_ids]
+        request = DecommissionConfigSetSomeRequest(decommission_configs)
+        client = DecommissionConfigServiceStub(self._channel)
+        responses = client.set_some(request, metadata=self._metadata, timeout=timeout)
+
+        return [response.key async for response in responses]
+
+    @GRPCRequestHandler()
+    async def get_all_decommissions(
+        self: CVClientProtocol,
+        device_ids: list[str],
+        workspace_id: str,
+        state: Literal["success", "unspecified", "failure"],
+        timeout: float = DEFAULT_API_TIMEOUT,
+    ) -> list[DecommissionConfig]:
+        """
+        Subscribe to the decommission status with arista.studio_topology.v1.DecommissionService.Subscribe API.
+
+        Parameters:
+            device_ids: Device IDs to decommission.
+            workspace_id: Workspace ID to decommission the devices from.
+            state: Decommission state to wait for.
+            timeout: Timeout in seconds.
+
+        Returns:
+            List of DecommissionConfig objects.
+        """
+        decommissioning_devices = [Decommission(key=DeviceKey(device_id=device_id, workspace_id=workspace_id)) for device_id in device_ids]
+        request = DecommissionStreamRequest(decommissioning_devices)
+        client = DecommissionServiceStub(self._channel)
+        responses = client.subscribe(request, metadata=self._metadata, timeout=timeout)
+        async for response in responses:
+            LOGGER.debug("wait_for_decommission_success: Response is '%s.'", response)
+            if hasattr(response, "value") and response.value.status == DECOMMISSION_STATUS_MAP[state]:
+                LOGGER.info("wait_for_decommission_success: Got response for request '%s': %s", device_ids, response.value.status)
+                return response.value
+        # Use case where stream completed without getting ChangeControl update in the desired state
+        msg = f"Device decommissioning has not reached desired state '{state}'."
+        raise CVDecommissioningFailed(msg)
