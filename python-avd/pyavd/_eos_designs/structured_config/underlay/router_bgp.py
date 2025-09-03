3901212929e48438079ae9ec3d6477c524a7fb75
Laxmikant Chintakindi <laxmikant.c-ext@Laxmikants-MacBook-Pro.local>
2025-09-03 15:51:42 +0530

    Fix CI

--- python-avd/pyavd/_eos_designs/structured_config/underlay/router_bgp.py
+++ python-avd/pyavd/_eos_designs/structured_config/underlay/router_bgp.py
@@ -10,8 +10,6 @@ from pyavd._eos_cli_config_gen.schema import EosCliConfigGen
 from pyavd._eos_designs.structured_config.structured_config_generator import structured_config_contributor
 
 if TYPE_CHECKING:
-    from pyavd._eos_designs.eos_designs_facts.schema import EosDesignsFacts
-
     from . import AvdStructuredConfigUnderlayProtocol
 
 
