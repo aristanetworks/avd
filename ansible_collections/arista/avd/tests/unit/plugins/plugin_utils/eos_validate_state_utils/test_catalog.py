246d66c65848c961988072265f477ba78285cda9
Claus Holbech <holbech@arista.com>
2025-09-04 09:34:58 +0200

    Apply suggestion from @ClausHolbechArista

--- ansible_collections/arista/avd/tests/unit/plugins/plugin_utils/eos_validate_state_utils/test_catalog.py
+++ ansible_collections/arista/avd/tests/unit/plugins/plugin_utils/eos_validate_state_utils/test_catalog.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
 
-# TODO: implement something here
+# TODO: Add tests for Catalog
 class TestCatalog:
     def test(self) -> None:
         pass
