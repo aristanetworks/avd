684af5a39925a5e620ad2c1f75687b47a47db1a8
gmuloc <gmulocher@arista.com>
2025-09-26 09:52:19 +0200

    Fix: Schema messed up by myself

--- python-avd/pyavd/_eos_designs/schema/__init__.py
+++ python-avd/pyavd/_eos_designs/schema/__init__.py
@@ -43261,14 +43261,14 @@ class EosDesigns(EosDesignsRootModel):
                     """
                     nodes: Nodes
                     """
-                    List of node names or fully matching regexes to configure the peer group, e.g. 'DC1-BL[12]B'.
-                    The
-                    regexes should be written in quotes.
-                    The peer-group is also created on devices which have a bgp_peer
-                    mapped to the corresponding peer_group.
+                    List of node names or regular expressions to match against hostnames to configure the peer-group.
+                    The regexes need to match full hostname and be enclosed in quotes, e.g 'DC1-BL[12]A'.
+                    The peer-group
+                    is also created on devices which have a bgp_peer mapped to the corresponding peer_group.
 
 
-                    Subclass of AvdList with `str` items.
+                    Subclass
+                    of AvdList with `str` items.
                     """
                     address_family_ipv4: AddressFamilyIpv4
                     """Subclass of AvdModel."""
@@ -43428,14 +43428,14 @@ class EosDesigns(EosDesignsRootModel):
                                    To protect the password at rest it is strongly recommended to
                                    make use of a vault or similar.
                                 nodes:
-                                   List of node names or fully matching regexes to configure the peer group, e.g. 'DC1-BL[12]B'.
-                                   The
-                                   regexes should be written in quotes.
-                                   The peer-group is also created on devices which have a bgp_peer
-                                   mapped to the corresponding peer_group.
+                                   List of node names or regular expressions to match against hostnames to configure the peer-group.
+                                   The regexes need to match full hostname and be enclosed in quotes, e.g 'DC1-BL[12]A'.
+                                   The peer-group
+                                   is also created on devices which have a bgp_peer mapped to the corresponding peer_group.
 
 
-                                   Subclass of AvdList with `str` items.
+                                   Subclass
+                                   of AvdList with `str` items.
                                 address_family_ipv4: Subclass of AvdModel.
                                 address_family_ipv6: Subclass of AvdModel.
                                 type: Key only used for documentation or validation purposes.
@@ -48261,11 +48261,11 @@ class EosDesigns(EosDesignsRootModel):
                         """Time-to-live in range of hops."""
                         nodes: Nodes
                         """
-                        List of node names or fully matching regexes, e.g `DC1-BL[12]A`.
-                        The regexes should be written in
-                        quotes.
-                        Nodes is required to restrict configuration of BGP neighbors to certain nodes in the
-                        network.
+                        List of node names or regular expressions to match against hostnames.
+                        The regexes need to match full
+                        hostname and be enclosed in quotes, e.g 'DC1-BL[12]A'.
+                        Nodes is required to restrict configuration
+                        of BGP neighbors to certain nodes in the network.
 
                         Subclass of AvdList with `str` items.
                         """
@@ -48375,11 +48375,11 @@ class EosDesigns(EosDesignsRootModel):
                                     update_source: update_source
                                     ebgp_multihop: Time-to-live in range of hops.
                                     nodes:
-                                       List of node names or fully matching regexes, e.g `DC1-BL[12]A`.
-                                       The regexes should be written in
-                                       quotes.
-                                       Nodes is required to restrict configuration of BGP neighbors to certain nodes in the
-                                       network.
+                                       List of node names or regular expressions to match against hostnames.
+                                       The regexes need to match full
+                                       hostname and be enclosed in quotes, e.g 'DC1-BL[12]A'.
+                                       Nodes is required to restrict configuration
+                                       of BGP neighbors to certain nodes in the network.
 
                                        Subclass of AvdList with `str` items.
                                     set_ipv4_next_hop:
@@ -49065,14 +49065,14 @@ class EosDesigns(EosDesignsRootModel):
                         """BGP peer group name."""
                         nodes: Nodes
                         """
-                        List of node names or fully matching regex to configure the peer group, e.g. 'DC1-BL[12]B'.
-                        The
-                        regexes should be written in quotes.
-                        The peer-group is also created on devices which have a bgp_peer
-                        mapped to the corresponding peer_group.
+                        List of node names or regular expressions to match against hostnames to configure the peer-group.
+                        The regexes need to match full hostname and be enclosed in quotes, e.g 'DC1-BL[12]A'.
+                        The peer-group
+                        is also created on devices which have a bgp_peer mapped to the corresponding peer_group.
 
 
-                        Subclass of AvdList with `str` items.
+                        Subclass
+                        of AvdList with `str` items.
                         """
                         password: str | None
                         """
@@ -49236,14 +49236,14 @@ class EosDesigns(EosDesignsRootModel):
                                 Args:
                                     name: BGP peer group name.
                                     nodes:
-                                       List of node names or fully matching regex to configure the peer group, e.g. 'DC1-BL[12]B'.
-                                       The
-                                       regexes should be written in quotes.
-                                       The peer-group is also created on devices which have a bgp_peer
-                                       mapped to the corresponding peer_group.
+                                       List of node names or regular expressions to match against hostnames to configure the peer-group.
+                                       The regexes need to match full hostname and be enclosed in quotes, e.g 'DC1-BL[12]A'.
+                                       The peer-group
+                                       is also created on devices which have a bgp_peer mapped to the corresponding peer_group.
 
 
-                                       Subclass of AvdList with `str` items.
+                                       Subclass
+                                       of AvdList with `str` items.
                                     password:
                                        Type 7 encrypted password.
                                        Takes precedence over `cleartext_password`.
