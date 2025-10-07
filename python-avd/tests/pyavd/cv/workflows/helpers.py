0d707e4f6595fa0294006eca7f0d2cb21cfd4a53
Carl Baillargeon <carl.baillargeon@arista.com>
2025-10-07 03:58:47 -0400

    Fix(cv_deploy): Properly support a custom Change Control name (#5964)
    Co-authored-by: Guillaume Mulocher <gmulocher@arista.com>

--- python-avd/tests/pyavd/cv/workflows/helpers.py
+++ python-avd/tests/pyavd/cv/workflows/helpers.py
@@ -1,12 +1,17 @@
 # Copyright (c) 2025 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
+from aristaproto import _DateTime
+
+from pyavd._cv.api.arista.changecontrol.v1 import Change, ChangeControl, ChangeControlStatus, Flag
 from pyavd._cv.api.arista.configlet.v1 import ConfigletAssignment, ConfigletAssignmentKey, MatchPolicy
 from pyavd._cv.api.fmp import RepeatedString
 from pyavd._cv.workflows.models import CVManifest
 
+DEFAULT_TIMESTAMP = _DateTime.fromisoformat("2025-10-03T00:00:00")
+
 # === Mock Creation Functions ===
-# These functions create instances of the api classes.
+# These functions create instances of the API classes.
 
 
 def create_grpc_container(
@@ -22,6 +27,19 @@ def create_grpc_container(
         child_assignment_ids=RepeatedString(values=child_ids or []),
         match_policy=MatchPolicy.MATCH_ALL,
     )
+
+
+def create_grpc_change_control(
+    status: ChangeControlStatus = ChangeControlStatus.NOT_STARTED,
+    approved: bool = False,
+    error: str | None = None,
+    name: str = "Test CC",
+    notes: str = "Test Notes",
+    time: _DateTime = DEFAULT_TIMESTAMP,
+) -> ChangeControl:
+    """Create a gRPC ChangeControl object."""
+    change = Change(name=name, notes=notes, time=time)
+    return ChangeControl(change=change, approve=Flag(value=approved), status=status, error=error)
 
 
 # === Other Helper Functions ===
