77ba5551d12794217ac8bcf82893874772232b9d
Guillaume Mulocher <gmulocher@arista.com>
2025-10-16 14:41:03 +0200

    Update python-avd/pyavd/_schema/models/avd_indexed_list.py
    Co-authored-by: Claus Holbech <holbech@arista.com>

--- python-avd/pyavd/_schema/models/avd_indexed_list.py
+++ python-avd/pyavd/_schema/models/avd_indexed_list.py
@@ -305,7 +305,7 @@ class AvdIndexedList(Sequence[T_AvdModel], AvdBase, Generic[T_PrimaryKey, T_AvdM
         Combining is different from merging in the sense that it will raise if there is a conflict
         between one of our elements and the other elements.
 
-        for AvdIndexedList this is simply an append, if any conflict occurs a duplicate error will be raised.
+        For AvdIndexedList this is simply an append, if any conflict occurs a duplicate error will be raised.
 
         Args:
             other: The other instance of the same type to combine into this instance.
