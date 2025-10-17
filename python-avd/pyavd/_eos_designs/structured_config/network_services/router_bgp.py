ee27fd2223ee4f32e4575f43f937c70ffaefec09
Guillaume Mulocher <gmulocher@arista.com>
2025-10-17 13:14:24 +0200

    Update python-avd/pyavd/_eos_designs/structured_config/network_services/router_bgp.py

--- python-avd/pyavd/_eos_designs/structured_config/network_services/router_bgp.py
+++ python-avd/pyavd/_eos_designs/structured_config/network_services/router_bgp.py
@@ -175,7 +175,7 @@ class RouterBgpMixin(Protocol):
                     if self.shared_utils.inband_mgmt_vrf == vrf.name and self.shared_utils.inband_management_parent_vlans:
                         bgp_vrf.redistribute.attached_host.enabled = True
 
-                    # Common things but need it repeated between default and non-defualt since type checker gets too confused
+                    # Common things but need it repeated between default and non-default since type checker gets too confused
                     # about the type of bgp_vrf vs. bgp_peer_config.
                     for aggregate_address in vrf.aggregate_addresses:
                         # Below we recast directly to eos_cli_config_gen. Losing incompatible keys, but relaying everything else.
