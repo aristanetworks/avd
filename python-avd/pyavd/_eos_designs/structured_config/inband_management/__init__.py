8044f6098c8dcd61ec00daf4251b576fe0e62c08
Guillaume Mulocher <gmulocher@arista.com>
2025-10-30 16:29:02 +0100

    Update python-avd/pyavd/_eos_designs/structured_config/inband_management/__init__.py

--- python-avd/pyavd/_eos_designs/structured_config/inband_management/__init__.py
+++ python-avd/pyavd/_eos_designs/structured_config/inband_management/__init__.py
@@ -23,7 +23,6 @@ class AvdStructuredConfigInbandManagement(StructuredConfigGenerator):
         if self.shared_utils.configure_inband_mgmt or self.shared_utils.configure_inband_mgmt_ipv6:
             # TODO: Refactor this later to inject from filtered tenants
             # Note that an attempt was made for this in #6073 but it has been postponed
-            # please reach out to @gmuloc before attempting this.
             if self.shared_utils.node_config.inband_mgmt_vlan not in self.structured_config.vlans:
                 # The VLAN was not added by an SVI in network_services
                 _ = self.structured_config.vlans.append_new(
