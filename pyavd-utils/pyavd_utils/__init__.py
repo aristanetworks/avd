46b6301355676d6d0524ba9d75b2184c0a60839f
Claus Holbech <holbech@arista.com>
2025-10-30 12:22:31 +0100

    Improve packaging of pyavd-utils

--- pyavd-utils/pyavd_utils/__init__.py
+++ pyavd-utils/pyavd_utils/__init__.py
@@ -1,3 +1,8 @@
 # Copyright (c) 2025 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
+
+__author__ = "Arista Networks"
+__copyright__ = "Copyright 2025 Arista Networks"
+__license__ = "Apache 2.0"
+__version__ = "6.0.0.dev2"
