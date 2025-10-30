a4c470041423a335826c74c86e88069094492fbe
gmuloc <gmulocher@arista.com>
2025-10-30 17:57:59 +0100

    Fix: Refactoring is hard

--- python-avd/pyavd/_eos_designs/structured_config/network_services/tunnel_interfaces.py
+++ python-avd/pyavd/_eos_designs/structured_config/network_services/tunnel_interfaces.py
@@ -43,6 +43,6 @@ class TunnelInterfacesMixin(Protocol):
             ipsec_profile=ipsec_profile,
         )
 
-        tunnel_interface.nat_profile = self.INTERNET_EXIT_ZSCALER_NAT_PROFILE_NAME
+        tunnel_interface.nat_profile = self.shared_utils.INTERNET_EXIT_ZSCALER_NAT_PROFILE_NAME
 
         self.structured_config.tunnel_interfaces.append(tunnel_interface)
