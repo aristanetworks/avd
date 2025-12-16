d023f85ba0098f8d234166b05a2d8f3db18cd6b1
Mahesh Kumar <122076792+MaheshGSLAB@users.noreply.github.com>
2025-12-16 20:14:24 +0530

    Refactor(eos_cli_config_gen): Changed the input data model from ip_tftp_client_source_interfaces to ip_tftp_client (#6237)
    Co-authored-by: Guillaume Mulocher <gmulocher@arista.com>

--- python-avd/pyavd/_eos_cli_config_gen/schema/__init__.py
+++ python-avd/pyavd/_eos_cli_config_gen/schema/__init__.py
@@ -17464,34 +17464,67 @@ class EosCliConfigGen(EosCliConfigGenRootModel):
 
     IpTelnetClientSourceInterfaces._item_type = IpTelnetClientSourceInterfacesItem
 
-    class IpTftpClientSourceInterfacesItem(AvdModel):
+    class IpTftpClient(AvdModel):
         """Subclass of AvdModel."""
 
-        _fields: ClassVar[dict] = {"name": {"type": str}, "vrf": {"type": str}}
-        name: str
-        """Interface Name."""
-        vrf: str | None
-        """VRF Name."""
+        class VrfsItem(AvdModel):
+            """Subclass of AvdModel."""
+
+            _fields: ClassVar[dict] = {"name": {"type": str}, "source_interface": {"type": str}}
+            name: str
+            source_interface: str
+
+            if TYPE_CHECKING:
+
+                def __init__(self, *, name: str | UndefinedType = Undefined, source_interface: str | UndefinedType = Undefined) -> None:
+                    """
+                    VrfsItem.
+
+
+                    Subclass of AvdModel.
+
+                    Args:
+                        name: name
+                        source_interface: source_interface
+
+                    """
+
+        class Vrfs(AvdIndexedList[str, VrfsItem]):
+            """Subclass of AvdIndexedList with `VrfsItem` items. Primary key is `name` (`str`)."""
+
+            _primary_key: ClassVar[str] = "name"
+
+        Vrfs._item_type = VrfsItem
+
+        _fields: ClassVar[dict] = {"source_interface": {"type": str}, "vrfs": {"type": Vrfs}}
+        source_interface: str | None
+        """Define `source_interface` for VRF default."""
+        vrfs: Vrfs
+        """
+        Define `source interfaces` for any VRF other than default.
+
+        Subclass of AvdIndexedList with
+        `VrfsItem` items. Primary key is `name` (`str`).
+        """
 
         if TYPE_CHECKING:
 
-            def __init__(self, *, name: str | UndefinedType = Undefined, vrf: str | None | UndefinedType = Undefined) -> None:
+            def __init__(self, *, source_interface: str | None | UndefinedType = Undefined, vrfs: Vrfs | UndefinedType = Undefined) -> None:
                 """
-                IpTftpClientSourceInterfacesItem.
+                IpTftpClient.
 
 
                 Subclass of AvdModel.
 
                 Args:
-                    name: Interface Name.
-                    vrf: VRF Name.
+                    source_interface: Define `source_interface` for VRF default.
+                    vrfs:
+                       Define `source interfaces` for any VRF other than default.
+
+                       Subclass of AvdIndexedList with
+                       `VrfsItem` items. Primary key is `name` (`str`).
 
                 """
-
-    class IpTftpClientSourceInterfaces(AvdList[IpTftpClientSourceInterfacesItem]):
-        """Subclass of AvdList with `IpTftpClientSourceInterfacesItem` items."""
-
-    IpTftpClientSourceInterfaces._item_type = IpTftpClientSourceInterfacesItem
 
     class Ipv6AccessListsItem(AvdModel):
         """Subclass of AvdModel."""
@@ -68585,7 +68618,7 @@ class EosCliConfigGen(EosCliConfigGenRootModel):
         "ip_ssh_client_source_interfaces": {"type": IpSshClientSourceInterfaces},
         "ip_tacacs_source_interfaces": {"type": IpTacacsSourceInterfaces},
         "ip_telnet_client_source_interfaces": {"type": IpTelnetClientSourceInterfaces},
-        "ip_tftp_client_source_interfaces": {"type": IpTftpClientSourceInterfaces},
+        "ip_tftp_client": {"type": IpTftpClient},
         "ip_virtual_router_mac_address": {"type": str},
         "ip_virtual_router_mac_address_advertisement_interval": {"type": int},
         "ip_virtual_router_mac_address_mlag_peer": {"type": bool},
@@ -68944,8 +68977,8 @@ class EosCliConfigGen(EosCliConfigGenRootModel):
     """Subclass of AvdList with `IpTacacsSourceInterfacesItem` items."""
     ip_telnet_client_source_interfaces: IpTelnetClientSourceInterfaces
     """Subclass of AvdList with `IpTelnetClientSourceInterfacesItem` items."""
-    ip_tftp_client_source_interfaces: IpTftpClientSourceInterfaces
-    """Subclass of AvdList with `IpTftpClientSourceInterfacesItem` items."""
+    ip_tftp_client: IpTftpClient
+    """Subclass of AvdModel."""
     ip_virtual_router_mac_address: str | None
     """MAC address (hh:hh:hh:hh:hh:hh)."""
     ip_virtual_router_mac_address_advertisement_interval: int | None
@@ -69327,7 +69360,7 @@ class EosCliConfigGen(EosCliConfigGenRootModel):
             ip_ssh_client_source_interfaces: IpSshClientSourceInterfaces | UndefinedType = Undefined,
             ip_tacacs_source_interfaces: IpTacacsSourceInterfaces | UndefinedType = Undefined,
             ip_telnet_client_source_interfaces: IpTelnetClientSourceInterfaces | UndefinedType = Undefined,
-            ip_tftp_client_source_interfaces: IpTftpClientSourceInterfaces | UndefinedType = Undefined,
+            ip_tftp_client: IpTftpClient | UndefinedType = Undefined,
             ip_virtual_router_mac_address: str | None | UndefinedType = Undefined,
             ip_virtual_router_mac_address_advertisement_interval: int | None | UndefinedType = Undefined,
             ip_virtual_router_mac_address_mlag_peer: bool | None | UndefinedType = Undefined,
@@ -69609,7 +69642,7 @@ class EosCliConfigGen(EosCliConfigGenRootModel):
                 ip_ssh_client_source_interfaces: Subclass of AvdList with `IpSshClientSourceInterfacesItem` items.
                 ip_tacacs_source_interfaces: Subclass of AvdList with `IpTacacsSourceInterfacesItem` items.
                 ip_telnet_client_source_interfaces: Subclass of AvdList with `IpTelnetClientSourceInterfacesItem` items.
-                ip_tftp_client_source_interfaces: Subclass of AvdList with `IpTftpClientSourceInterfacesItem` items.
+                ip_tftp_client: Subclass of AvdModel.
                 ip_virtual_router_mac_address: MAC address (hh:hh:hh:hh:hh:hh).
                 ip_virtual_router_mac_address_advertisement_interval: Advertisement interval in seconds.
                 ip_virtual_router_mac_address_mlag_peer: Enable MLAG peer gateway.
