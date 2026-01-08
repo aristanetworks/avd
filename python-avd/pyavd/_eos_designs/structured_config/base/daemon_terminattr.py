5b6840d2e9f3bf93dd70c552c285d728c6883e8d
MaheshGSLAB <mahesh.kumar-ext@arista.com>
2026-01-08 18:35:17 +0530

    Refactor(eos_designs): Raise error when cv servers are defined but ntp server is missing

--- python-avd/pyavd/_eos_designs/structured_config/base/daemon_terminattr.py
+++ python-avd/pyavd/_eos_designs/structured_config/base/daemon_terminattr.py
@@ -3,6 +3,7 @@
 # that can be found in the LICENSE file.
 from __future__ import annotations
 
+import ipaddress
 from typing import TYPE_CHECKING, Protocol
 
 from pyavd._eos_cli_config_gen.schema import EosCliConfigGen
@@ -32,7 +33,7 @@ class DaemonTerminattrMixin(Protocol):
         if not (cv_settings := self.inputs.cv_settings):
             self._validate_missing_cv_settings(first_tracker_exported_to_cloudvision)
             return
-
+        self._validate_onprem_clusters_dependencies(cv_settings.onprem_clusters)
         clusters: list[EosDesigns.CvSettings.Cvaas.ClustersItem | EosDesigns.CvSettings.OnpremClustersItem] = (
             list(cv_settings.cvaas.clusters) if cv_settings.cvaas.enabled else []
         )
@@ -129,3 +130,34 @@ class DaemonTerminattrMixin(Protocol):
                 " Please configure 'cv_settings' when enabling 'sflow_settings.export_to_cloudvision.enabled'."
             )
             raise AristaAvdInvalidInputsError(msg)
+
+    def _validate_onprem_clusters_dependencies(self: AvdStructuredConfigBaseProtocol, onprem_clusters: EosDesigns.CvSettings.OnpremClusters) -> None:
+        """
+        Validate dependencies required when CV on-prem clusters are configured.
+
+        - Requires NTP to be configured.
+        - Requires DNS settings if any CVP server is defined using a DNS name.
+        """
+        if not onprem_clusters:
+            return
+
+        if any(self._is_dns_name(server.name) for cluster in onprem_clusters for server in cluster.servers) and not self.inputs.dns_settings.servers:
+            msg = "'dns_settings' must be configured when cv_settings.onprem_clusters[].servers[].name' is set to a DNS name."
+            raise AristaAvdInvalidInputsError(msg)
+
+        if not self.structured_config.ntp.servers:
+            msg = "'ntp.servers' must be configured when 'cv_settings.onprem_clusters' is defined."
+            raise AristaAvdInvalidInputsError(msg)
+
+    @staticmethod
+    def _is_dns_name(fqdm: str) -> bool:
+        """
+        Check if the string is a valid IP address or not.
+
+        If it is not a valid IP address, it is considered a DNS name.
+        """
+        try:
+            ipaddress.ip_address(fqdm)
+        except ValueError:
+            return True
+        return False
