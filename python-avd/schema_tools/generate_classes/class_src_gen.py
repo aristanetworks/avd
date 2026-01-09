d311b5b75c767a26f39c0e6fa91dea35177e52c1
Claus Holbech <holbech@arista.com>
2026-01-09 15:38:35 +0100

    fix pyright issue

--- python-avd/schema_tools/generate_classes/class_src_gen.py
+++ python-avd/schema_tools/generate_classes/class_src_gen.py
@@ -5,17 +5,15 @@ from __future__ import annotations
 
 from functools import cached_property
 from keyword import iskeyword
-from typing import TYPE_CHECKING, Generic, TypeVar
-
-from schema_tools.metaschema.meta_schema_model import AvdSchemaDict
+from typing import TYPE_CHECKING, Generic, TypeVar, cast
 
 from .src_generators import ClassVarSrc, FieldSrc, FieldTypeHintSrc, ListSrc, LiteralSrc, ModelSrc, SrcData
 from .utils import generate_class_name, generate_class_name_from_ref
 
 if TYPE_CHECKING:
-    from schema_tools.metaschema.meta_schema_model import AvdSchemaBool, AvdSchemaInt, AvdSchemaList, AvdSchemaStr
+    from schema_tools.metaschema.meta_schema_model import AvdSchemaBool, AvdSchemaDict, AvdSchemaInt, AvdSchemaList, AvdSchemaStr
 
-    T = TypeVar("T", bound="AvdSchemaBool | AvdSchemaDict | AvdSchemaInt | AvdSchemaList | AvdSchemaStr")
+T = TypeVar("T", bound="AvdSchemaBool | AvdSchemaDict | AvdSchemaInt | AvdSchemaList | AvdSchemaStr")
 
 
 class SrcGenBase(Generic[T]):
@@ -225,8 +223,10 @@ class SrcGenList(SrcGenBase["AvdSchemaList"]):
         item_classes = []
 
         # Ensure that the items for lists with primary key always have the primary key as the first key:
-        if self.schema.primary_key and isinstance(self.schema.items, AvdSchemaDict) and self.schema.items.keys:
-            self.schema.items.keys = {self.schema.primary_key: self.schema.items.keys[self.schema.primary_key]} | self.schema.items.keys
+        if self.schema.primary_key:
+            self.schema.items = cast("AvdSchemaDict", self.schema.items)
+            if self.schema.items.keys:
+                self.schema.items.keys = {self.schema.primary_key: self.schema.items.keys[self.schema.primary_key]} | self.schema.items.keys
 
         fieldsrc = self.schema.items._generate_class_src(f"{self.get_class_name()}Item")
         item_classes.append(fieldsrc.cls)
