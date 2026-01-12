9fad534102184b570c64e5574995c10989451fe4
Shivani-gslab <145646625+Shivani-gslab@users.noreply.github.com>
2026-01-12 11:02:48 +0530

    Update python-avd/pyavd/_eos_designs/structured_config/base/__init__.py
    Co-authored-by: Guillaume Mulocher <gmulocher@arista.com>

--- python-avd/pyavd/_eos_designs/structured_config/base/__init__.py
+++ python-avd/pyavd/_eos_designs/structured_config/base/__init__.py
@@ -689,7 +689,10 @@ class AvdStructuredConfigBaseProtocol(
                 )
 
             radius_server_vrf = self.structured_config.radius_server.vrfs.obtain(server_vrf)
-            radius_server_vrf.servers.append_new(host=server.host, tls=server.tls, key=self._get_tacacs_or_radius_server_password(server))
+            if server.tls.enabled:
+                radius_server_vrf.servers.append_new(host=server.host, vrf=server_vrf, tls=server.tls)
+            else:
+                radius_server_vrf.servers.append_new(host=server.host, vrf=server_vrf, key=self._get_tacacs_or_radius_server_password(server))
 
             for group in server.groups:
                 radius_group = self.structured_config.aaa_server_groups.obtain(group)
