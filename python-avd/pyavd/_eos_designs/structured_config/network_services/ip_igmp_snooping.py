09c019e534dd676b225af9155bac8330f02e72a0
MaheshGSLAB <mahesh.kumar-ext@arista.com>
2026-01-13 15:55:07 +0530

    updated the code logic to remove cast

--- python-avd/pyavd/_eos_designs/structured_config/network_services/ip_igmp_snooping.py
+++ python-avd/pyavd/_eos_designs/structured_config/network_services/ip_igmp_snooping.py
@@ -122,20 +122,20 @@ class IpIgmpSnoopingMixin(Protocol):
         source_address_key = default(l2vlan.igmp_snooping_querier.source_address, tenant.igmp_snooping_querier.source_address)
 
         source_address = self.shared_utils.router_id if source_address_key in {"main_router_id", "diagnostic_loopback", "vrf_router_id"} else source_address_key
-        source_address = cast("str", source_address) if source_address is None else source_address
-        # Validate (None and invalid IPv4 handled together)
-        try:
-            IPv4Address(source_address)
-        except AddressValueError:
-            msg = (
-                f"Invalid IGMP snooping querier source address for VLAN '{l2vlan.name}' "
-                f"in Tenant '{tenant.name}'. The value '{source_address}' resolved from "
-                f"'igmp_snooping_querier.source_address: {source_address_key}' "
-                "is not a valid IPv4 address."
-            )
-            raise AristaAvdInvalidInputsError(msg) from None
-
-        return source_address
+        msg = (
+            f"Invalid IGMP snooping querier source address for VLAN '{l2vlan.name}' "
+            f"in Tenant '{tenant.name}'. The value '{source_address}' resolved from "
+            f"'igmp_snooping_querier.source_address: {source_address_key}' "
+            "is not a valid IPv4 address."
+        )
+        if source_address is not None:
+            try:
+                IPv4Address(source_address)
+            except AddressValueError:
+                raise AristaAvdInvalidInputsError(msg) from None
+            else:
+                return source_address
+        raise AristaAvdInvalidInputsError(msg)
 
     def _get_svi_igmp_querier_source_address(
         self: AvdStructuredConfigNetworkServicesProtocol,
@@ -166,17 +166,19 @@ class IpIgmpSnoopingMixin(Protocol):
             case _:
                 source_address = source_address_key
 
-        source_address = cast("str", source_address) if source_address is None else source_address
+        msg = (
+            f"Invalid IGMP snooping querier source address for VLAN '{svi.name}' "
+            f"in VRF '{vrf.name}' in Tenant '{tenant.name}'. "
+            f"The value '{source_address}' resolved from "
+            f"'igmp_snooping_querier.source_address: {source_address_key}' is not a valid IPv4 address."
+        )
 
-        try:
-            IPv4Address(source_address)
-        except AddressValueError:
-            msg = (
-                f"Invalid IGMP snooping querier source address for VLAN '{svi.name}' "
-                f"in VRF '{vrf.name}' in Tenant '{tenant.name}'. "
-                f"The value '{source_address}' resolved from "
-                f"'igmp_snooping_querier.source_address: {source_address_key}' is not a valid IPv4 address."
-            )
-            raise AristaAvdInvalidInputsError(msg) from None
+        if source_address is not None:
+            try:
+                IPv4Address(source_address)
+            except AddressValueError:
+                raise AristaAvdInvalidInputsError(msg) from None
+            else:
+                return source_address
 
-        return source_address
+        raise AristaAvdInvalidInputsError(msg)
