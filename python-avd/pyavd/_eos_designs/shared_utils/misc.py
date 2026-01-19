b0815de833bed4ea76440c1d672c18513d1ee544
Vibhu-gslab <vibhu.tripathi-ext@arista.com>
2026-01-19 13:55:21 +0530

    updating the error msg

--- python-avd/pyavd/_eos_designs/shared_utils/misc.py
+++ python-avd/pyavd/_eos_designs/shared_utils/misc.py
@@ -118,8 +118,8 @@ class MiscMixin(Protocol):
         if uplink_interface_candidates := range_expand(self.node_config.uplink_interfaces or self.cv_topology_config.uplink_interfaces):
             if len(uplink_interface_candidates) != len(self.uplink_switches):
                 msg = (
-                    f"Length of uplink_interfaces: {uplink_interface_candidates} does not match the length of uplink_switches: {self.uplink_switches} for host:"
-                    f" {self.hostname}."
+                    f"Length of uplink_interfaces: {len(uplink_interface_candidates)} does not match the length of uplink_switches: {len(self.uplink_switches)}"
+                    f" for host: {self.hostname}."
                 )
                 raise AristaAvdInvalidInputsError(msg)
             return uplink_interface_candidates
@@ -127,8 +127,8 @@ class MiscMixin(Protocol):
         uplink_interface_candidates = range_expand(self.default_interfaces.uplink_interfaces)
         if len(uplink_interface_candidates) < len(self.uplink_switches):
             msg = (
-                f"Length of default_interfaces.uplink_interfaces: {uplink_interface_candidates} is less than the length of uplink_switches: "
-                f"{self.uplink_switches} for host: {self.hostname}."
+                f"Length of default_interfaces.uplink_interfaces: {len(uplink_interface_candidates)} is less than the length of uplink_switches: "
+                f"{len(self.uplink_switches)} for host: {self.hostname}."
             )
             raise AristaAvdInvalidInputsError(msg)
 
