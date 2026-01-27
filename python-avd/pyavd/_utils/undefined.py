b2822bf1478ed4ae4f72498b59c7638d0641abfb
gmuloc <gmulocher@arista.com>
2026-01-27 15:03:32 +0100

    Refactor: Remove the need for extra dep

--- python-avd/pyavd/_utils/undefined.py
+++ python-avd/pyavd/_utils/undefined.py
@@ -1,9 +1,12 @@
 # Copyright (c) 2024-2026 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
-from typing import Any, Literal
+from __future__ import annotations
 
-from typing_extensions import Self
+from typing import TYPE_CHECKING, Any, Literal
+
+if TYPE_CHECKING:
+    from typing_extensions import Self
 
 
 class UndefinedType:
