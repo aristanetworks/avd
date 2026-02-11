3c535cdb3663e69a579aca8a89ed5eef76602257
gmuloc <gmulocher@arista.com>
2026-02-11 17:06:15 +0100

    CI: Make ansible-test sanity happy

--- ansible_collections/arista/avd/plugins/action/verify_requirements.py
+++ ansible_collections/arista/avd/plugins/action/verify_requirements.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2023-2026 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
+from __future__ import annotations
 
 import json
 import sys
