ea623385d0f3829c7aea0934cacba177301018d5
Claus Holbech <holbech@arista.com>
2026-02-19 13:04:31 +0100

    Update python-avd/tests/pyavd/cv/workflows/test_models.py

--- python-avd/tests/pyavd/cv/workflows/test_models.py
+++ python-avd/tests/pyavd/cv/workflows/test_models.py
@@ -2,7 +2,6 @@
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
 
-from dataclasses import asdict
 from pathlib import Path
 from typing import Any
 
