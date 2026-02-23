636a20ed4222804365b27c541cd77024f76a1b29
Mahesh Kumar <122076792+MaheshGSLAB@users.noreply.github.com>
2026-02-23 17:48:39 +0530

    Update development/sort_hosts_yml.py
    Co-authored-by: Guillaume Mulocher <gmulocher@arista.com>

--- development/sort_hosts_yml.py
+++ development/sort_hosts_yml.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2025-2026 Arista Networks, Inc.
+# Copyright (c) 2026 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
 from __future__ import annotations
