c9742fb2e51cb8d5c231b354d52f0e651b1feeb5
Shivani-gslab <schourasiya-ext@arista.com>
2026-02-27 12:44:57 +0530

    adding comment

--- python-avd/pyavd/_eos_designs/structured_config/network_services/ip_igmp_snooping.py
+++ python-avd/pyavd/_eos_designs/structured_config/network_services/ip_igmp_snooping.py
@@ -107,8 +107,9 @@ class IpIgmpSnoopingMixin(Protocol):
                 vlan_item.querier.version = default(vlan.igmp_snooping_querier.version, tenant.igmp_snooping_querier.version)
 
         # Set fast_leave regardless of evpn_l2_multicast_enabled state
-        # When evpn_l2_multicast is enabled, use deprecated evpn_l2_multicast.fast_leave as last resort
+        # When evpn_l2_multicast is enabled, use deprecated evpn_l2_multicast.fast_leave
         # When evpn_l2_multicast is not enabled, ignore the deprecated key
+        # TODO: 7.0.0 - Remove support for evpn_l2_multicast.fast_leave and clean up this logic
         if evpn_l2_multicast_enabled:
             fast_leave = default(vlan.igmp_snooping_querier.fast_leave, tenant.igmp.fast_leave, tenant.evpn_l2_multicast.fast_leave)
         else:
