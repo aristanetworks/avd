62fadcff04d411ef8a40f5e7eb8a362b827b3475
Shivani-gslab <145646625+Shivani-gslab@users.noreply.github.com>
2026-04-15 17:00:56 +0530

    Apply suggestions from code review
    Co-authored-by: laxmikantchintakindi <159624484+laxmikantchintakindi@users.noreply.github.com>

--- python-avd/pyavd/_eos_designs/shared_utils/misc.py
+++ python-avd/pyavd/_eos_designs/shared_utils/misc.py
@@ -432,7 +432,7 @@ class MiscMixin(Protocol):
 
         self._update_l3_generic_interface_ipv4_bgp(interface, description, context, neighbors, prefix_lists, route_maps)
 
-        if isinstance(interface, EosDesigns._DynamicKeys.DynamicNodeTypesItem.NodeTypes.NodesItem.L3InterfacesItem):
+        if schema_key == "l3_interfaces:
             self._update_l3_interface_ipv6_bgp(interface, description, ipv6_neighbors)
 
     @cached_property
