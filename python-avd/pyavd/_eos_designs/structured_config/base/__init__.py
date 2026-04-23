83342dd9194cf1dbdccfdf006bccd56efc563694
MaheshGSLAB <mahesh.kumar-ext@arista.com>
2026-04-23 11:13:51 +0530

    updated the method name

--- python-avd/pyavd/_eos_designs/structured_config/base/__init__.py
+++ python-avd/pyavd/_eos_designs/structured_config/base/__init__.py
@@ -309,10 +309,17 @@ class AvdStructuredConfigBaseProtocol(
         self.structured_config.queue_monitor_length = queue_monitor_length
 
     @structured_config_contributor
-    def ip_name_server(self) -> None:
-        """Set ip name servers using old name_servers model and new dns_settings model. Results will be combined."""
+    def dns_settings(self) -> None:
+        """
+        Configure DNS settings from the dns_settings input model.
+
+        Sets IP name servers (with VRF and priority), IP hosts, DNS domain, domain list, and domain-lookup source interfaces per VRF.
+        """
         if not self.inputs.dns_settings:
             return
+
+        if self.inputs.dns_settings.ip_hosts:
+            self.structured_config.ip_hosts = EosCliConfigGen.IpHosts(self.inputs.dns_settings.ip_hosts)
 
         if self.inputs.dns_settings.domain:
             self.structured_config.dns_domain = self.inputs.dns_settings.domain
@@ -332,13 +339,6 @@ class AvdStructuredConfigBaseProtocol(
 
             ip_name_server_vrf = self.structured_config.ip_name_server.vrfs.obtain(server_vrf)
             ip_name_server_vrf.servers.append_new(ip_address=server.ip_address, priority=server.priority)
-
-    @structured_config_contributor
-    def ip_hosts(self) -> None:
-        """Configure IP host setting based on the input data model."""
-        if not self.inputs.dns_settings.ip_hosts:
-            return
-        self.structured_config.ip_hosts = self.inputs.dns_settings.ip_hosts
 
     @structured_config_contributor
     def logging(self) -> None:
