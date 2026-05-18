f6e130972b5d8e00902fbb144ec0b2c77cb9bf39
Vibhu-gslab <vibhu.tripathi-ext@arista.com>
2026-05-18 13:12:23 +0530

    removing space

--- python-avd/pyavd/_eos_designs/structured_config/base/monitor_connectivity.py
+++ python-avd/pyavd/_eos_designs/structured_config/base/monitor_connectivity.py
@@ -31,7 +31,7 @@ class MonitorConnectivityMixin(Protocol):
             name_server_group=self.inputs.monitor_connectivity.name_server_group,
         )
         for interface_set in self.inputs.monitor_connectivity.interface_sets:
-            monitor_connectivity.interface_sets.append_new(name=interface_set.name, interfaces=", ".join(interface_set.interfaces))
+            monitor_connectivity.interface_sets.append_new(name=interface_set.name, interfaces=",".join(interface_set.interfaces))
         if (local_interfaces := self.inputs.monitor_connectivity.local_interfaces) is not None:
             if local_interfaces in self.inputs.monitor_connectivity.interface_sets:
                 monitor_connectivity.local_interfaces = local_interfaces
@@ -52,7 +52,7 @@ class MonitorConnectivityMixin(Protocol):
                 address_only=vrf.address_only,
             )
             for interface_set in vrf.interface_sets:
-                monitor_connectivity_vrf.interface_sets.append_new(name=interface_set.name, interfaces=", ".join(interface_set.interfaces))
+                monitor_connectivity_vrf.interface_sets.append_new(name=interface_set.name, interfaces=",".join(interface_set.interfaces))
             if (vrf_local_interfaces := vrf.local_interfaces) is not None:
                 if vrf_local_interfaces in vrf.interface_sets:
                     monitor_connectivity_vrf.local_interfaces = vrf_local_interfaces
