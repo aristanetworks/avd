53df762eb1dbdf36e3fefa7835381d4e900b6401
Mahesh Kumar <122076792+MaheshGSLAB@users.noreply.github.com>
2026-06-03 17:24:22 +0530

    Refactor(eos_designs): Include TLS on radius server group members (#7033)
    Co-authored-by: Shivani-gslab <145646625+Shivani-gslab@users.noreply.github.com>
    Co-authored-by: Carl Baillargeon <carl.baillargeon@arista.com>
    Co-authored-by: Shivani-gslab <schourasiya-ext@arista.com>

--- python-avd/pyavd/_eos_designs/schema/__init__.py
+++ python-avd/pyavd/_eos_designs/schema/__init__.py
@@ -367,7 +367,13 @@ class EosDesigns(EosDesignsRootModel):
                 recommended to make use of a vault or similar.
                 """
                 tls: EosCliConfigGen.RadiusServer.ServersItem.Tls
-                """When TLS is configured, `key` and `cleartext_key` are ignored."""
+                """
+                When TLS is configured, `key` and `cleartext_key` are ignored.
+                For proper functionality to render
+                TLS settings on the
+                server group member entry, set `avd_design_future.fix_radius_server_group_tls:
+                true`.
+                """
 
                 if TYPE_CHECKING:
 
@@ -425,7 +431,12 @@ class EosDesigns(EosDesignsRootModel):
                                Encrypted to Type 7 by AVD.
                                To protect the password at rest it is strongly
                                recommended to make use of a vault or similar.
-                            tls: When TLS is configured, `key` and `cleartext_key` are ignored.
+                            tls:
+                               When TLS is configured, `key` and `cleartext_key` are ignored.
+                               For proper functionality to render
+                               TLS settings on the
+                               server group member entry, set `avd_design_future.fix_radius_server_group_tls:
+                               true`.
 
                         """
 
@@ -977,6 +988,7 @@ class EosDesigns(EosDesignsRootModel):
             "configure_inband_mgmt_ipv6_vrf": {"type": bool, "default": False},
             "only_configure_ipv6_inband_mgmt_prefix_list_when_used": {"type": bool, "default": False},
             "consistent_uplink_vlans": {"type": bool, "default": False},
+            "fix_radius_server_group_tls": {"type": bool, "default": False},
         }
         accept_dhcp_default_route_for_mgmt_ip_dhcp: bool
         """
@@ -1031,6 +1043,13 @@ class EosDesigns(EosDesignsRootModel):
 
         Default value: `False`
         """
+        fix_radius_server_group_tls: bool
+        """
+        Fix to configure TLS on RADIUS server group members to match their global RADIUS server
+        configurations.
+
+        Default value: `False`
+        """
 
         if TYPE_CHECKING:
 
@@ -1045,6 +1064,7 @@ class EosDesigns(EosDesignsRootModel):
                 configure_inband_mgmt_ipv6_vrf: bool | UndefinedType = Undefined,
                 only_configure_ipv6_inband_mgmt_prefix_list_when_used: bool | UndefinedType = Undefined,
                 consistent_uplink_vlans: bool | UndefinedType = Undefined,
+                fix_radius_server_group_tls: bool | UndefinedType = Undefined,
             ) -> None:
                 """
                 AvdDesignFuture.
@@ -1069,6 +1089,9 @@ class EosDesigns(EosDesignsRootModel):
                        Always configure Port-Channel uplinks with consistent 'switchport trunk allowed' on both ends
                        and on
                        all 'uplink_switches' even when available VLANs differ between the 'uplink_switches'.
+                    fix_radius_server_group_tls:
+                       Fix to configure TLS on RADIUS server group members to match their global RADIUS server
+                       configurations.
 
                 """
 
