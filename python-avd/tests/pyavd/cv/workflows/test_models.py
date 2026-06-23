7a194cc15e9b4704bc8263b0e67a4f37834f28fa
Alexey Gorbunov <ag.justice@gmail.com>
2026-06-23 12:57:02 -0700

    WS rebase phase3: Remove stale pylint exception for line qty

--- python-avd/tests/pyavd/cv/workflows/test_models.py
+++ python-avd/tests/pyavd/cv/workflows/test_models.py
@@ -1,7 +1,6 @@
 # Copyright (c) 2025-2026 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
-# pylint: disable=too-many-lines
 
 import re
 from contextlib import AbstractContextManager
