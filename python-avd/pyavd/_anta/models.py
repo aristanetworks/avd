3b585d1fbd8ead7fb02b13d8704639eb5f84545e
Alexey Gorbunov <ag.justice@gmail.com>
2026-06-29 12:05:40 -0700

    Fix call to AristaAvdError in _anta

--- python-avd/pyavd/_anta/models.py
+++ python-avd/pyavd/_anta/models.py
@@ -90,7 +90,7 @@ class InputFactoryDataSource:
         """Get the AVDDeviceData object for this device."""
         device_data = self._fabric_data.devices.get(self.hostname)
         if device_data is None:
-            raise AristaAvdError(message=f"Device '{self.hostname}' structured configuration is not loaded in AVDFabricData.")
+            raise AristaAvdError(f"Device '{self.hostname}' structured configuration is not loaded in AVDFabricData.")
         return device_data
 
     @cached_property
