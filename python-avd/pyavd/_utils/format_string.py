57ee3579061fa2ff2811cb342aed326c7f62f863
laxmikantchintakindi <laxmikant.c-ext@arista.com>
2026-07-08 16:13:48 +0530

    Update docstring

--- python-avd/pyavd/_utils/format_string.py
+++ python-avd/pyavd/_utils/format_string.py
@@ -25,7 +25,7 @@ class AvdStringFormatter(Formatter):
         suffix ::= string including spaces which will be inserted after the field value.
                    Most useful in combination with ?. Suffix should not contain "<", ">", "!" or ":".
         conversion ::= "!u" for "upper()", "!l" for "lower()", "!t" for "title()" and
-                       "!c" for short name of the network interfaces. e.g Portchannel2.2 > Po2.2, Ethernet1 > Et1.
+                       "!c" for short name of the network interfaces. e.g Port-channel2.2 > Po2.2, Ethernet1 > Et1.
                        Using "!c" for other strings gives no result (The regular Python conversions "!r", "!s", "!a" have been removed).
 
     Note the order of syntax field matters!
@@ -142,8 +142,8 @@ class AvdStringFormatter(Formatter):
         """
         Convert the value according to the given conversion instruction.
 
-        Mostly a copy from the base class, but supporting !u for upper(), !t for title(), !l for lower() and
-        !c for short names of portchannel, ethernet and vlan interfaces.
+        Supports !u for upper(), !t for title(), !l for lower() and
+        !c for short names of portchannel, ethernet, loopback, vlan and tunnel interfaces.
         """
         # do any conversion on the resulting object
         if conversion is None:
