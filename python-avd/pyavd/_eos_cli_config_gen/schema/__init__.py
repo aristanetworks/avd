17308dad7ff0a069510b583b20846b611fb9d2c1
MaheshGSLAB <mahesh.kumar-ext@arista.com>
2026-07-14 11:08:04 +0530

    updated schema and eos template

--- python-avd/pyavd/_eos_cli_config_gen/schema/__init__.py
+++ python-avd/pyavd/_eos_cli_config_gen/schema/__init__.py
@@ -6618,13 +6618,16 @@ class EosCliConfigGen(EosCliConfigGenRootModel):
         """
         render_combined_separator_for_ipv6_hardware_and_unicast_routing: bool
         """
-        Render a single `!` separator for the combined IPv6 routing block when any of these are rendered:
-        -
-        `ipv6 unicast-routing`
-        - `ipv6 unicast-routing vrf <vrf_name>`
-        - `ipv6 hardware fib optimize
-        prefixes profile <profile_name>`
-        When `false`, separators are handled by each individual section.
+        Available from AVD 6.4.0.
+        Render a single '!' separator for the combined IPv6 routing block when any
+        of these are rendered:
+        - 'ipv6 unicast-routing'
+        - 'ipv6 unicast-routing vrf <vrf_name>'
+        - 'ipv6
+        hardware fib optimize prefixes profile <profile_name>'
+        When 'false', separators are handled by each
+        individual section.
+        When 'true', it overrides 'always_render_ipv6_unicast_routing_vrfs_separator'.
 
         Default value: `False`
         """
@@ -6714,13 +6717,16 @@ class EosCliConfigGen(EosCliConfigGenRootModel):
                        Without this the '!' is missing when the first VRF (once VRFs are sorted) is not configured
                        for IPv6 routing.
                     render_combined_separator_for_ipv6_hardware_and_unicast_routing:
-                       Render a single `!` separator for the combined IPv6 routing block when any of these are rendered:
-                       -
-                       `ipv6 unicast-routing`
-                       - `ipv6 unicast-routing vrf <vrf_name>`
-                       - `ipv6 hardware fib optimize
-                       prefixes profile <profile_name>`
-                       When `false`, separators are handled by each individual section.
+                       Available from AVD 6.4.0.
+                       Render a single '!' separator for the combined IPv6 routing block when any
+                       of these are rendered:
+                       - 'ipv6 unicast-routing'
+                       - 'ipv6 unicast-routing vrf <vrf_name>'
+                       - 'ipv6
+                       hardware fib optimize prefixes profile <profile_name>'
+                       When 'false', separators are handled by each
+                       individual section.
+                       When 'true', it overrides 'always_render_ipv6_unicast_routing_vrfs_separator'.
                     new_ip_radius_cli_order:
                        Available from AVD 6.1.0.
                        When `true`, renders the new EOS CLI order using `ip_radius`, sorted by
