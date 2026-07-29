b2d8e3abb25e300605727fc463fc8a1293cf2640
Vibhu-gslab <vibhu.tripathi-ext@arista.com>
2026-07-29 12:43:04 +0530

    fixing CI

--- python-avd/pyavd/_eos_designs/structured_config/network_services/router_bgp.py
+++ python-avd/pyavd/_eos_designs/structured_config/network_services/router_bgp.py
@@ -284,13 +284,13 @@ class RouterBgpMixin(Protocol):
                                 if ospf_redistribute.route_map:
                                     bgp_vrf.redistribute.ospf.route_map = ospf_redistribute.route_map
                     elif vrf.redistribute_ospf:
-                            bgp_vrf.redistribute.ospf.enabled = True
-                            warnings.warn(
-                                "<network_services_key>.vrfs.redistribute_ospf is deprecated and will be removed in AVD 7.0. "
-                                "Use redistribute_ospf_settings instead.",
-                                AristaAvdModelDeprecationWarning,
-                                stacklevel=2,
-                            )
+                        bgp_vrf.redistribute.ospf.enabled = True
+                        warnings.warn(
+                            "<network_services_key>.vrfs.redistribute_ospf is deprecated and will be removed in AVD 7.0. "
+                            "Use redistribute_ospf_settings instead.",
+                            AristaAvdModelDeprecationWarning,
+                            stacklevel=2,
+                        )
 
                 if bgp_vrf.neighbors and self.inputs.bgp_update_wait_install and self.shared_utils.platform_settings.feature_support.bgp_update_wait_install:
                     bgp_vrf.updates.wait_install = True
