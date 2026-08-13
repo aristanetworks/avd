2640734266082c743bfec09450fccfeea1272c84
laxmikantchintakindi <159624484+laxmikantchintakindi@users.noreply.github.com>
2026-08-13 10:17:08 +0000

    Feat(eos_designs): Add support for spanning_tree_settings.bpdu_guard_default (#7327)
    Co-authored-by: Mahesh Kumar <122076792+MaheshGSLAB@users.noreply.github.com>

--- python-avd/pyavd/_eos_designs/schema/__init__.py
+++ python-avd/pyavd/_eos_designs/schema/__init__.py
@@ -48517,6 +48517,7 @@ class EosDesigns(EosDesignsRootModel):
             "priority": {"type": int, "default": 32768},
             "port_id_allocation_port_channel_range": {"type": EosCliConfigGen.SpanningTree.PortIdAllocationPortChannelRange},
             "loop_guard_default": {"type": bool, "default": False},
+            "edge_port_bpduguard_default": {"type": bool, "default": False},
         }
         mode: Mode | None
         """
@@ -48549,6 +48550,12 @@ class EosDesigns(EosDesignsRootModel):
 
         Default value: `False`
         """
+        edge_port_bpduguard_default: bool
+        """
+        Enable BPDU Guard on portfast interfaces.
+
+        Default value: `False`
+        """
 
         if TYPE_CHECKING:
 
@@ -48559,6 +48566,7 @@ class EosDesigns(EosDesignsRootModel):
                 priority: int | UndefinedType = Undefined,
                 port_id_allocation_port_channel_range: EosCliConfigGen.SpanningTree.PortIdAllocationPortChannelRange | UndefinedType = Undefined,
                 loop_guard_default: bool | UndefinedType = Undefined,
+                edge_port_bpduguard_default: bool | UndefinedType = Undefined,
             ) -> None:
                 """
                 SpanningTreeSettings.
@@ -48585,6 +48593,7 @@ class EosDesigns(EosDesignsRootModel):
                        If
                        both are set, the setting under node type settings takes precedence.
                     loop_guard_default: Enable loopguard by default on all ports.
+                    edge_port_bpduguard_default: Enable BPDU Guard on portfast interfaces.
 
                 """
 
