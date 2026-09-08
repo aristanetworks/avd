63505e67dcba736242cfe195d3d1d82ce14591e2
laxmikantchintakindi <laxmikant.c-ext@arista.com>
2026-09-08 17:53:54 +0530

    Improve coverage

--- python-avd/pyavd/_eos_designs/shared_utils/utils.py
+++ python-avd/pyavd/_eos_designs/shared_utils/utils.py
@@ -112,8 +112,8 @@ class UtilsMixin(Protocol):
         resolved_profile = self.inputs.port_profiles[profile_name]._deepcopy()
         if self.inputs.avd_design_future.allow_infinite_profile_inheritance:
             while port_profile.parent_profile is not None:
-                if profile_name not in self.inputs.port_profiles:
-                    msg = f"Profile '{profile_name}' applied under port profile '{profile_name}' does not exist in `port_profiles`."
+                if port_profile.parent_profile  not in self.inputs.port_profiles:
+                    msg = f"Profile '{port_profile.parent_profile}' applied under port profile '{profile_name}' does not exist in 'port_profiles'."
                     raise AristaAvdInvalidInputsError(msg)
                 if port_profile.parent_profile in port_profiles_chain or port_profile.parent_profile == profile_name:
                     msg = (
