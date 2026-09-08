bac0517a85e14abadfdc00286dd93dcf4a03119f
pvinci <pvinci@arista.com>
2026-09-08 16:56:13 -0400

    Fix(eos_designs): Preserve ACT node settings compatibility

--- python-avd/pyavd/api/fabric_documentation/__init__.py
+++ python-avd/pyavd/api/fabric_documentation/__init__.py
@@ -21,10 +21,10 @@ class ActNodeSettings:
     node_type: str
     ip_addr: str | None
     version: str
-    instance_type: str | None
     # internet_access attribute is only applicable to cloudeos and veos node types and is ignored by ACT for all other node types
     internet_access: bool | None
     ports: tuple[str, ...] | None
+    instance_type: str | None = None
 
 
 @dataclass(frozen=True)
