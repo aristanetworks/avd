6ffa041d9e6a698efe421622cecf333c5b85981f
laxmikantchintakindi <laxmikant.c-ext@arista.com>
2026-09-09 16:26:17 +0530

    Fixing negative test

--- python-avd/pyavd/_eos_designs/shared_utils/device_config.py
+++ python-avd/pyavd/_eos_designs/shared_utils/device_config.py
@@ -47,7 +47,7 @@ class DeviceConfigMixin(Protocol):
             while device_profile.parent_profile is not None:
                 if not (device_parent_profile := self.inputs.device_profiles.get(device_profile.parent_profile)):
                     msg = (
-                        f"The Device Profile '{device_profile.parent_profile}' applied for the device '{self.hostname}' does not exist under `device_profiles`."
+                        f"The Device Profile '{device_profile.parent_profile}' applied for the device '{self.hostname}' does not exist under 'device_profiles'."
                     )
                     raise AristaAvdInvalidInputsError(msg)
                 if device_profile.parent_profile in device_profiles_chain or device_profile.parent_profile == device_profile_name:
