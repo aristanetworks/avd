04a6fcb0f07e3d1f0f5344f8fde39fb7fda8f6af
MaheshGSLAB <mahesh.kumar-ext@arista.com>
2026-09-11 17:34:07 +0530

    updated the hash logic for only type 0

--- python-avd/pyavd/_eos_designs/structured_config/base/snmp_server.py
+++ python-avd/pyavd/_eos_designs/structured_config/base/snmp_server.py
@@ -196,9 +196,9 @@ class SnmpServerMixin(Protocol):
 
                 if user.auth is not None and user.auth_passphrase is not None:
                     user_dict.auth = user.auth
-                    hash_filter = {}
-                    if compute_v3_user_localized_key:
-                        hash_filter = {"passphrase": user.auth_passphrase, "auth": user.auth, "engine_id": engine_ids.local}
+                    hash_filter = {"auth": user.auth, "engine_id": engine_ids.local} if compute_v3_user_localized_key else {}
+                    if compute_v3_user_localized_key and user.auth_passphrase_type not in ["7", "8a"]:
+                        hash_filter.update({"passphrase": user.auth_passphrase})
                         user_dict.auth_passphrase = snmp_hash(hash_filter)
                     else:
                         user_dict.auth_passphrase = user.auth_passphrase
@@ -207,7 +207,7 @@ class SnmpServerMixin(Protocol):
 
                     if user.priv is not None and user.priv_passphrase is not None:
                         user_dict.priv = user.priv
-                        if compute_v3_user_localized_key:
+                        if compute_v3_user_localized_key and user.priv_passphrase_type not in ["7", "8a"]:
                             hash_filter.update({"passphrase": user.priv_passphrase, "priv": user.priv})
                             user_dict.priv_passphrase = snmp_hash(hash_filter)
                         else:
