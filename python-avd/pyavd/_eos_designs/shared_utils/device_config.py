90c76e4812d29e169dea089a7d0f2f8cb2e88eb3
laxmikantchintakindi <laxmikant.c-ext@arista.com>
2026-09-18 14:29:26 +0530

    Fixing eror

--- python-avd/pyavd/_eos_designs/shared_utils/device_config.py
+++ python-avd/pyavd/_eos_designs/shared_utils/device_config.py
@@ -48,7 +48,7 @@ class DeviceConfigMixin(Protocol):
                 if not (device_parent_profile := self.inputs.device_profiles.get(device_profile.parent_profile)):
                     msg = (
                         f"The parent device profile '{device_profile.parent_profile}' applied under the profile '{device_profile.name}'"
-                        "does not exist in 'device_profiles'."
+                        " does not exist in 'device_profiles'."
                     )
                     raise AristaAvdInvalidInputsError(msg, host=self.hostname)
                 if device_profile.parent_profile in device_profiles_chain or device_profile.parent_profile == device_profile_name:
