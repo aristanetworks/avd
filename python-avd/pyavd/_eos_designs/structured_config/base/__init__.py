60d77734f0e460b518c8082e15e1d93ab3133ebf
vitthalmagadum <vitthal.magadum-ext@arista.com>
2026-09-22 09:07:03 +0530

    Addressed review comments: updated the logic

--- python-avd/pyavd/_eos_designs/structured_config/base/__init__.py
+++ python-avd/pyavd/_eos_designs/structured_config/base/__init__.py
@@ -370,28 +370,24 @@ class AvdStructuredConfigBaseProtocol(
         tcam_profile_name = self.shared_utils.platform_settings.tcam_profile
         additional_tcam_profile_names = self.shared_utils.platform_settings.additional_tcam_profiles
 
-        # Set system profile if configured
-        if tcam_profile_name:
-            self.structured_config.tcam_profile.system = tcam_profile_name
+        tcam_profiles = EosCliConfigGen.TcamProfile.Profiles()
 
-        # Determine which profiles to configure
-        profiles_to_configure = []
-
-        # Add additional profiles first (in order specified), validating they are defined
+        # Add additional profiles first
         for additional_tcam_profile_name in additional_tcam_profile_names:
             if additional_tcam_profile_name not in self.inputs.tcam_profiles:
                 msg = f"TCAM profile '{additional_tcam_profile_name}' referenced under 'additional_tcam_profiles' is not defined under 'tcam_profiles'."
                 raise AristaAvdInvalidInputsError(msg)
             if additional_tcam_profile_name != tcam_profile_name:
-                profiles_to_configure.append(additional_tcam_profile_name)
+                tcam_profiles.append(self.inputs.tcam_profiles[additional_tcam_profile_name])
 
-        # Add the system profile if it's in tcam_profiles
-        if tcam_profile_name and tcam_profile_name in self.inputs.tcam_profiles:
-            profiles_to_configure.append(tcam_profile_name)
+        # Set system profile if configured
+        if tcam_profile_name:
+            self.structured_config.tcam_profile.system = tcam_profile_name
+            # Add the system profile if it's in tcam_profiles
+            if tcam_profile_name in self.inputs.tcam_profiles:
+                tcam_profiles.append(self.inputs.tcam_profiles[tcam_profile_name])
 
-        # Add profiles to structured config
-        for profile_name in profiles_to_configure:
-            self.structured_config.tcam_profile.profiles.append(self.inputs.tcam_profiles[profile_name])
+        self.structured_config.tcam_profile.profiles = tcam_profiles
 
     @structured_config_contributor
     def mac_address_table(self) -> None:
